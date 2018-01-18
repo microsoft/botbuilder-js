02b13ac68bb68618944386e09b2f139dfaa6370e
Steven Ickman <stevenic@microsoft.com>
2018-01-17 17:10:25 -0800

    removed botbuilder-prompts

--- libraries/botbuilder-prompts/docs/build-docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-typedoc --entryPoint botbuilder-prompts --excludePrivate --includeDeclarations --ignoreCompilerErrors --module amd --out doc ../lib/index.d.ts --hideGenerator --name "Bot Builder SDK - Prompts" --readme none
