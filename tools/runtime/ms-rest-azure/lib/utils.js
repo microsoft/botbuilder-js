d28cc6b9e4c4eae7bbc32d2c9a15bce30ab77c20
Pablo Costantini <sixdemons@gmail.com>
2018-01-25 16:01:35 -0300

    Import test framework for testing ConnectorClient

---
+++ tools/runtime/ms-rest-azure/lib/utils.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information. 
+
+'use strict';
+
+var uuid = require('uuid');
+
+/**
+* Generated UUID
+*
+* @return {string} RFC4122 v4 UUID.
+*/
+exports.generateUuid = function () {
+  return uuid.v4();
+};
+
+exports = module.exports;
