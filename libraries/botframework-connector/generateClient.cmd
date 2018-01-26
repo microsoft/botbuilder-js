3f5192b8ad9ee18953a9df30dbb55a9f3b4a4543
Amit Stein <amit.stein@southworks.com>
2018-01-26 14:43:07 -0300

    Update generateClient.cmd to use config from Swagger/README.md

--- libraries/botframework-connector/generateClient.cmd
+++ libraries/botframework-connector/generateClient.cmd
@@ -1,2 +1,2 @@
-rd /s /q lib
-autorest --input-file=swagger\ConnectorAPI.json --output-folder=lib --add-credentials --package-name=connector --nodejs --override-client-name=ConnectorClient
+rd /s /q lib\generated
+call autorest .\Swagger\README.md --nodejs
