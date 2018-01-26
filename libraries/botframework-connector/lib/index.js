13c38b38d2fc6b198a2903f6d8cddb681f7fe81b
Pablo Costantini <sixdemons@gmail.com>
2018-01-26 17:02:57 -0300

    Export models in connector package

--- libraries/botframework-connector/lib/index.js
+++ libraries/botframework-connector/lib/index.js
@@ -2,3 +2,4 @@
 
 exports.ConnectorClient = require('./generated/connectorClient');
 exports.MicrosoftAppCredentials = require('./customs/microsoftAppCredentials').MicrosoftAppCredentials;
+exports.Models = require('./generated/models');
