81b7d70487b79cbd72a95c932cd7001ac695070a
Pablo Costantini <sixdemons@gmail.com>
2018-01-26 17:02:57 -0300

    Export models in connector package

--- libraries/botframework-connector/lib/index.d.ts
+++ libraries/botframework-connector/lib/index.d.ts
@@ -1,2 +1,3 @@
 export { default as ConnectorClient } from './generated/connectorClient';
 export { MicrosoftAppCredentials } from './customs/microsoftAppCredentials';
+export { default as Models } from './generated/models';
