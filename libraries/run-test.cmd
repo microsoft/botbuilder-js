b90b8f3710b71db6a58c097d7aa6d684832ad672
Steven Ickman <stevenic@microsoft.com>
2018-01-29 17:57:14 -0800

    Updated unit tests to run under lerna (#22)
    - Added echo statements to swagger generated packages.
    - Commented out a test with a lerna dependency in qnaMakerTests.js
    - Renamed testall.cmd to run-test.cmd
    - Deleted testall.sh

---
+++ libraries/run-test.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo setting test keys
+call \\fusebox\private\sdk\UnitTestKeys.cmd
+echo starting storage emulator
+@if exist "%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe" (
+  "%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe" start
+) else (
+  @echo You don't have Azure Storage Emulator installed
+  @echo go to https://go.microsoft.com/fwlink/?LinkId=717179 to download and install 
+)
+call lerna run test
