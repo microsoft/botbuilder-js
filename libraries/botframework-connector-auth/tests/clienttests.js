4ad38699f1ba24c37c024b256a1c0f82c1a65faf
Amit Stein <amit.stein@southworks.com>
2018-01-31 14:02:11 -0300

    Cleanup

--- libraries/botframework-connector-auth/tests/clienttests.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-const connector = require('../');
-
-describe('Connector Auth Tests', function() {
-    console.warn('WARNING: no botframework-connector-auth tests have been defined yet.');
-});
