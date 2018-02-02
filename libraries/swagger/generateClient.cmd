66ee07d69eceb8e926b9a350fcbe989c59dd8719
pablo costantini <pablo.costantini@southworks.com>
2018-02-01 21:22:37 -0300

    generateClient.cmd (windows)

--- libraries/swagger/generateClient.cmd
+++ libraries/swagger/generateClient.cmd
@@ -1,2 +1,12 @@
+@echo off
+
 rd /s /q generated
-call autorest .\Swagger\README.md --nodejs
+call autorest README.md --nodejs
+
+call ..\..\node_modules\.bin\replace "'../models'" "'botbuilder-schema'" ./generated -r
+call ..\..\node_modules\.bin\replace "'./models'" "'botbuilder-schema'" ./generated -r
+
+rd /s /q ..\botbuilder-schema\src\generated\models
+rd /s /q ..\botframework-connector\lib\generated
+move generated\models ..\botbuilder-schema\src\generated\models
+move generated ..\botframework-connector\lib\
