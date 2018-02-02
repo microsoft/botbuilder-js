14dca50b75c40d4ca436e85701e7fe1e820694f7
Steven Ickman <stevenic@microsoft.com>
2018-02-02 12:36:42 -0800

    Ported AlarmBot smaple from TS to JS

---
+++ samples/alarmbot-es6/addAlarm.js
@@ -0,0 +1,54 @@
+
+module.exports = {
+    begin(context) {
+        // Set topic and initialize empty alarm
+        context.state.conversation.topic = 'addAlarm';
+        context.state.conversation.alarm = {};
+        return nextField(context);
+    },
+    routeReply(context) {
+        // Handle users reply to prompt
+        const utterance = context.request.text.trim();
+        switch (context.state.conversation.prompt) {
+            case 'title':
+                // Validate reply and save to alarm
+                if (utterance.length > 2) {
+                    context.state.conversation.alarm.title = utterance;
+                } else {
+                    context.reply(`I'm sorry. Your alarm should have a title at least 3 characters long.`);
+                }
+                break;
+            case 'time':
+                // TODO: validate time user replied with
+                context.state.conversation.alarm.time = utterance;
+                break;
+        }
+        return nextField(context);
+    }
+};
+
+function nextField(context) {
+    // Prompt user for next missing field
+    const alarm = context.state.conversation.alarm;
+    if (alarm.title === undefined) {
+        context.reply(`What would you like to call your alarm?`);
+        context.state.conversation.prompt = 'title';
+    } else if (alarm.time === undefined) {
+        context.reply(`What time would you like to set the "${alarm.title}" alarm for?`);
+        context.state.conversation.prompt = 'time';
+    } else {
+        // Alarm completed so set alarm.
+        const list = context.state.user.alarms || [];
+        list.push(alarm);
+        context.state.user.alarms = list;
+
+        // TODO: set alarm
+
+        // Notify user and cleanup topic state
+        context.reply(`Your alarm named "${alarm.title}" is set for "${alarm.time}".`);
+        context.state.conversation.topic = undefined;
+        context.state.conversation.alarm = undefined;
+        context.state.conversation.prompt = undefined;
+    }
+    return Promise.resolve();
+}
