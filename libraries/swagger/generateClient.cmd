3a31c008035045f1d68657d73cc353b5e5155afd
Pablo Costantini <sixdemons@gmail.com>
2018-02-05 19:55:26 -0300

    Flatten botbuilder-schema dir structure

--- libraries/swagger/generateClient.cmd
+++ libraries/swagger/generateClient.cmd
@@ -7,8 +7,8 @@ call ..\..\node_modules\.bin\replace "as Models from \"./models" "as Models from
 call ..\..\node_modules\.bin\replace "as Models from \"../models" "as Models from \"botbuilder-schema" ./generated -r
 call ..\..\node_modules\.bin\replace "\?: " ": " ./generated/models/index.ts
 
-del /q ..\botbuilder-schema\src\generated\*
+del /q ..\botbuilder-schema\src\index.ts
 rd /s /q ..\botframework-connector\src\generated
 
-move generated\models\index.ts ..\botbuilder-schema\src\generated\index.ts
+move generated\models\index.ts ..\botbuilder-schema\src\index.ts
 move generated ..\botframework-connector\src\
