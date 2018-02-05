d17a891acbdd6eef7a549ee20d6704169e3cfee2
Pablo Costantini <sixdemons@gmail.com>
2018-02-05 19:42:20 -0300

    Connector Types generation based on Typescript

--- libraries/swagger/generateClient.cmd
+++ libraries/swagger/generateClient.cmd
@@ -1,12 +1,14 @@
 @echo off
 
 rd /s /q generated
-call autorest README.md --nodejs
+call autorest README.md --typescript
 
-call ..\..\node_modules\.bin\replace "'../models'" "'botbuilder-schema'" ./generated -r
-call ..\..\node_modules\.bin\replace "'./models'" "'botbuilder-schema'" ./generated -r
+call ..\..\node_modules\.bin\replace "as Models from \"./models" "as Models from \"botbuilder-schema" ./generated -r
+call ..\..\node_modules\.bin\replace "as Models from \"../models" "as Models from \"botbuilder-schema" ./generated -r
 
-rd /s /q ..\botbuilder-schema\src\generated\models
-rd /s /q ..\botframework-connector\lib\generated
-move generated\models ..\botbuilder-schema\src\generated\models
-move generated ..\botframework-connector\lib\
+
+del /q ..\botbuilder-schema\src\generated\*
+rd /s /q ..\botframework-connector\src\generated
+
+move generated\models\index.ts ..\botbuilder-schema\src\generated\index.ts
+move generated ..\botframework-connector\src\
