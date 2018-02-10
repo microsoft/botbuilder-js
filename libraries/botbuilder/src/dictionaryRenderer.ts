ee2d659e37a1dd5de147f30ba8dd0e642b524fbf
Steven Ickman <stevenic@microsoft.com>
2018-02-09 16:14:59 -0800

    Fixed dilename case issue and publishing hot fix

--- libraries/botbuilder/src/dictionaryRenderer.ts
+++ libraries/botbuilder/src/dictionaryRenderer.ts
@@ -5,7 +5,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.  
  * Licensed under the MIT License.
  */
-import { TemplateRenderer } from './TemplateManager';
+import { TemplateRenderer } from './templateManager';
 import { Middleware } from './middleware';
 import { BotService } from './botService';
 import { Activity } from './activity';
