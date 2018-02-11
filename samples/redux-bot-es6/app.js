1764571cf73390852a76e6c695fdbd3dab0ed6e3
John Taylor <johtaylo@microsoft.com>
2018-02-11 15:53:47 -0800

    working sample of BotFwk and Redux

--- samples/redux-bot-es6/app.js
+++ samples/redux-bot-es6/app.js
@@ -1,4 +1,11 @@
 
+// This sample demonstrates how easy it can be to combine the BotFramework with
+// other frameworks. As an example of that we are using Redux to take
+// a state-centric view of our bot design.
+
+// In our example, once a conversation has started and messages have been exchanged this bot will
+// start to proactively report the current time to the conversation - every 10 seconds!
+
 const services = require('botbuilder-services');
 const restify = require('restify');
 const redux = require('redux');
@@ -12,22 +19,28 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Example middleware: lower case the activity text
+// Example redux middleware: lower case the activity text
 const lowerCase = store => next => action => {
-    action.activity.text = action.activity.text.toLowerCase();
+    if (action.type === 'message') {
+        action.activity.text = action.activity.text.toLowerCase();
+    }
     return next(action);
 }
 
 // Create store
 const store = redux.createStore(conversation.store, 
     redux.applyMiddleware(
+        // our custom Redux middleware
         lowerCase,
+        // and a popular piece of Redux middleware from npm
         createLogger()
 ));
 
 // Create adapter
 const adapter = new services.BotFrameworkAdapter(process.env.MICROSOFT_APP_ID, process.env.MICROSOFT_APP_PASSWORD);
 server.post('/api/messages', adapter.listen());
+
+// Redux provides a simple pub-sub model that we can use to help organize our application logic in a decoupled way
 
 // tie the adapter to the store
 adapter.onReceive = function (activity) {
@@ -39,7 +52,16 @@ adapter.onReceive = function (activity) {
 store.subscribe(() => {
     const state = store.getState();
     if (state) {
-        adapter.post([ state[state.length - 1].bot ]);
+        const last = state[state.length - 1];
+        if (last && last.bot) {
+            adapter.post([ last.bot ]);
+        }
     }
 });
 
+// and now let's have another source of conversation state change...
+setInterval(() => {
+    store.dispatch({ type: 'interval', now: new Date() });
+}, 10000);
+
+// and, of course, we might have other aspects of our implementation subscribed to the state
--- samples/redux-bot-es6/conversation.js
+++ samples/redux-bot-es6/conversation.js
@@ -1,6 +1,14 @@
 
 // require nothing:-)
 
+// Redux asks the application developer to think carefully about state
+// this has a lot of benefits - not least of which is testability (the store is pure function)
+
+// More details on Redux can be found here https://redux.js.org/
+
+// The Bot Framework contains helper code for dealing with conversation Activities
+// but for the benefit of the curious, here is some raw implementation...
+
 const echo = function (requestActivity) {
     const responseActivity = {
         type: 'message',
@@ -15,13 +23,26 @@ const echo = function (requestActivity) {
     return responseActivity;
 };
 
+// A Redux store is a pure function
+
 const store = function (state = [], action) {
     switch (action.type) {
+        // this state change came from the bot framework channel
         case 'message':
             state.push({ user: action.activity });
             state.push({ bot: echo(action.activity) });
-            return state;
+            break;
+        // this state change was generated interanlly in our bot logic
+        case 'interval': {
+            // a smarter implementation might keep tabs on the current conversation users
+            // however, here we are just looking at the tail of our conversation log 
+            const last = state[state.length - 1];
+            if (last && last.bot) {
+                state.push({ bot: Object.assign({}, last.bot, { text: 'The time is: ' + action.now.toUTCString() }) });
+            }
+        }
     }
+    return state;
 };
 
 module.exports = {
