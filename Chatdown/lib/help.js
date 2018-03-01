57ca975e2b39b3bb99b800d3f33e8317c3bbd8aa
Tom Laird-McConnell <tomlm@microsoft.com>
2018-02-28 17:50:49 -0800

    fix formatting to wrap with cli-table2

--- Chatdown/lib/help.js
+++ Chatdown/lib/help.js
@@ -1,14 +1,17 @@
-const Table = require('cli-table');
+const Table = require('cli-table2');
 module.exports = function () {
     process.stdout.write('\nChatdown cli tool used to parse chat dialogs into transcripts\n\n© 2018 Microsoft Corporation\n\nUsage:\n\n');
-    let [x] = process.stdout.getWindowSize();
+    let [x] = process.stdout.getWindowSize() ;
+    let left = 16;
+    let right = x-left-3; // 3 is for 3 vertical bar characters
     const table = new Table({
         head: ['Argument', 'Description'],
-        colWidths: [Math.floor(x * .25), Math.floor(x * .74)],
-        style: {head: ['green'], 'padding-left': 1, 'padding-right': 1}
+        colWidths: [left, right],
+        style: {head: ['green'], 'padding-left': 1, 'padding-right': 1},
+        wordWrap: true
     });
-    table.push({'--in [file] (optional)': ['[file] is the location of the chat file to parse. \nIf omitted, piping is assumed and stdin will be used.']});
-    table.push({'--out [file] )(optional)': ['[file] is the location transcript file to write. \nIf omitted, the transcript will be written to stdout.']});
+    table.push({'--in [file]': ['[file] is the location of the chat file to parse. \nIf omitted, piping is assumed and stdin will be used.']});
+    table.push({'--out [file]': ['[file] is the location transcript file to write. \nIf omitted, the transcript will be written to stdout.']});
     table.push({'--help': ['Prints this help info to the console.']});
     process.stdout.write(table.toString());
 };
