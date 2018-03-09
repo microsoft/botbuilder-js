d2638c1a53796d93b57a1bc3eb45f4a174e7642b
Justin Wilaby (Aditi Consulting LLC) <v-juwila@microsoft.com>
2018-03-09 15:24:04 -0800

    Unit tests for LUIS --help and LUIS --init commands

---
+++ LUIS/test/luis.cli.help.test.suite.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const path = require('path');
+const {exec} = require('child_process');
+const luis = path.resolve('../bin/luis');
+
+describe("The LUIS cli --help -h argument", () => {
+
+    it('should print the help contents when --help is used', done => {
+        exec(`node ${luis} --help`, (error, stdout) => {
+            assert(stdout.includes('Where <api group> is one of the following:'));
+            assert(stdout.includes('Where <action> is one of the following:'));
+            assert(stdout.includes('Arguments:'));
+            assert(stdout.includes('Global Arguments:'));
+            done();
+        });
+    });
+
+    it('should print help contents for an <api group>', done => {
+        exec(`node ${luis} apps --help`, (error, stdout) => {
+            assert(stdout.includes('Where <action> is one of the following:'));
+            assert(stdout.includes('Where <target> may be one of the following:'));
+            done();
+        });
+    });
+
+    it('should print help contents for a <target>', done => {
+        exec(`node ${luis} apps customprebuiltdomains -h`, (error, stdout) => {
+            assert(stdout.includes('Where <action> is one of the following:'));
+            done();
+        });
+    });
+
+    it('should print help contents for a <subtarget>', done => {
+        exec(`node ${luis} models compositeentities -h`, (error, stdout) => {
+            assert(stdout.includes('Where <action> is one of the following:'));
+            assert(stdout.includes('Where <subtarget> may be one of the following:'));
+            done();
+        });
+    });
+
+    it('should provide suggestions when the target does not exist', done => {
+        exec(`node ${luis} apps customprebuiltdomains -h`, (error, stdout) => {
+            assert(stdout.includes('Where <action> is one of the following:'));
+            done();
+        });
+    });
+
+    it('should provide suggestions when the target does not exist', done => {
+        exec(`node ${luis} models compositeentities -h`, (error, stdout) => {
+            assert(stdout.includes('compositeentitis does not exist. Did you mean one of these?'));
+            done();
+        });
+    });
+});
---
+++ LUIS/test/luis.cli.init.test.suite.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const fs = require('fs-extra');
+const path = require('path');
+const {spawn} = require('child_process');
+const luis = path.resolve('../bin/luis');
+
+describe('The LUIS cli --init argument', () => {
+    const rcPath = path.resolve('.luisrc');
+    beforeEach(async () => {
+        const rc = await fs.stat(rcPath);
+        if (rc) {
+            await fs.unlink(path.resolve('.luisrc'));
+        }
+    });
+
+    it('should prompt the user though the creation of the .luisrc and write the file', async () => {
+        const luisProcess = spawn('node', [luis, '--init'], {stdio: ['pipe', 'pipe', process.stderr]});
+        let msgCt = 0;
+        const appId = Math.floor(Math.random() * 9999999);
+        const versionId = Math.floor(Math.random() * 111111);
+        const location = Math.floor(Math.random() * 8888888);
+
+        await new Promise(resolve => {
+            luisProcess.stdout.on('data', data => {
+                const message = (msgCt++, data.toString());
+
+                switch (msgCt) {
+                    case 1:
+                        assert(message === '\nThis util will walk you through creating a .luisrc file\n\nPress ^C at any time to quit.\n\n');
+                        break;
+
+                    case 2:
+                        assert(message === 'Subscription key: ');
+                        luisProcess.stdin.write('abc123\r');
+                        break;
+
+                    case 3:
+                        assert(message === 'Region: ');
+                        luisProcess.stdin.write(`${location}\r`);
+                        break;
+
+                    case 4:
+                        assert(message === 'App ID (leave blank to use the --appId argument instead): ');
+                        luisProcess.stdin.write(`${appId}\r`);
+                        break;
+
+                    case 5:
+                        assert(message === 'Version ID (leave blank to use the --versionId argument instead): ');
+                        luisProcess.stdin.write(`${versionId}\r`);
+                        break;
+
+                    case 6:
+                        assert(message.includes('Does this look ok?'));
+                        luisProcess.stdin.write('yes\r');
+                        break;
+
+                    case 7:
+                        assert(fs.statSync(rcPath));
+                        resolve();
+                        break;
+                }
+            });
+        });
+
+        const rc = await fs.readJson(rcPath);
+        assert(rc.appId === '' + appId);
+        assert(rc.versionId === '' + versionId);
+        assert(rc.endpointBasePath === `https://${location}.api.cognitive.microsoft.com/luis/api/v2.0`);
+    });
+});
