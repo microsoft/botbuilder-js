876308bcd45055a15723120dda2dd900ba25acb5
Steven Ickman <stevenic@microsoft.com>
2018-03-10 11:23:08 -0800

    updated inside-middleware sample for dialogs

--- samples/dialogs/inside-middleware-ts/src/goodbyeMiddleware.ts
+++ samples/dialogs/inside-middleware-ts/src/goodbyeMiddleware.ts
@@ -1,53 +1,56 @@
-import { Middleware, Activity } from 'botbuilder';
-import { DialogSet, ConfirmPrompt, ListStyle } from 'botbuilder-dialogs';
-
-const GoodbyeStackName = 'goodbyeStack';
+import { Middleware, BotContext, ConversationState } from 'botbuilder';
+import { DialogSet, ConfirmPrompt } from 'botbuilder-dialogs';
 
 export class GoodbyeMiddleware implements Middleware {
-    public receiveActivity(context: BotContext, next: () => Promise<void>): Promise<void> {
-        // Ensure we only respond to messages
-        if (context.request.type !== 'message') {
-            return next();
-        }
+    private dialogs = new DialogSet();
 
-        // Intercept the message if we're prompting the user.
-        return dialogs.continue(context).then(() => {
-            // Ensure the utterance handled by an active dialog
-            if (!context.responded) {
-                // Check for user to say "goodbye"
-                const utterance = (context.request.text || '').trim().toLowerCase();
-                if (utterance === 'goodbye') {
-                    // Start confirmation dialog
-                    return dialogs.begin(context, 'confirmGoodbye');
+    constructor(private conversationState: ConversationState, private stackName = 'goodbyeStack') { 
+        // Add private dialogs for confirming goodbye
+        this.dialogs.add('confirmGoodbye', [
+            async function (dc) {
+                await dc.prompt('confirmPrompt', `This will end any active tasks. Are you sure?`);
+            },
+            async function (dc, value: boolean) {
+                if (value) {
+                    // Clear conversation state
+                    await dc.context.sendActivity(`Ok... Goodbye`);
+                    conversationState.clear(dc.context);
                 } else {
-                    return next();
+                    await dc.context.sendActivity(`Ok...`);
                 }
-            } else {
-                // Prevent further processing since we're in a dialog with the user.
-                return Promise.resolve();
+                return dc.end();
             }
-        });
+        ]);
+        
+        this.dialogs.add('confirmPrompt', new ConfirmPrompt());
+    }
+
+    public async onProcessRequest(context: BotContext, next: () => Promise<void>) {
+        if (context.request.type === 'message') {
+            // Create dialog context using our middlewares private dialog stack
+            const state = await this.conversationState.read(context);
+            if (!Array.isArray(state[this.stackName])) { state[this.stackName] = [] }
+            const dc = this.dialogs.createContext(context, state[this.stackName]);
+
+            // Intercept the message if we're prompting the user
+            await dc.continue();
+            if (context.responded) {
+                return;
+            }
+
+            // Check for user to say "goodbye"
+            const utterance = (context.request.text || '').trim().toLowerCase();
+            if (utterance === 'goodbye') {
+                // Start confirmation dialog
+                await dc.begin('confirmGoodbye');
+            } else {
+                // Let bot process request
+                await next();
+            }
+        } else {
+            // Let bot process request
+            await next();
+        }
     }
 }
 
-// Create dialogs for middleware. Uses its own namespace isolated stack to avoid collisions with 
-// the bot or other middleware.
-const dialogs = new DialogSet(GoodbyeStackName);
-
-dialogs.add('confirmGoodbye', [
-    function (context) {
-        return dialogs.prompt(context, 'confirmPrompt', `This will end any active tasks. Are you sure?`);
-    },
-    function (context, value) {
-        if (value) {
-            // Reset conversation state
-            context.reply(`Ok... Goodbye`);
-            context.state.conversation = {};
-        } else {
-            context.reply(`Ok...`);
-        }
-        return dialogs.end(context);
-    }
-]);
-
-dialogs.add('confirmPrompt', new ConfirmPrompt());
