6aac1f84b90f03d216dd1a475850c4c697f89963
Justin Wilaby (Aditi Consulting LLC) <v-juwila@microsoft.com>
2018-03-14 11:55:58 -0700

    Updated the tsconfig.json to prevent unneeded types from being pullen in

---
+++ libraries/botframework-connector/lib/src/generated/operations/index.js
@@ -0,0 +1,12 @@
+/**
+ * @module botbuilder
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+var attachments_1 = require("./attachments");
+exports.Attachments = attachments_1.Attachments;
+var conversations_1 = require("./conversations");
+exports.Conversations = conversations_1.Conversations;
+//# sourceMappingURL=index.js.map
