8124f4ce3da824ce17d99aaa00e10277a2fbc254
Steven Ickman <stevenic@microsoft.com>
2018-03-22 16:56:59 -0700

    added unit test for attachmentPrompt

--- libraries/botbuilder-prompts/tests/attachmentPrompt.test.js
+++ libraries/botbuilder-prompts/tests/attachmentPrompt.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { BotContext, TestAdapter } = require('botbuilder');
-const { createDatetimePrompt } = require('../lib');
+const { createAttachmentPrompt } = require('../lib');
 
 class TestContext extends BotContext {
     constructor(request) {
@@ -12,11 +12,11 @@ class TestContext extends BotContext {
     }
 }
 
-describe('DatetimePrompt', function() {
+describe('AttachmentPrompt', function() {
     this.timeout(5000);
 
     it('should create prompt.', function (done) {
-        const prompt = createDatetimePrompt();
+        const prompt = createAttachmentPrompt();
         assert(prompt, `Prompt not created.`);
         assert(prompt.prompt, `Prompt.prompt() not found.`);
         assert(prompt.recognize, `Prompt.recognize() not found.`);
@@ -25,7 +25,7 @@ describe('DatetimePrompt', function() {
 
     it('should send prompt().', function (done) {
         const context = new TestContext({ text: 'test', type: 'message' });
-        const prompt = createDatetimePrompt();
+        const prompt = createAttachmentPrompt();
         prompt.prompt(context, `test prompt`).then(() => {
             assert(Array.isArray(context.sent) && context.sent.length > 0, `prompt not sent.`);
             assert(context.sent[0].text === 'test prompt', `invalid prompt sent.`);
@@ -33,9 +33,9 @@ describe('DatetimePrompt', function() {
         });
     });
 
-    it('should recognize() a datetime.', function (done) {
-        const context = new TestContext({ text: `tomorrow at 9am`, type: 'message' });
-        const prompt = createDatetimePrompt();
+    it('should recognize() a attachment.', function (done) {
+        const context = new TestContext({ attachments: [{ contentType: 'foo' }], type: 'message' });
+        const prompt = createAttachmentPrompt();
         prompt.recognize(context).then((values) => {
             assert(Array.isArray(values), `values not an array.`);
             assert(values.length === 1, `reply not recognized.`);
@@ -45,7 +45,7 @@ describe('DatetimePrompt', function() {
 
     it('should NOT recognize() other text.', function (done) {
         const context = new TestContext({ text: `what was that?`, type: 'message' });
-        const prompt = createDatetimePrompt();
+        const prompt = createAttachmentPrompt();
         prompt.recognize(context).then((values) => {
             assert(values === undefined, `shouldn't have recognized.`);
             done();
@@ -54,8 +54,8 @@ describe('DatetimePrompt', function() {
     
     it('should call custom validator.', function (done) {
         let called = false;
-        const context = new TestContext({ text: `how about tomorrow at 9am?`, type: 'message' });
-        const prompt = createDatetimePrompt((context, values) => {
+        const context = new TestContext({ attachments: [{ contentType: 'foo' }], type: 'message' });
+        const prompt = createAttachmentPrompt((context, values) => {
             assert(Array.isArray(values), `values not an array.`);
             assert(values.length === 1, `reply not recognized.`);
             called = true;
@@ -71,7 +71,7 @@ describe('DatetimePrompt', function() {
     it('should handle an undefined request.', function (done) {
         let called = false;
         const context = new TestContext(undefined);
-        const prompt = createDatetimePrompt((context, values) => {
+        const prompt = createAttachmentPrompt((context, values) => {
             assert(values === undefined, `value shouldn't have been recognized.`);
             called = true;
             return undefined;
