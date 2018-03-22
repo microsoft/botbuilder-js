96a990787413ae6867d93cb5633762acb133c4af
Steven Ickman <stevenic@microsoft.com>
2018-03-22 16:51:31 -0700

    added unit test for dateTime prompt

--- libraries/botbuilder-prompts/tests/textPrompt.test.js
+++ libraries/botbuilder-prompts/tests/textPrompt.test.js
@@ -23,16 +23,27 @@ describe('TextPrompt', function() {
         done();
     });
 
-    it('should send prompt().', function (done) {
+    it('should send text prompt().', function (done) {
         const context = new TestContext({ text: 'test', type: 'message' });
         const prompt = createTextPrompt();
-        prompt.prompt(context, `test prompt`).then(() => {
+        prompt.prompt(context, `test text`, `test ssml`).then(() => {
             assert(Array.isArray(context.sent) && context.sent.length > 0, `prompt not sent.`);
-            assert(context.sent[0].text === 'test prompt', `invalid prompt sent.`);
+            assert(context.sent[0].text === 'test text', `invalid prompt text sent.`);
+            assert(context.sent[0].speak === 'test ssml', `invalid prompt speak sent.`);
             done();
         });
     });
 
+    it('should send activity based prompt().', function (done) {
+        const context = new TestContext({ text: 'test', type: 'message' });
+        const prompt = createTextPrompt();
+        prompt.prompt(context, { text: 'foo', type: 'message' }).then(() => {
+            assert(Array.isArray(context.sent) && context.sent.length > 0, `prompt not sent.`);
+            assert(context.sent[0].text === 'foo', `invalid prompt text sent.`);
+            done();
+        });
+    });
+    
     it('should recognize() any text.', function (done) {
         const context = new TestContext({ text: 'test', type: 'message' });
         const prompt = createTextPrompt();
