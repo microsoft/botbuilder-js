e20363d2a07ad420a27457b42ef7e941fd1ad40a
Mostafa ElAraby <mostafa00791@gmail.com>
2018-04-01 21:08:39 +0200

    Fixed LocaleConverter  failing Unit Tests (#155)
    * Added translator middleware
    
    * Added no translate patters and locale converter middleware
    
    * Added delegates to translator and locale converter middlewares
    
    * Code cleanup
    
    * Changed name to LocaleConverter
    
    * Added TranslatorSettings object
    
    * Fixed dependencies
    
    * Code cleanup
    
    * Added unit tests
    
    * Code cleanup
    
    * Fixed unit tests and code cleanup
    
    * Added LocaleConverterSettings object
    
    * Updated unit tests
    
    * Modified locale converter unit tests
    
    * Modified language translator test cases
    
    * Remove unnecessary logs
    
    * Removed unncessary .map files
    
    * Compiled with latest typescript version
    
    * Added samples, 100% code coverage
    
    * Added es6 samples
    
    * Minor modifications to language translator
    
    * Fixed timezone bug

--- libraries/botbuilder-ai/lib/localeConverter.js
+++ libraries/botbuilder-ai/lib/localeConverter.js
@@ -118,10 +118,10 @@ class MicrosoftLocaleConverter {
             let resolutionValues = result.resolution["values"][0];
             let type = result.typeName.replace('datetimeV2.', '');
             if (type.includes('date') && !type.includes('range')) {
-                moment = new Date(resolutionValues["value"] + 'Z');
+                moment = new Date(new Date(resolutionValues["value"]).getTime() + new Date().getTimezoneOffset() * 60 * 1000);
             }
             else if (type.includes('date') && type.includes('range')) {
-                moment = new Date(resolutionValues['start'] + 'Z');
+                moment = new Date(new Date(resolutionValues["start"]).getTime() + new Date().getTimezoneOffset() * 60 * 1000);
             }
             else {
                 moment = new Date();
