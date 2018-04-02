b8ec8f170e3560693aa6c5eb660feca1d1b0d9a1
Tom Laird-McConnell <tomlm@microsoft.com>
2018-04-02 11:03:17 -0700

    remove bin folder

--- MSBot/bin/msbot-connect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const program = require("commander");
-program
-    .command('azure', 'connect to Azure Bot Service')
-    .command('luis', 'connect to LUIS a service')
-    .command('qna', 'connect to QNA a service');
-program.parse(process.argv);
-//# sourceMappingURL=msbot-connect.js.map
