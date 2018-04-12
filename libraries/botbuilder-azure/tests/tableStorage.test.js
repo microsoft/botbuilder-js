e56b0b9b4fe47885fbc41a9985535d4cf8095272
Pablo Costantini <sixdemons@gmail.com>
2018-04-12 18:13:30 -0300

    Functional Tests

--- libraries/botbuilder-azure/tests/tableStorage.test.js
+++ libraries/botbuilder-azure/tests/tableStorage.test.js
@@ -1,129 +1,66 @@
 const assert = require('assert');
-const { TableStorage } = require('../');
+const { TableStorage, StoreItemContainer } = require('../');
 const azureStorage = require('azure-storage');
 var fs = require('fs');
 var child_process = require('child_process');
 
-class TestTableStorage extends TableStorage {
-    constructor(settings) {
-        super(settings);
-    }
-
-    testCreateTableService() {
-        return super.createTableService(this.settings.storageAccountOrConnectionString, this.settings.storageAccessKey, this.settings.host);
-    }
-
-    createTableService(accountOrConnectionString, accessKey, host) {
-        let data;
-        let nextTag = 0;
-        return {
-            createTableIfNotExistsAsync(table) {
-                if (!data) {
-                    data = {};
-                    return Promise.resolve({ isSuccessful: true, created: true, exists: false });
-                }                
-                return Promise.resolve({ isSuccessful: true, created: false, exists: true })
-            },
-
-            deleteTableIfExistsAsync(table) {
-                if (data) {
-                    data = undefined;
-                    return Promise.resolve(true);
-                } else {
-                    return Promise.resolve(false);
-                }
-            },
-        
-            retrieveEntityAsync(table, partitionKey, rowKey) {
-                assert(rowKey === '0');
-                if (data.hasOwnProperty(partitionKey)) {
-                    return Promise.resolve(Object.assign({}, data[partitionKey]));
-                } else {
-                    const err = new Error('not found');
-                    err.statusCode = 404;
-                    return Promise.reject(err);
-                }
-            },
-
-            replaceEntityAsync(table, entity) {
-                // Check eTag
-                assert(entity);
-                if (data.hasOwnProperty(entity.PartitionKey._)) {
-                    const etag = entity['.metadata'].etag;
-                    const cur = data[entity.PartitionKey._];
-                    if (etag === cur['.metadata'].etag) {
-                        const cpy = Object.assign({}, entity);
-                        cpy['.metadata'].etag = (nextTag++).toString();
-                        data[entity.PartitionKey._] = cpy;
-                        return Promise.resolve(cpy['.metadata']);
-                    } else {
-                        const err = new Error('conflict');
-                        err.statusCode = 409;
-                        return Promise.reject(err);
-                    }
-                } else {
-                    const err = new Error('not found');
-                    err.statusCode = 404;
-                    return Promise.reject(err);
-                }
-            },
-
-            insertOrReplaceEntityAsync(table, entity) {
-                const cpy = Object.assign({}, entity);
-                cpy['.metadata'] = { etag: (nextTag++).toString() };
-                data[entity.PartitionKey._] = cpy;
-                return Promise.resolve(cpy['.metadata']);
-            },
-
-            deleteEntityAsync(table, entity) {
-                assert(entity);
-                if (data.hasOwnProperty(entity.PartitionKey._)) {
-                    delete data[entity.PartitionKey._];
-                    return Promise.resolve();
-                } else {
-                    const err = new Error('not found');
-                    err.statusCode = 404;
-                    return Promise.reject(err);
-                }
-            }
-        };
-    }
-}
+const connectionString = 'DefaultEndpointsProtocol=https;AccountName=pcsandbox2tfynyz;AccountKey=sHj3+rD8HhWJrpA/TJicnnF18IEVh8aIuOUNNLLZZWBTFp7LBFfxocVQq6nOxDQ0G4KbnGoqKYiTr2MOvOUB7w==;EndpointSuffix=core.windows.net';
+// const connectionString = 'UseDevelopmentStorage=true';
+const testTableName = 'storagetests';
 
 testStorage = function () {
-    let connectionString = 'UseDevelopmentStorage=true';
-    /*
-    before(function () {
-        let emulatorPath = "c:/Program Files (x86)/Microsoft SDKs/Azure/Storage Emulator/azurestorageemulator.exe";
-        if (!fs.existsSync(emulatorPath)){
-            console.log('skipping azure storage tests because azure storage emulator is not installed');
-            this.skip();
-        }
-        else
-            child_process.spawnSync(emulatorPath, ['start']);
+    var storage = new TableStorage({ tableName: testTableName, storageAccountOrConnectionString: connectionString });
+
+    // cleanup
+    after((done) => {
+        var table = new azureStorage.TableService(connectionString);
+        table.deleteTableIfExists(testTableName, () => done());
     });
-    */
+
+    // check & start emulator
+    before(function () {
+        if (process.platform === 'win32') {
+            var emulatorPath = "c:/Program Files (x86)/Microsoft SDKs/Azure/Storage Emulator/azurestorageemulator.exe";
+            if (!fs.existsSync(emulatorPath)) {
+                console.log('skipping azure storage tests because azure storage emulator is not installed');
+                this.skip();
+            }
+            else {
+                child_process.spawnSync(emulatorPath, ['start']);
+            }
+        }
+    });
+
+    function handleError(err) {
+        if (err.code == 'ECONNREFUSED') {
+            console.log('skipping test because azure storage emulator is not running');
+        } else {
+            console.error('error', err);
+            assert(false, 'should not throw');
+        }
+    }
+
+    function randomString(size) {
+        var text = '';
+        var chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        for (var i = 0; i < size; i++) {
+            text += chars.charAt(Math.floor(chars.length * Math.random()));
+        }
+
+        return text;
+    }
 
     it('read of unknown key', function () {
-        let storage = new TestTableStorage({ tableName: 'unknown', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then(deleted => storage.read(['unk']))
+        return storage.read(['unk'])
             .then((result) => {
                 assert(result != null, 'result should be object');
                 assert(!result.unk, 'key should be undefined');
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
+            .catch(handleError);
     });
 
     it('key creation', function () {
-        let storage = new TestTableStorage({ tableName: 'keyCreate', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ keyCreate: { count: 1 } }))
+        return storage.write({ keyCreate: { count: 1 } })
             .then(() => storage.read(['keyCreate']))
             .then((result) => {
                 assert(result != null, 'result should be object');
@@ -131,18 +68,11 @@ testStorage = function () {
                 assert(result.keyCreate.count == 1, 'object should have count of 1');
                 assert(!result.eTag, 'ETag should be defined');
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, `should not throw: ${reason.toString()}`);
-            });
+            .catch(handleError);
     });
 
     it('key update', function () {
-        let storage = new TestTableStorage({ tableName: 'keyUpdate', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ keyUpdate: { count: 1 } }))
+        return storage.write({ keyUpdate: { count: 1 } })
             .then(() => storage.read(['keyUpdate']))
             .then((result) => {
                 result.keyUpdate.count = 2;
@@ -152,18 +82,12 @@ testStorage = function () {
                         assert(updated.keyUpdate.count == 2, 'object should be updated');
                         assert(updated.keyUpdate.eTag != result.keyUpdate.eTag, 'Etag should be updated on write');
                     });
-            }).catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, `should not throw: ${reason.toString()}`);
-            });
+            })
+            .catch(handleError);
     });
 
     it('invalid eTag', function () {
-        let storage = new TestTableStorage({ tableName: 'invalidETag', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ keyUpdate2: { count: 1 } }))
+        return storage.write({ keyUpdate2: { count: 1 } })
             .then(() => storage.read(['keyUpdate2']))
             .then((result) => {
                 result.keyUpdate2.count = 2;
@@ -174,18 +98,11 @@ testStorage = function () {
                         .catch((reason) => { });
                 });
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
+            .catch(handleError);
     });
 
     it('wildcard eTag', function () {
-        let storage = new TestTableStorage({ tableName: 'wildcards', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ keyUpdate3: { count: 1 } }))
+        return storage.write({ keyUpdate3: { count: 1 } })
             .then(() => storage.read(['keyUpdate3']))
             .then((result) => {
                 result.keyUpdate3.eTag = '*';
@@ -196,30 +113,16 @@ testStorage = function () {
                         .catch((reason) => assert(false, 'should NOT fail on etag writes with wildcard'));
                 });
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
+            .catch(handleError);
     });
 
     it('delete unknown', function () {
-        let storage = new TestTableStorage({ tableName: 'deleteUnknown', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.delete(['unknown']))
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not fail delete of unknown key');
-            });
+        return storage.delete(['unknown'])
+            .catch(handleError);
     });
 
     it('delete known', function () {
-        let storage = new TestTableStorage({ tableName: 'delete', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ delete1: { count: 1 } }))
+        return storage.write({ delete1: { count: 1 } })
             .then(() => storage.delete(['delete1']))
             .then(() => storage.read(['delete1']))
             .then(result => {
@@ -227,41 +130,15 @@ testStorage = function () {
                     console.log(JSON.stringify(result.delete1));
                 assert(!result.delete1, 'delete1 should not be found');
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
-
+            .catch(handleError);
     });
 
-    it('delete table after use', function () {
-        let storage = new TestTableStorage({ tableName: 'delete', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({ delete1: { count: 1 } }))
-            .then(() => storage.deleteTable(storage.settings.tableName))
-            .then((deleted) => {
-                assert(deleted, 'table not deleted');
-            })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
-
-    });
-
-    
     it('batch operations', function () {
-        let storage = new TestTableStorage({ tableName: 'batch', storageAccountOrConnectionString: connectionString });
-        return storage.deleteTable(storage.settings.tableName)
-            .then((deleted) => storage.write({
-                batch1: { count: 10 },
-                batch2: { count: 20 },
-                batch3: { count: 30 },
-            }))
+        return storage.write({
+            batch1: { count: 10 },
+            batch2: { count: 20 },
+            batch3: { count: 30 },
+        })
             .then(() => storage.read(['batch1', 'batch2', 'batch3']))
             .then((result) => {
                 assert(result.batch1 != null, 'batch1 should exist and doesnt');
@@ -281,19 +158,12 @@ testStorage = function () {
                 assert(!result.batch2, 'batch2 should not exist and does');
                 assert(!result.batch3, 'batch3 should not exist and does');
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
-
+            .catch(handleError);
     });
 
     it('crazy keys work', function () {
-        let storage = new TestTableStorage({ tableName: 'crazy', storageAccountOrConnectionString: connectionString });
-        let obj = {};
-        let crazyKey = '!@#$%^&*()_+??><":QASD~`';
+        var obj = {};
+        var crazyKey = '!@#$%^&*()_+??><":QASD~`';
         obj[crazyKey] = { count: 1 };
         return storage.write(obj)
             .then(() => storage.read([crazyKey]))
@@ -303,39 +173,19 @@ testStorage = function () {
                 assert(result[crazyKey].count == 1, 'object should have count of 1');
                 assert(result[crazyKey].eTag, 'ETag should be defined');
             })
-            .catch(reason => {
-                if (reason.code == 'ECONNREFUSED')
-                    console.log('skipping test because azure storage emulator is not running');
-                else
-                    assert(false, 'should not throw');
-            });
-
+            .catch(handleError);
     });
 
-    it('should create table storage client', function (done) {
-        let storage = new TestTableStorage({ tableName: 'client', storageAccountOrConnectionString: connectionString });
-        const client = storage.testCreateTableService();
-        assert(client, `client not created.`);
-        done();
-    });
-
-    it('should denodify() a callback based function', function (done) {
-        let storage = new TestTableStorage({ tableName: 'client' });
-        const fn = storage.denodeify(null, function (err, result, cb) {
-            cb(err, result);
-        });
-        fn(undefined, 'foo').then((result) => {
-            assert(result === 'foo', `result not passed through`);
-            return fn(new Error(`failed`), undefined).catch((err) => {
-                assert(err, `error not passed through.`);
-                done();
-            });
-        })
+    it('table name should be validated', function () {
+        var invalidSampleNames = ['test-test', '0001test', 'test_test', ''];
+        invalidSampleNames.forEach(invalidName =>
+            assert.throws(() =>
+                new TableStorage({ tableName: invalidName, storageAccountOrConnectionString: connectionString }),
+                (err) => err.message.includes('table name')));
     });
 }
 
-describe('TestTableStorage', function () {
-    this.timeout(10000);
+describe('TableStorage', function () {
+    this.timeout(40000);
     testStorage();
 });
-
