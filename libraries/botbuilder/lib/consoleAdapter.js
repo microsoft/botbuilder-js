824a6b449421070e59adb3e37eb0917747e1185c
Steven Ickman <stevenic@microsoft.com>
2018-04-12 21:10:44 -0700

    Flushed out reference docs for the ConsoleAdapter

--- libraries/botbuilder/lib/consoleAdapter.js
+++ libraries/botbuilder/lib/consoleAdapter.js
@@ -17,9 +17,17 @@ const readline = require("readline");
  * **Usage Example**
  *
  * ```JavaScript
+ * const adapter = new ConsoleAdapter();
+ * const closeFn = adapter.listen(async (context) => {
+ *    await context.sendActivity(`Hello World`);
+ * });
  * ```
  */
 class ConsoleAdapter extends botbuilder_core_1.BotAdapter {
+    /**
+     * Creates a new ConsoleAdapter instance.
+     * @param reference (Optional) reference used to customize the address information of activites sent from the adapter.
+     */
     constructor(reference) {
         super();
         this.nextId = 0;
@@ -32,7 +40,32 @@ class ConsoleAdapter extends botbuilder_core_1.BotAdapter {
         }, reference);
     }
     /**
-     * Begins listening to console input.
+     * Begins listening to console input. A function will be returned that can be used to stop the
+     * bot listening and therefore end the process.
+     *
+     * Upon receiving input from the console the flow is as follows:
+     *
+     * - An 'message' activity will be created containing the users input text.
+     * - A revokable `TurnContext` will be created for the activity.
+     * - The context will be routed through any middleware registered with [use()](#use).
+     * - The bots logic handler that was passed in will be executed.
+     * - The promise chain setup by the middleware stack will be resolved.
+     * - The context object will be revoked and any future calls to its members will result in a
+     *   `TypeError` being thrown.
+     *
+     * **Usage Example**
+     *
+     * ```JavaScript
+     * const closeFn = adapter.listen(async (context) => {
+     *    const utterance = context.activity.text.toLowerCase();
+     *    if (utterance.includes('goodbye')) {
+     *       await context.sendActivity(`Ok... Goodbye`);
+     *       closeFn();
+     *    } else {
+     *       await context.sendActivity(`Hello World`);
+     *    }
+     * });
+     * ```
      * @param logic Function which will be called each time a message is input by the user.
      */
     listen(logic) {
@@ -54,6 +87,30 @@ class ConsoleAdapter extends botbuilder_core_1.BotAdapter {
             rl.close();
         };
     }
+    /**
+     * Lets a bot proactively message the user.
+     *
+     * The processing steps for this method are very similar to [listen()](#listen)
+     * in that a `TurnContext` will be created which is then routed through the adapters middleware
+     * before calling the passed in logic handler. The key difference being that since an activity
+     * wasn't actually received it has to be created.  The created activity will have its address
+     * related fields populated but will have a `context.activity.type === undefined`.
+     *
+     * **Usage Example**
+     *
+     * ```JavaScript
+     * function delayedNotify(context, message, delay) {
+     *    const reference = TurnContext.getConversationReference(context.activity);
+     *    setTimeout(() => {
+     *       adapter.continueConversation(reference, async (ctx) => {
+     *          await ctx.sendActivity(message);
+     *       });
+     *    }, delay);
+     * }
+     * ```
+     * @param reference A `ConversationReference` saved during a previous message from a user.  This can be calculated for any incoming activity using `TurnContext.getConversationReference(context.activity)`.
+     * @param logic A function handler that will be called to perform the bots logic after the the adapters middleware has been run.
+     */
     continueConversation(reference, logic) {
         // Create context and run middleware pipe
         const activity = botbuilder_core_1.TurnContext.applyConversationReference({}, reference, true);
@@ -61,6 +118,15 @@ class ConsoleAdapter extends botbuilder_core_1.BotAdapter {
         return this.runMiddleware(context, logic)
             .catch((err) => { this.printError(err.toString()); });
     }
+    /**
+     * Logs a set of activities to the console.
+     *
+     * Calling `TurnContext.sendActivities()` or `TurnContext.sendActivity()` is the preferred way of
+     * sending activities as that will ensure that outgoing activities have been properly addressed
+     * and that any interested middleware has been notified.
+     * @param context Context for the current turn of conversation with the user.
+     * @param activities List of activities to send.
+     */
     sendActivities(context, activities) {
         const that = this;
         return new Promise((resolve, reject) => {
@@ -96,18 +162,38 @@ class ConsoleAdapter extends botbuilder_core_1.BotAdapter {
             next(0);
         });
     }
+    /**
+     * Not supported for the ConsoleAdapter.  Calling this method or `TurnContext.updateActivity()`
+     * will result an error being returned.
+     */
     updateActivity(context, activity) {
         return Promise.reject(new Error(`ConsoleAdapter.updateActivity(): not supported.`));
     }
+    /**
+     * Not supported for the ConsoleAdapter.  Calling this method or `TurnContext.deleteActivity()`
+     * will result an error being returned.
+     */
     deleteActivity(context, reference) {
         return Promise.reject(new Error(`ConsoleAdapter.deleteActivity(): not supported.`));
     }
+    /**
+     * Allows for mocking of the console interface in unit tests.
+     * @param options Console interface options.
+     */
     createInterface(options) {
         return readline.createInterface(options);
     }
+    /**
+     * Logs text to the console.
+     * @param line Text to print.
+     */
     print(line) {
         console.log(line);
     }
+    /**
+     * Logs an error to the console.
+     * @param line Error text to print.
+     */
     printError(line) {
         console.error(line);
     }
--- libraries/botbuilder/src/consoleAdapter.ts
+++ libraries/botbuilder/src/consoleAdapter.ts
@@ -16,12 +16,20 @@ import * as readline from 'readline';
  * **Usage Example**
  *
  * ```JavaScript
+ * const adapter = new ConsoleAdapter();
+ * const closeFn = adapter.listen(async (context) => {
+ *    await context.sendActivity(`Hello World`);
+ * });
  * ```
  */
 export class ConsoleAdapter extends BotAdapter {
     private nextId = 0;
     private readonly reference: ConversationReference;
 
+    /**
+     * Creates a new ConsoleAdapter instance.
+     * @param reference (Optional) reference used to customize the address information of activites sent from the adapter.
+     */
     constructor(reference?: ConversationReference) {
         super();
         this.reference = Object.assign({
@@ -34,7 +42,32 @@ export class ConsoleAdapter extends BotAdapter {
     }
 
     /**
-     * Begins listening to console input. 
+     * Begins listening to console input. A function will be returned that can be used to stop the
+     * bot listening and therefore end the process.
+     * 
+     * Upon receiving input from the console the flow is as follows:
+     * 
+     * - An 'message' activity will be created containing the users input text.
+     * - A revokable `TurnContext` will be created for the activity.
+     * - The context will be routed through any middleware registered with [use()](#use).
+     * - The bots logic handler that was passed in will be executed.
+     * - The promise chain setup by the middleware stack will be resolved.
+     * - The context object will be revoked and any future calls to its members will result in a
+     *   `TypeError` being thrown.
+     * 
+     * **Usage Example**
+     *
+     * ```JavaScript
+     * const closeFn = adapter.listen(async (context) => {
+     *    const utterance = context.activity.text.toLowerCase();
+     *    if (utterance.includes('goodbye')) {
+     *       await context.sendActivity(`Ok... Goodbye`);
+     *       closeFn();
+     *    } else {
+     *       await context.sendActivity(`Hello World`);
+     *    }
+     * });
+     * ```
      * @param logic Function which will be called each time a message is input by the user.
      */
     public listen(logic: (context: TurnContext) => Promiseable<void>): Function {
@@ -58,6 +91,30 @@ export class ConsoleAdapter extends BotAdapter {
         }
     }
 
+    /**
+     * Lets a bot proactively message the user.
+     * 
+     * The processing steps for this method are very similar to [listen()](#listen)
+     * in that a `TurnContext` will be created which is then routed through the adapters middleware 
+     * before calling the passed in logic handler. The key difference being that since an activity 
+     * wasn't actually received it has to be created.  The created activity will have its address 
+     * related fields populated but will have a `context.activity.type === undefined`.
+     * 
+     * **Usage Example**
+     *
+     * ```JavaScript
+     * function delayedNotify(context, message, delay) {
+     *    const reference = TurnContext.getConversationReference(context.activity);
+     *    setTimeout(() => {
+     *       adapter.continueConversation(reference, async (ctx) => {
+     *          await ctx.sendActivity(message);
+     *       });
+     *    }, delay);
+     * }
+     * ```
+     * @param reference A `ConversationReference` saved during a previous message from a user.  This can be calculated for any incoming activity using `TurnContext.getConversationReference(context.activity)`.
+     * @param logic A function handler that will be called to perform the bots logic after the the adapters middleware has been run.
+     */
     public continueConversation(reference: ConversationReference, logic: (context: TurnContext) => Promiseable<void>): Promise<void> {
             // Create context and run middleware pipe
             const activity = TurnContext.applyConversationReference({}, reference, true);
@@ -66,6 +123,15 @@ export class ConsoleAdapter extends BotAdapter {
                        .catch((err) => { this.printError(err.toString()) });
     }
 
+    /**
+     * Logs a set of activities to the console.
+     * 
+     * Calling `TurnContext.sendActivities()` or `TurnContext.sendActivity()` is the preferred way of
+     * sending activities as that will ensure that outgoing activities have been properly addressed 
+     * and that any interested middleware has been notified. 
+     * @param context Context for the current turn of conversation with the user.
+     * @param activities List of activities to send.
+     */
     public sendActivities(context: TurnContext, activities: Partial<Activity>[]): Promise<ResourceResponse[]> {
         const that = this;
         return new Promise((resolve, reject) => {
@@ -100,22 +166,42 @@ export class ConsoleAdapter extends BotAdapter {
         });
     }
 
+    /**
+     * Not supported for the ConsoleAdapter.  Calling this method or `TurnContext.updateActivity()`
+     * will result an error being returned.
+     */
     public updateActivity(context: TurnContext, activity: Partial<Activity>): Promise<void> {
         return Promise.reject(new Error(`ConsoleAdapter.updateActivity(): not supported.`));
     }
 
+    /**
+     * Not supported for the ConsoleAdapter.  Calling this method or `TurnContext.deleteActivity()`
+     * will result an error being returned.
+     */
     public deleteActivity(context: TurnContext, reference: Partial<ConversationReference>): Promise<void> {
         return Promise.reject(new Error(`ConsoleAdapter.deleteActivity(): not supported.`));
     }
 
+    /**
+     * Allows for mocking of the console interface in unit tests.
+     * @param options Console interface options.
+     */
     protected createInterface(options: readline.ReadLineOptions): readline.ReadLine {
         return readline.createInterface(options);
     }
 
+    /**
+     * Logs text to the console.
+     * @param line Text to print.
+     */
     protected print(line: string) {
         console.log(line);
     }
 
+    /**
+     * Logs an error to the console.
+     * @param line Error text to print.
+     */
     protected printError(line: string) {
         console.error(line);
     }
