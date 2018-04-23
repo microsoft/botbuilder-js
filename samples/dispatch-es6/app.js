aa37e6a7a014efd4df12ffbfa01e908fc8187be3
Steven Gum <steven.gum@gmail.com>
2018-04-23 11:04:17 -0700

    add dispatch-es6 sample with LUIS apps and QnAMaker KB

---
+++ samples/dispatch-es6/app.js
@@ -0,0 +1,134 @@
+const { BotFrameworkAdapter, ConversationState, MemoryStorage, TurnContext } = require('botbuilder');
+const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
+const { DialogSet } = require('botbuilder-dialogs');
+const restify = require('restify');
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Create LuisRecognizers and QnAMaker
+// The LUIS applications are public, meaning you can use your own subscription key to test the applications.
+// For QnAMaker, users are required to create their own knowledge base.
+// The exported LUIS applications and QnAMaker knowledge base can be found adjacent to this sample bot.
+
+// The corresponding LUIS application JSON is `dispatchSample.json`
+const dispatcher = new LuisRecognizer({
+    appId: '0b18ab4f-5c3d-4724-8b0b-191015b48ea9',
+    subscriptionKey: process.env.LUIS_SUBSCRIPTION_KEY,
+    serviceEndpoint: 'https://westus.api.cognitive.microsoft.com/',
+    verbose: true
+});
+
+// The corresponding LUIS application JSON is `homeautomation.json`
+const homeAutomation = new LuisRecognizer({
+    appId: 'c6d161a5-e3e5-4982-8726-3ecec9b4ed8d',
+    subscriptionKey: process.env.LUIS_SUBSCRIPTION_KEY,
+    serviceEndpoint: 'https://westus.api.cognitive.microsoft.com/',
+    verbose: true
+});
+
+// The corresponding LUIS application JSON is `weather.json`
+const weather = new LuisRecognizer({
+    appId: '9d0c9e9d-ce04-4257-a08a-a612955f2fb5',
+    subscriptionKey: process.env.LUIS_SUBSCRIPTION_KEY,
+    serviceEndpoint: 'https://westus.api.cognitive.microsoft.com/',
+    verbose: true
+});
+
+// The QnAMaker knowledge base used in this sample is `sampleKnowledgeBase.tsv`
+const faq = new QnAMaker({
+    knowledgeBaseId: process.env.QNAMAKER_KNOWLEDGE_BASE,
+    subscriptionKey: process.env.QNAMAKER_SUBSCRIPTION_KEY,
+    serviceEndpoint: 'https://westus.api.cognitive.microsoft.com/'
+});
+
+// create conversation state
+const conversationState = new ConversationState(new MemoryStorage());
+adapter.use(conversationState);
+
+// register some dialogs for usage with the LUIS apps that are being dispatched to
+const dialogs = new DialogSet();
+
+dialogs.add('HomeAutomation.TurnOff', [
+    async (dialogContext) => {
+        const state = conversationState.get(dialogContext.context);
+        state.homeAutomationTurnOff = state.homeAutomationTurnOff ? state.homeAutomationTurnOff + 1 : 1;
+        await dialogContext.context.sendActivity(`${state.homeAutomationTurnOff}: You reached the "HomeAutomation.TurnOff" dialog.`);
+
+        await dialogContext.end();
+    }
+]);
+
+dialogs.add('HomeAutomation.TurnOn', [
+    async (dialogContext) => {
+        const state = conversationState.get(dialogContext.context);
+        state.homeAutomationTurnOn = state.homeAutomationTurnOn ? state.homeAutomationTurnOn + 1 : 1;
+        await dialogContext.context.sendActivity(`${state.homeAutomationTurnOn}: You reached the "HomeAutomation.TurnOn" dialog.`);
+
+        await dialogContext.end();
+    }
+]);
+
+dialogs.add('Weather.GetForecast', [
+    async (dialogContext) => {
+        const state = conversationState.get(dialogContext.context);
+        state.weatherGetForecast = state.weatherGetForecast ? state.weatherGetForecast + 1 : 1;
+        await dialogContext.context.sendActivity(`${state.weatherGetForecast}: You reached the "Weather.GetForecast" dialog.`);
+
+        await dialogContext.end();
+    }
+]);
+
+dialogs.add('Weather.GetCondition', [
+    async (dialogContext) => {
+        const state = conversationState.get(dialogContext.context);
+        state.weatherGetCondition = state.weatherGetCondition ? state.weatherGetCondition + 1 : 1;
+        await dialogContext.context.sendActivity(`${state.weatherGetCondition}: You reached the "Weather.GetCondition" dialog.`);
+
+        await dialogContext.end();
+    }
+]);
+
+adapter.use(dispatcher);
+
+// Listen for incoming Activities
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        if (context.activity.type === 'message') {
+            const state = conversationState.get(context);
+            const dc = dialogs.createContext(context, state);
+
+            // Retrieve the LUIS results from our dispatcher LUIS application
+            const luisResults = dispatcher.get(context);
+
+            // Extract the top intent from LUIS and use it to select which LUIS application to dispatch to
+            const topIntent = LuisRecognizer.topIntent(luisResults);
+
+            await dc.continue();
+
+            switch (topIntent) {
+                case 'l_homeautomation':
+                    const topHomeAutoIntent = LuisRecognizer.topIntent(await homeAutomation.recognize(context));
+                    await dc.begin(topHomeAutoIntent);
+                    break;
+                case 'l_weather':
+                    const topWeatherIntent = LuisRecognizer.topIntent(await weather.recognize(context));
+                    await dc.begin(topWeatherIntent);
+                    break;
+                case 'q_faq':
+                    await faq.answer(context);
+                    break;
+            }
+        }
+    });
+});
+
