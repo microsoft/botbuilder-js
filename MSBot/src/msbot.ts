5ff5d2812465c822718d9941e5beab6bcf0bb063
Tom Laird-McConnell <tomlm@microsoft.com>
2018-04-24 12:27:29 -0700

    comment out export/clone

--- MSBot/src/msbot.ts
+++ MSBot/src/msbot.ts
@@ -19,11 +19,11 @@ program
 program
     .command('secret', 'set or clear the secret for a .bot file');
 
-program
-    .command('export', 'export all connected services');
+// program
+//     .command('export', 'export all connected services');
 
-program
-    .command('clone', 'create a new .bot file based on another .bot file');
+// program
+//     .command('clone', 'create a new .bot file based on another .bot file');
 
 program
     .command('connect <service>', 'connect to a resource (Luis/Qna/Azure/...) used by the bot');
