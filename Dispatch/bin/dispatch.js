c6135345dcd183c74c7525acab8c2e7f41f34049
Tien Suwandy <tiens@microsoft.com>
2018-04-29 20:29:59 -0700

    latest updates
    QnA download endpoint update, QnA service schema update, QnA file processing update (tsv->json)

--- Dispatch/bin/dispatch.js
+++ Dispatch/bin/dispatch.js
@@ -2,7 +2,11 @@
 let args = '';
 process.argv.forEach((val, index) => {
   if (index > 1) {
-    args = args + ' ' + val;
+      args = args + ' "' + val + '"';
   }
 });
-require('child_process').execSync('dotnet ' + __dirname + '/netcoreapp2.0/Dispatch.dll ' + args, {stdio:[0,1,2]});
+try {
+    require('child_process').execSync('dotnet ' + __dirname + '/netcoreapp2.0/Dispatch.dll ' + args, { stdio: [0, 1, 2] });
+}
+catch (err) {
+}
