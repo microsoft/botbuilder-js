bd935421593cae96d1f552ab91d5d881ff650451
Bruce Haley <v-brhale@microsoft.com>
2018-05-08 15:41:58 -0700

    Polish PublishEmbedML.ps1

--- Build/PublishEmbedML.ps1
+++ Build/PublishEmbedML.ps1
@@ -7,30 +7,27 @@ param
     [string]$newBranchName
 )
 
-# Get name of Build Artifacts folder 
-#$folder = Get-ChildItem $artifactsPath -Directory | Select-Object -ExpandProperty Name
-#if($folder -is [system.array]) {
-#    $folder = $folder[0]
-#}
-Write-Host "Copying files from $artifactsPath"
+Write-Host "Copying files from" ($artifactsPath + '\EmbedML*\**')
 
-Set-PSDebug -Trace 1
 # Set default identity
 git config --global user.email "v-brhale@micrsoft.com"
 git config --global user.name "BruceHaley"
 
+Set-PSDebug -Trace 1
 git checkout master
 git pull origin master
 git checkout -b $newBranchName master
 git status
-# Add-Content '.\testfile.txt' 'The Test Message'
-Remove-Item '.\Dispatch\bin\netcoreapp2.0\*.*' -Force
+Set-PSDebug -Trace 0
+
+Remove-Item -Force -Verbose '.\Dispatch\bin\netcoreapp2.0\*.*'
 Copy-item -Force -Verbose ($artifactsPath + '\EmbedML*\**') -Destination '.\Dispatch\bin\netcoreapp2.0'
+
+Set-PSDebug -Trace 1
 git add .
 git add -u
 git status
 git commit -m "Push signed EmbedML files."
-#git push origin embedml-signed-$(Build.BuildNumber)
+git push origin $newBranchName
 git status
-
 Set-PSDebug -Trace 0
