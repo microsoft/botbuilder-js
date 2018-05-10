6a9c85a32b64590e53de6f40542207c0a04dcc28
Bruce Haley <v-brhale@microsoft.com>
2018-05-10 14:05:59 -0700

    Debug...

--- Build/PublishEmbedML.ps1
+++ Build/PublishEmbedML.ps1
@@ -27,8 +27,8 @@ git add -u
 $result = git status
 Write-Host "git status result: [$result]"
 
-Add-Content 'buildsummary.md' "1)Published to GitHub at https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0"
-Write-Host "##vso[task.uploadsummary]buildsummary.md"
+Add-Content "$PSScriptRoot\buildsummary.md" "1)Published to GitHub at https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0"
+Write-Host "##vso[task.uploadsummary]$PSScriptRoot\buildsummary.md"
 Write-Host "##vso[task.logissue type=warning;] 2)Published to GitHub at https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0"
 
 if ($result.StartsWith('nothing to commit') -eq $true) {
