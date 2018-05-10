6337e78fe5074459349be420e07401e36c5a9579
Bruce Haley <v-brhale@microsoft.com>
2018-05-10 15:10:11 -0700

    Debug

--- Build/PublishEmbedML.ps1
+++ Build/PublishEmbedML.ps1
@@ -27,7 +27,7 @@ git add -u
 $result = git status
 Write-Host "git status result: [$result]"
 
-Add-Content -Path ("$artifactsPath\Results") -Value ("Dispatch tool published to: <https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0>")
+Add-Content -Path "$artifactsPath\Results" -Value "Dispatch tool published to: [https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0](https://github.com/Microsoft/botbuilder-tools/tree/$newBranchName/Dispatch/bin/netcoreapp2.0)"
 Write-Host "##vso[task.uploadsummary] $artifactsPath\Results"
 
 if ($result.StartsWith('nothing to commit') -eq $true) {
