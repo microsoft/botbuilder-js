651c63bd3e5b7cbfa4f2f60a147c91a0a3197c43
Bruce Haley <v-brhale@microsoft.com>
2018-05-21 10:58:47 -0700

    Fix

--- Build/PushBitsToGithub.ps1
+++ Build/PushBitsToGithub.ps1
@@ -8,8 +8,6 @@ param
     [string]$repoDestinationPath    #Dispatch/bin/netcoreapp2.0
 )
 
-Write-Host "Copying files from" ($sourcePath + '/*/**')
-
 # Set default identity
 git config --global user.email "v-brhale@micrsoft.com"
 git config --global user.name "BruceHaley"
@@ -18,8 +16,8 @@ git checkout master
 git pull origin master
 git checkout -b $newBranchName master
 
-Write-Host "'Deleting the old files from ./$repoDestinationPath"
-Remove-Item -Force './$repoDestinationPath/*.*'
+Write-Host "Deleting the old files from ./$repoDestinationPath"
+Remove-Item -Force ./$repoDestinationPath/*.*
 Write-Host "Copying the new files from $sourcePath/*/** to ./$repoDestinationPath"
 Copy-item -Force $sourcePath/*/** -Destination ./$repoDestinationPath -Verbose
 
@@ -32,7 +30,7 @@ if ($result.StartsWith('nothing to commit') -eq $true) {
     Write-Host "##vso[task.logissue type=error;] Quit without publishing: Everything up-to-date. Looks like these bits are already in GitHub."
     throw;
 }
-git commit -m "Automated commit: new release of dispatch tool"
+git commit -m "Automated commit: new botbuilder tool release"
 git push origin $newBranchName
 
 # Display a Results section on the build summary page
