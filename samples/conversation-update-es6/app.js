0ffe62272d1a92f83cd69d04edcf8286f0e9b704
Pablo Costantini <sixdemons@gmail.com>
2018-05-29 14:13:30 -0300

    ConversationUpdate sample - Test using transcript file

--- samples/conversation-update-es6/app.js
+++ samples/conversation-update-es6/app.js
@@ -4,6 +4,8 @@
 const { BotFrameworkAdapter } = require('botbuilder');
 const restify = require('restify');
 
+const botLogic = require('./bot');
+
 // Create server
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -11,19 +13,12 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create adapter
-const adapter = new BotFrameworkAdapter({ 
-    appId: process.env.MICROSOFT_APP_ID, 
-    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for incoming requests 
+// Listen for incoming requests
 server.post('/api/messages/', (req, res) => {
-    adapter.processActivity(req, res, (context) => {
-        // On "conversationUpdate"-type activities this bot will send a greeting message to users joining the conversation.
-        if (context.activity.type === 'conversationUpdate' && context.activity.membersAdded[0].name !== 'Bot') {
-            await context.sendActivity(`Hello "${context.activity.membersAdded[0].name}"!`);
-        } else if (context.activity.type === 'message') {
-            await context.sendActivity(`Welcome to the conversationUpdate-bot! On a "conversationUpdate"-type activity, this bot will greet new users.`);
-        }
-    });
+    adapter.processActivity(req, res, botLogic);
 });
