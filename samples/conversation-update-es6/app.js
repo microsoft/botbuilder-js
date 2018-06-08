727ca3f0e3cfb5735e1eae987a8b4a268420bd20
Pablo Costantini <pcostantini@users.noreply.github.com>
2018-06-08 16:14:16 -0300

    Sample Fixes (#263)
    * Sample dependencies fixes
    
    * Missing async on TurnContext function handler

--- samples/conversation-update-es6/app.js
+++ samples/conversation-update-es6/app.js
@@ -20,5 +20,12 @@ const adapter = new BotFrameworkAdapter({
 
 // Listen for incoming requests
 server.post('/api/messages/', (req, res) => {
-    adapter.processActivity(req, res, botLogic);
+    adapter.processActivity(req, res, async (context) => {
+        // On "conversationUpdate"-type activities this bot will send a greeting message to users joining the conversation.
+        if (context.activity.type === 'conversationUpdate' && context.activity.membersAdded[0].name !== 'Bot') {
+            await context.sendActivity(`Hello "${context.activity.membersAdded[0].name}"!`);
+        } else if (context.activity.type === 'message') {
+            await context.sendActivity(`Welcome to the conversationUpdate-bot! On a "conversationUpdate"-type activity, this bot will greet new users.`);
+        }
+    });
 });
