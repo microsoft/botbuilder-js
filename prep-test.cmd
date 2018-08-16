1b7e06805ea929f971e91af87acc993c24e07b40
Chris McConnell <chrimc@microsoft.com>
2018-08-16 15:35:24 -0700

    Update prep script to not be dependent on Microsoft file shares and to start cosmos db.

--- prep-test.cmd
+++ prep-test.cmd
@@ -1,11 +1,30 @@
 @echo off
-echo setting test keys
-call \\fusebox\private\sdk\UnitTestKeys.cmd
-echo starting storage emulator
-@if exist "%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe" (
-  "%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe" start
+setlocal
+
+set keys="\\fusebox\private\sdk\UnitTestKeys.cmd"
+if exist %keys% (
+  echo Setting internal microsoft keys--ensure tests can run without them
+  call %keys%
+) 
+
+set storage="%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe"
+if exist %storage% (
+  echo Starting storage emulator 
+  %storage% start
 ) else (
-  @echo You don't have Azure Storage Emulator installed
-  @echo go to https://go.microsoft.com/fwlink/?LinkId=717179 to download and install 
+  echo You don't have Azure Storage Emulator installed
+  echo go to https://go.microsoft.com/fwlink/?LinkId=717179 to download and install 
 )
+
+set cosmos="%ProgramFiles%\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe"
+if exist %cosmos% (
+  echo Starting cosmos db emulator
+  %cosmos% /NoUI /NoExplorer
+) else (
+  echo You don't have Azure Cosmos DB Emulator installed
+  echo go to https://aka.ms/cosmosdb-emulator to download and install
+)
+endlocal
+
+
 
