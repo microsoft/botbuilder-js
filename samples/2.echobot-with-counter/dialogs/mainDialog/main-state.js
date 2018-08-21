6f410966f566f9dd0ad22cb5a69a13c7eff14716
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-08-20 19:53:08 -0700

    Updates to state.

--- samples/2.echobot-with-counter/dialogs/mainDialog/main-state.js
+++ samples/2.echobot-with-counter/dialogs/mainDialog/main-state.js
@@ -9,6 +9,27 @@ class TurnCounter {
         if(!state || !state.createProperty) throw('Invalid state provided. Need either converesation or user state');
         this.count = state.createProperty(TURN_COUNTER);
     }
+
+    /**
+     * 
+     * @param {object} context context object
+     */
+    async get(context) {
+        if(!context) throw ('Invalid context provided');
+        return await this.count.get(context);
+    }
+    /**
+     * 
+     * @param {object} context context object
+     * @param {number} value new count value to set
+     */
+    async set(context, value) {
+        if(!context) throw ('Invalid context provided');
+        if(isNaN(value)) throw ('Can only take numbers for TurnCounter state')
+        // do any validations on value before grounding
+        if(!value || value < 0) throw ('Invalid value for count');
+        return this.count.set(context, value);
+    }
 }
 
 module.exports = TurnCounter;
--- samples/2.echobot-with-counter/dialogs/mainDialog/main.js
+++ samples/2.echobot-with-counter/dialogs/mainDialog/main.js
@@ -1,18 +1,17 @@
 const mainState = require('./main-state');
-//const TURN_COUNTER = 'turnCounter';
+
 class MainDialog {
     constructor (conversationState) {
-        this.turnCounter = new mainState(conversationState);
-        //this.turnCounter = conversationState.createProperty(TURN_COUNTER);
+        this.state = new mainState(conversationState);
     }
     async onTurn(context) {
         if (context.activity.type === 'message') {
             // read from state.
-            let count = await this.turnCounter.count.get(context);
+            let count = await this.state.get(context);
             count = count === undefined ? 0 : count;
             await context.sendActivity(`${count}: You said "${context.activity.text}"`);
             // increment and set turn counter.
-            this.turnCounter.count.set(context, ++count);
+            this.state.set(context, ++count);
         }
         else {
             await context.sendActivity(`[${context.activity.type} event detected]`);
