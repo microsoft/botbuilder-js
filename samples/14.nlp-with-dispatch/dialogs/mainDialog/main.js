072f2f7a982e36225b6210bd297c09f1aa6daf82
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-08-21 20:35:09 -0700

    updates to readme

---
+++ samples/14.nlp-with-dispatch/dialogs/mainDialog/main.js
@@ -0,0 +1,22 @@
+const mainState = require('./mainState');
+
+class MainDialog {
+    constructor (conversationState) {
+        this.state = new mainState(conversationState);
+    }
+    async onTurn(context) {
+        if (context.activity.type === 'message') {
+            // read from state.
+            let count = await this.state.get(context);
+            count = count === undefined ? 0 : count;
+            await context.sendActivity(`${count}: You said "${context.activity.text}"`);
+            // increment and set turn counter.
+            this.state.set(context, ++count);
+        }
+        else {
+            await context.sendActivity(`[${context.activity.type} event detected]`);
+        }
+    }
+}
+
+module.exports = MainDialog;
