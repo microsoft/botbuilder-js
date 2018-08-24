68c9c575e206883740fc6f695619bc9e1fbe399d
Ben Brown <ben@xoxco.com>
2018-08-24 15:36:27 -0500

    linter fixes for choiceFactory

--- libraries/botbuilder-dialogs/src/choices/choiceFactory.ts
+++ libraries/botbuilder-dialogs/src/choices/choiceFactory.ts
@@ -6,12 +6,12 @@
  * Licensed under the MIT License.
  */
 
-import { MessageFactory, TurnContext, ActionTypes, InputHints, Activity, CardAction } from 'botbuilder-core';
-import { Choice } from './findChoices';
+import { ActionTypes, Activity, CardAction, InputHints, MessageFactory, TurnContext } from 'botbuilder-core';
 import * as channel from './channel';
+import { Choice } from './findChoices';
 
 /**
- * Additional options used to tweak the formatting of choice lists. 
+ * Additional options used to tweak the formatting of choice lists.
  */
 export interface ChoiceFactoryOptions {
     /**
@@ -24,7 +24,7 @@ export interface ChoiceFactoryOptions {
      * (Optional) separator inserted between the choices when their are only 2 choices. The default
      * value is `" or "`.
      */
-    inlineOr?: string
+    inlineOr?: string;
 
     /**
      * (Optional) separator inserted between the last 2 choices when their are more than 2 choices.
@@ -34,23 +34,23 @@ export interface ChoiceFactoryOptions {
 
     /**
      * (Optional) if `true`, inline and list style choices will be prefixed with the index of the
-     * choice as in "1. choice". If `false`, the list style will use a bulleted list instead. The 
+     * choice as in "1. choice". If `false`, the list style will use a bulleted list instead. The
      * default value is `true`.
      */
     includeNumbers?: boolean;
 }
 
 /**
- * A set of utility functions to assist with the formatting a 'message' activity containing a list 
- * of choices. 
+ * A set of utility functions to assist with the formatting a 'message' activity containing a list
+ * of choices.
  *
  * @remarks
- * This example shows creating a message containing a list of choices that has been conditionally 
- * formatted based on the capabilities of the underlying channel: 
+ * This example shows creating a message containing a list of choices that has been conditionally
+ * formatted based on the capabilities of the underlying channel:
  *
  * ```JavaScript
  * const { ChoiceFactory } = require('botbuilder-choices');
- * 
+ *
  * const message = ChoiceFactory.forChannel(context, ['red', 'green', 'blue'], `Pick a color.`);
  * await context.sendActivity(message);
  * ```
@@ -58,15 +58,15 @@ export interface ChoiceFactoryOptions {
 export class ChoiceFactory {
 
     /**
-     * Returns a 'message' activity containing a list of choices that has been automatically 
-     * formatted based on the capabilities of a given channel. 
-     * 
+     * Returns a 'message' activity containing a list of choices that has been automatically
+     * formatted based on the capabilities of a given channel.
+     *
      * @remarks
-     * The algorithm prefers to format the supplied list of choices as suggested actions but can 
-     * decide to use a text based list if suggested actions aren't natively supported by the 
-     * channel, there are too many choices for the channel to display, or the title of any choice 
-     * is too long.  
-     * 
+     * The algorithm prefers to format the supplied list of choices as suggested actions but can
+     * decide to use a text based list if suggested actions aren't natively supported by the
+     * channel, there are too many choices for the channel to display, or the title of any choice
+     * is too long.
+     *
      * If the algorithm decides to use a list it will use an inline list if there are 3 or less
      * choices and all have short titles. Otherwise, a numbered list is used.
      *
@@ -83,27 +83,34 @@ export class ChoiceFactory {
      * @param speak (Optional) SSML to speak for the message.
      * @param options (Optional) formatting options to use when rendering as a list.
      */
-    static forChannel(channelOrContext: string|TurnContext, choices: (string|Choice)[], text?: string, speak?: string, options?: ChoiceFactoryOptions): Partial<Activity> {
-        const channelId = typeof channelOrContext === 'string' ? channelOrContext : channel.getChannelId(channelOrContext);
+    public static forChannel(
+        channelOrContext: string|TurnContext,
+        choices: (string|Choice)[],
+        text?: string,
+        speak?: string,
+        options?: ChoiceFactoryOptions
+    ): Partial<Activity> {
+
+        const channelId: string = typeof channelOrContext === 'string' ? channelOrContext : channel.getChannelId(channelOrContext);
 
         // Normalize choices
-        const list = ChoiceFactory.toChoices(choices);
+        const list: Choice[] = ChoiceFactory.toChoices(choices);
 
         // Find maximum title length
-        let maxTitleLength = 0;
-        list.forEach((choice) => {
-            let l = choice.action && choice.action.title ? choice.action.title.length : choice.value.length;
+        let maxTitleLength: number = 0;
+        list.forEach((choice: Choice) => {
+            const l: number = choice.action && choice.action.title ? choice.action.title.length : choice.value.length;
             if (l > maxTitleLength) {
                 maxTitleLength = l;
             }
         });
 
         // Determine list style
-        const supportsSuggestedActions = channel.supportsSuggestedActions(channelId, choices.length);
-        const supportsCardActions = channel.supportsCardActions(channelId, choices.length);
-        const maxActionTitleLength = channel.maxActionTitleLength(channelId);
-        const hasMessageFeed = channel.hasMessageFeed(channelId);
-        const longTitles = maxTitleLength > maxActionTitleLength;
+        const supportsSuggestedActions: boolean = channel.supportsSuggestedActions(channelId, choices.length);
+        const supportsCardActions: boolean = channel.supportsCardActions(channelId, choices.length);
+        const maxActionTitleLength: number = channel.maxActionTitleLength(channelId);
+        const hasMessageFeed: boolean = channel.hasMessageFeed(channelId);
+        const longTitles: boolean = maxTitleLength > maxActionTitleLength;
         if (!longTitles && (supportsSuggestedActions || (!hasMessageFeed && supportsCardActions))) {
             // We always prefer showing choices using suggested actions. If the titles are too long, however,
             // we'll have to show them as a text list.
@@ -119,8 +126,8 @@ export class ChoiceFactory {
 
     /**
      * Returns a 'message' activity containing a list of choices that has been formatted as an
-     * inline list.  
-     * 
+     * inline list.
+     *
      * @remarks
      * This example generates a message text of "Pick a color: (1. red, 2. green, or 3. blue)":
      *
@@ -133,23 +140,25 @@ export class ChoiceFactory {
      * @param speak (Optional) SSML to speak for the message.
      * @param options (Optional) formatting options to tweak rendering of list.
      */
-    static inline(choices: (string|Choice)[], text?: string, speak?: string, options?: ChoiceFactoryOptions): Partial<Activity> {
-        const opt = Object.assign({
+    public static inline(choices: (string|Choice)[], text?: string, speak?: string, options?: ChoiceFactoryOptions): Partial<Activity> {
+        const opt: ChoiceFactoryOptions = {
             inlineSeparator: ', ',
             inlineOr: ' or ',
             inlineOrMore: ', or ',
-            includeNumbers: true
-        } as ChoiceFactoryOptions, options);
+            includeNumbers: true,
+            ...options
+        } as ChoiceFactoryOptions;
 
         // Format list of choices
-        let connector = '';
-        let txt = (text || '');
+        let connector: string = '';
+        let txt: string = (text || '');
         txt += ' ';
         ChoiceFactory.toChoices(choices).forEach((choice: any, index: number) => {
-            const title = choice.action && choice.action.title ? choice.action.title : choice.value;
+            const title: string = choice.action && choice.action.title ? choice.action.title : choice.value;
+            // tslint:disable-next-line:prefer-template
             txt += `${connector}${opt.includeNumbers ? '(' + (index + 1).toString() + ') ' : ''}${title}`;
-            if (index == (choices.length - 2)) {
-                connector = (index == 0 ? opt.inlineOr : opt.inlineOrMore) || '';
+            if (index === (choices.length - 2)) {
+                connector = (index === 0 ? opt.inlineOr : opt.inlineOrMore) || '';
             } else {
                 connector = opt.inlineSeparator || '';
             }
@@ -163,7 +172,7 @@ export class ChoiceFactory {
     /**
      * Returns a 'message' activity containing a list of choices that has been formatted as an
      * numbered or bulleted list.
-     * 
+     *
      * @remarks
      * This example generates a message with the choices presented as a numbered list:
      *
@@ -176,18 +185,20 @@ export class ChoiceFactory {
      * @param speak (Optional) SSML to speak for the message.
      * @param options (Optional) formatting options to tweak rendering of list.
      */
-    static list(choices: (string|Choice)[], text?: string, speak?: string, options?: ChoiceFactoryOptions): Partial<Activity> {
-        const opt = Object.assign({
-            includeNumbers: true
-        } as ChoiceFactoryOptions, options);
+    public static list(choices: (string|Choice)[], text?: string, speak?: string, options?: ChoiceFactoryOptions): Partial<Activity> {
+        const opt: ChoiceFactoryOptions = {
+            includeNumbers: true,
+            ...options
+        } as ChoiceFactoryOptions;
 
         // Format list of choices
-        let connector = '';
-        let txt = (text || '');
+        let connector: string = '';
+        let txt: string = (text || '');
         txt += '\n\n   ';
         ChoiceFactory.toChoices(choices).forEach((choice: any, index: number) => {
-            const title = choice.action && choice.action.title ? choice.action.title : choice.value;
-            txt += `${connector}${opt.includeNumbers ? (index + 1).toString() + '. ': '- '}${title}`;
+            const title: string = choice.action && choice.action.title ? choice.action.title : choice.value;
+            // tslint:disable-next-line:prefer-template
+            txt += `${connector}${opt.includeNumbers ? (index + 1).toString() + '. ' : '- '}${title}`;
             connector =  '\n   ';
         });
 
@@ -198,7 +209,7 @@ export class ChoiceFactory {
     /**
      * Returns a 'message' activity containing a list of choices that have been added as suggested
      * actions.
-     * 
+     *
      * @remarks
      * This example generates a message with the choices presented as suggested action buttons:
      *
@@ -210,13 +221,13 @@ export class ChoiceFactory {
      * @param text (Optional) text of the message.
      * @param speak (Optional) SSML to speak for the message.
      */
-    static suggestedAction(choices: (string|Choice)[], text?: string, speak?: string): Partial<Activity> {
+    public static suggestedAction(choices: (string|Choice)[], text?: string, speak?: string): Partial<Activity> {
         // Map choices to actions
-        const actions = ChoiceFactory.toChoices(choices).map<CardAction>((choice) => {
+        const actions: CardAction[] = ChoiceFactory.toChoices(choices).map<CardAction>((choice: Choice) => {
             if (choice.action) {
                 return choice.action;
             } else {
-                return { type: ActionTypes.ImBack, value: choice.value, title: choice.value }
+                return { type: ActionTypes.ImBack, value: choice.value, title: choice.value };
             }
         });
 
@@ -226,7 +237,7 @@ export class ChoiceFactory {
 
     /**
      * Takes a mixed list of `string` and `Choice` based choices and returns them as a `Choice[]`.
-     * 
+     *
      * @remarks
      * This example converts a simple array of string based choices to a properly formated `Choice[]`.
      *
@@ -235,7 +246,11 @@ export class ChoiceFactory {
      * ```
      * @param choices List of choices to add.
      */
-    static toChoices(choices: (string|Choice)[]|undefined): Choice[] {
-        return (choices || []).map((choice) => typeof choice === 'string' ? { value: choice } : choice).filter((choice) => choice);
+    public static toChoices(choices: (string|Choice)[]|undefined): Choice[] {
+        return (choices || []).map(
+            (choice: Choice) => typeof choice === 'string' ? { value: choice } : choice
+        ).filter(
+            (choice: Choice) => choice
+        );
     }
 }
