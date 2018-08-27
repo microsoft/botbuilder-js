fc0f78c436cafa3076106c808b0fd54e5725131c
Ben Brown <ben@xoxco.com>
2018-08-27 12:37:13 -0500

    linter fixes for botframework-connnector/auth files

--- libraries/botframework-connector/src/auth/openIdMetadata.ts
+++ libraries/botframework-connector/src/auth/openIdMetadata.ts
@@ -6,12 +6,12 @@
  * Licensed under the MIT License.
  */
 import * as request from 'request';
-var getPem = require('rsa-pem-from-mod-exp');
-var base64url = require('base64url');
+const getPem: any = import('rsa-pem-from-mod-exp');
+const base64url: any = import('base64url');
 
 export class OpenIdMetadata {
     private url: string;
-    private lastUpdated = 0;
+    private lastUpdated: number = 0;
     private keys: IKey[];
 
     constructor(url: string) {
@@ -19,11 +19,11 @@ export class OpenIdMetadata {
     }
 
     public getKey(keyId: string): Promise<IOpenIdMetadataKey | null> {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve: any, reject: any): void => {
             // If keys are more than 5 days old, refresh them
-            var now = new Date().getTime();
+            const now: number = new Date().getTime();
             if (this.lastUpdated < (now - 1000 * 60 * 60 * 24 * 5)) {
-                this.refreshCache((err) => {
+                this.refreshCache((err: any): void => {
                     if (err) {
                         //logger.error('Error retrieving OpenId metadata at ' + this.url + ', error: ' + err.toString());
                         // fall through and return cached key on error
@@ -31,51 +31,51 @@ export class OpenIdMetadata {
                     }
 
                     // Search the cache even if we failed to refresh
-                    var key = this.findKey(keyId);
+                    const key: IOpenIdMetadataKey = this.findKey(keyId);
                     resolve(key);
                 });
             } else {
                 // Otherwise read from cache
-                var key = this.findKey(keyId);
+                const key: IOpenIdMetadataKey = this.findKey(keyId);
                 resolve(key);
             }
         });
     }
 
     private refreshCache(cb: (err: Error) => void): void {
-        var options: request.Options = {
+        const options: request.Options = {
             method: 'GET',
             url: this.url,
             json: true
         };
 
-        request(options, (err, response, body) => {
+        request(options, (err: any, response: any, body: any) => {
             if (!err && (response.statusCode && response.statusCode >= 400 || !body)) {
-                err = new Error('Failed to load openID config: ' + response.statusCode);
+                err = new Error(`Failed to load openID config: ${ response.statusCode }`);
             }
 
             if (err) {
                 cb(err);
             } else {
-                var openIdConfig = <IOpenIdConfig>body;
+                const openIdConfig: IOpenIdConfig = <IOpenIdConfig>body;
 
-                var options: request.Options = {
+                const options2: request.Options = {
                     method: 'GET',
                     url: openIdConfig.jwks_uri,
                     json: true
                 };
 
-                request(options, (err, response, body) => {
-                    if (!err && (response.statusCode && response.statusCode >= 400 || !body)) {
-                        err = new Error("Failed to load Keys: " + response.statusCode);
+                request(options2, (err2: Error, response2: any, body2: any) => {
+                    if (!err2 && (response2.statusCode && response2.statusCode >= 400 || !body2)) {
+                        err2 = new Error(`Failed to load Keys: ${ response2.statusCode }`);
                     }
 
-                    if (!err) {
+                    if (!err2) {
                         this.lastUpdated = new Date().getTime();
-                        this.keys = <IKey[]>body.keys;
+                        this.keys = <IKey[]>body2.keys;
                     }
 
-                    cb(err);
+                    cb(err2);
                 });
             }
         });
@@ -86,17 +86,16 @@ export class OpenIdMetadata {
             return null;
         }
 
-        for (var i = 0; i < this.keys.length; i++) {
-            if (this.keys[i].kid == keyId) {
-                var key = this.keys[i];
+        for (const key of this.keys) {
+            if (key.kid === keyId) {
 
                 if (!key.n || !key.e) {
                     // Return null for non-RSA keys
                     return null;
                 }
 
-                var modulus = base64url.toBase64(key.n);
-                var exponent = key.e;
+                const modulus: any = base64url.toBase64(key.n);
+                const exponent: string = key.e;
 
                 return { key: getPem(modulus, exponent), endorsements: key.endorsements } as IOpenIdMetadataKey;
             }
@@ -126,6 +125,6 @@ interface IKey {
 }
 
 export interface IOpenIdMetadataKey {
-    key: string,
+    key: string;
     endorsements?: string[];
 }
