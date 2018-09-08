034de955b92a9a2dad72b324c7386ccb11c18987
Steven Gum <steven.gum@gmail.com>
2018-09-07 23:18:58 -0700

    add WaterfallDialog tests for step.values and step.options

--- libraries/botbuilder-dialogs/tests/waterfallDialog.test.js
+++ libraries/botbuilder-dialogs/tests/waterfallDialog.test.js
@@ -139,4 +139,275 @@ describe('WaterfallDialog', function () {
             .assertReply('ending WaterfallDialog.')
         done();
     });
+
+    it('should allow changing of `step.options` and persist changes across steps.', async function (done) {
+        const adapter = new TestAdapter(async (turnContext) => {
+            const dc = await dialogs.createContext(turnContext);
+
+            const results = await dc.continue();
+            switch (results.status) {
+                case (DialogTurnStatus.empty):
+                    await dc.begin('a', { test: 'test1' });
+                    break;
+                case (DialogTurnStatus.complete):
+                    await turnContext.sendActivity(results.result);
+                    break;
+            }
+        });
+
+        const convoState = new ConversationState(new MemoryStorage());
+        adapter.use(convoState);
+
+        const dialogState = convoState.createProperty('dialogState');
+        const dialogs = new DialogSet(dialogState);
+        dialogs.add(new WaterfallDialog('a', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test,
+                    'test1',
+                    `step.options.test "${step.options.test}", was not expected value of "test1".`);
+                step.options.test = 'test2';
+                await dc.context.sendActivity('bot responding.');
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test,
+                    'test2',
+                    `step.options.test "${step.options.test}", was not expected value of "test2".`);
+                step.options.test = 'test3';
+                await dc.context.sendActivity('bot responding again.');
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert.strictEqual(step.options.test,
+                    'test3',
+                    `step.options.test "${step.options.test}", was not expected value of "test3".`);
+                return await dc.end('ending WaterfallDialog.');
+            }
+        ]));
+
+        adapter.send(beginMessage)
+            .assertReply('bot responding.')
+            .send('continue')
+            .assertReply('bot responding again.')
+            .send('continue again')
+            .assertReply('ending WaterfallDialog.')
+        done();
+    });
+
+    it('should allow setting of step.values and persist values across steps.', async function (done) {
+        const adapter = new TestAdapter(async (turnContext) => {
+            const dc = await dialogs.createContext(turnContext);
+
+            const results = await dc.continue();
+            switch (results.status) {
+                case (DialogTurnStatus.empty):
+                    await dc.begin('a');
+                    break;
+                case (DialogTurnStatus.complete):
+                    await turnContext.sendActivity(results.result);
+                    break;
+            }
+        });
+
+        const convoState = new ConversationState(new MemoryStorage());
+        adapter.use(convoState);
+
+        const dialogState = convoState.createProperty('dialogState');
+        const dialogs = new DialogSet(dialogState);
+        dialogs.add(new WaterfallDialog('a', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert.equal(typeof step.values, 'object', `initial step.values should be an object.`);
+                step.values.test = 'test1'
+                await dc.context.sendActivity('bot responding.');
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.values, `step.values not found.`);
+                assert.strictEqual(step.values.test,
+                    'test1',
+                    `step.values.test ["${step.values.test}"] was not expected value "test1".`);
+                step.values.test2 = 'test2';
+                await dc.context.sendActivity('bot responding again.');
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.values, `step.values not found.`);
+                assert.strictEqual(step.values.test,
+                    'test1',
+                    `step.values.test ["${step.values.test}"] was not expected value "test1".`);
+                assert.strictEqual(step.values.test2,
+                    'test2',
+                    `step.values.test2 ["${step.values.test2}"] was not expected value "test2".`);
+                return await dc.end('ending WaterfallDialog.');
+            }
+        ]));
+
+        adapter.send(beginMessage)
+            .assertReply('bot responding.')
+            .send('continue')
+            .assertReply('bot responding again.')
+            .send('continue again')
+            .assertReply('ending WaterfallDialog.')
+        done();
+    });
+
+    it('should not move step.values from one WaterfallDialog to another.', async function (done) {
+        const adapter = new TestAdapter(async (turnContext) => {
+            const dc = await dialogs.createContext(turnContext);
+
+            const results = await dc.continue();
+            switch (results.status) {
+                case (DialogTurnStatus.empty):
+                    await dc.begin('a');
+                    break;
+                case (DialogTurnStatus.complete):
+                    await turnContext.sendActivity(results.result);
+                    break;
+            }
+        });
+
+        const convoState = new ConversationState(new MemoryStorage());
+        adapter.use(convoState);
+
+        const dialogState = convoState.createProperty('dialogState');
+        const dialogs = new DialogSet(dialogState);
+        dialogs.add(new WaterfallDialog('a', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert.equal(typeof step.values, 'object', `initial step.values should be an object.`);
+                step.values.test = 'test1';
+                await dc.context.sendActivity('bot responding.');
+                return await dc.begin('b');
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.values, `step.values not found.`);
+                assert.strictEqual(step.values.test,
+                    'test1',
+                    `step.values.test ["${step.values.test}"] was not expected value "test1".`);
+                assert.strictEqual(step.values.test_b,
+                    undefined,
+                    `step.values.test_b should not be available in WaterfallDialog('a').`);
+                return await dc.end('ending WaterfallDialog.');
+            }
+        ]));
+
+        dialogs.add(new WaterfallDialog('b', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert.equal(typeof step.values, 'object', `new step.values for second WaterfallDialog should be an object.`);
+                assert(!step.values.hasOwnProperty('test'), `new WaterfallDialog's step.values shouldn't have values from parent dialog's step.values.`);
+                
+                // Add a new value to this WaterfallDialog's step.values.
+                // This value should not be available to this dialog's parent.
+                step.values.test_b = 'test_b';
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert.equal(typeof step.values, 'object', `step.values for second WaterfallDialog should be an object.`);
+                assert(!step.values.hasOwnProperty('test'), `new WaterfallDialog's step.values shouldn't have values from parent dialog's step.values.`);
+                assert.strictEqual(step.values.test_b,
+                    'test_b',
+                    `step.values.test_b should not be available in WaterfallDialog 'a'.`);
+                
+                return await dc.end();
+            }
+        ]))
+
+        adapter.send(beginMessage)
+            .assertReply('bot responding.')
+            .send('continue')
+            .assertReply('ending WaterfallDialog.');
+        done();
+    });
+    
+    it('should not move step.options from one WaterfallDialog to another.', async function (done) {
+        const adapter = new TestAdapter(async (turnContext) => {
+            const dc = await dialogs.createContext(turnContext);
+
+            const results = await dc.continue();
+            switch (results.status) {
+                case (DialogTurnStatus.empty):
+                    await dc.begin('a', { test_a: 'test_a' });
+                    break;
+                case (DialogTurnStatus.complete):
+                    await turnContext.sendActivity(results.result);
+                    break;
+            }
+        });
+
+        const convoState = new ConversationState(new MemoryStorage());
+        adapter.use(convoState);
+
+        const dialogState = convoState.createProperty('dialogState');
+        const dialogs = new DialogSet(dialogState);
+        dialogs.add(new WaterfallDialog('a', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test_a,
+                    'test_a',
+                    `step.options.test_a "${step.options.test_a}", was not expected value of "test_a".`);
+                await dc.context.sendActivity('bot responding.');
+                return await dc.begin('b');
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test_a,
+                    'test_a',
+                    `step.options.test_a "${step.options.test_a}", was not expected value of "test_a".`);
+                return await dc.end('ending WaterfallDialog.');
+            }
+        ]));
+
+        dialogs.add(new WaterfallDialog('b', [
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test_a,
+                    undefined,
+                    `step.options.test_a "${step.options.test_a}", was not expected value of "undefined".`);
+                    step.options.test_b = 'test_b';
+                return Dialog.EndOfTurn;
+            },
+            async function (dc, step) {
+                assert(dc, `dc not found.`);
+                assert(step, `step not found.`);
+                assert(step.options, `step.options not found.`);
+                assert.strictEqual(step.options.test_b,
+                    'test_b',
+                    `step.options.test_b "${step.options.test_b}", was not expected value of "test_b".`)
+                return await dc.end();
+            }
+        ]))
+
+        adapter.send(beginMessage)
+            .assertReply('bot responding.')
+            .send('continue')
+            .assertReply('ending WaterfallDialog.');
+        done();
+    });
 });
