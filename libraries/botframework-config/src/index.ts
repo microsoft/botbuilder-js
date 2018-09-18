a3b9bd1f5a39e907a9889c7feb6cb2652d346402
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-18 14:21:03 -0700

    move export out of BotConfiguration class

--- libraries/botframework-config/src/index.ts
+++ libraries/botframework-config/src/index.ts
@@ -5,7 +5,6 @@
 export { BotConfiguration } from './botConfiguration';
 export { BotConfigurationBase } from './botConfigurationBase';
 export { BotRecipe, IBlobResource, ICosmosDBResource, IDispatchResource, IFileResource, IGenericResource, IResource, IUrlResource } from './botRecipe';
-export { ExportOptions } from './exportOptions';
 export * from './models';
 export * from './schema';
 
