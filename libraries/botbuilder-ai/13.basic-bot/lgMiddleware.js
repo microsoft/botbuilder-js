86ba083f8228acd1aad593be845d1a5371e3f5c6
Ahmed Elhanafy (IBS) <a-ahelh@microsoft.com>
2018-10-08 02:23:13 +0200

    Add simple bot sample

---
+++ libraries/botbuilder-ai/13.basic-bot/lgMiddleware.js
@@ -0,0 +1,29 @@
+const { LanguageGenerationResolver } = require('botbuilder-ai');
+
+class LGMiddleware {
+    /**
+     * Creates a translation middleware.
+     * @param {BotStatePropertyAccessor} entitiesStateAccessor Accessor for LG entities property in the user state.
+     * @param {LanguageGenerationApplication} languageGenerationApplication Language Generation configuration object.
+     */
+    constructor(entitiesStateAccessor, languageGenerationApplication) {
+        this.entitiesStateAccessor = entitiesStateAccessor;
+        this.lgResolver = new LanguageGenerationResolver(languageGenerationApplication);
+    }
+
+    async onTurn(turnContext, next) {
+        turnContext.onSendActivities(async (context, activities, next) => {
+            const { entities } = await this.entitiesStateAccessor.get(context, {});
+            await Promise.all(
+                activities.map(activity => {
+                    activity.locale = activity.locale || context.activity.locale;
+                    return this.lgResolver.resolve(activity, entities || {});
+                }),
+            );
+            await next();
+        });
+        await next();
+    }
+}
+
+module.exports = LGMiddleware;
