00de015f3423197e22a40711e2cba01d5c20b0ca
Dave Taniguchi <daveta@microsoft.com>
2018-11-26 09:32:40 -0800

    Forgot this

--- libraries/botbuilder-applicationinsights/src/index.ts
+++ libraries/botbuilder-applicationinsights/src/index.ts
@@ -1,11 +1,8 @@
 /**
- * @module botbuilder-azure
+ * @module botbuilder-applicationinsights
  */
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-export * from './cosmosDbStorage';
-export * from './blobStorage';
-export * from './azureBlobTranscriptStore';
-export * from './cosmosDbKeyEscape';
+export * from './telemetryBotIdInitializer.ts';
