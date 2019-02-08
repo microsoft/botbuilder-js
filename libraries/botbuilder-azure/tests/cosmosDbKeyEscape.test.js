a3c2a2e5c45d75352342cbca62e6eaadb3d00f20
Ben Brown <benbro@microsoft.com>
2019-02-08 14:16:22 -0600

    Truncate length of CosmosDb key to max of 255 chars

--- libraries/botbuilder-azure/tests/cosmosDbKeyEscape.test.js
+++ libraries/botbuilder-azure/tests/cosmosDbKeyEscape.test.js
@@ -65,4 +65,53 @@ describe('CosmosDbKeyEscape', function() {
         
         assert.notEqual(escaped1, escaped2, `${escaped1} should be different that ${escaped2}`)
     });
+
+    it ('should truncate longer keys', function() {
+
+        // create an extra long key
+        // limit is 255
+        let longKey = new Array(300).join('x');
+        let fixed = CosmosDbKeyEscape.escapeKey(longKey);
+        
+        assert(fixed.length <= 255, 'long key was not properly truncated');
+
+    })
+
+    it ('should not truncate short key', function() {
+
+        // create a short key
+        let shortKey = new Array(16).join('x');
+        let fixed2 = CosmosDbKeyEscape.escapeKey(shortKey);
+
+        assert(fixed2.length === 15, 'short key was truncated improperly');
+    });
+
+    it ('should create sufficiently different truncated keys of similar origin', function() {
+
+        // create 2 very similar extra long key where the difference will definitely be trimmed off by truncate function
+        let longKey = new Array(300).join('x') + "1";
+        let longKey2 = new Array(300).join('x') + "2";
+        
+        let fixed = CosmosDbKeyEscape.escapeKey(longKey);
+        let fixed2 = CosmosDbKeyEscape.escapeKey(longKey2);
+        
+        assert(fixed.length !== fixed2, 'key truncation failed to create unique key');
+
+    });
+
+    it ('should properly truncate keys with special chars', function() {
+
+        // create a short key
+        let longKey = new Array(300).join('*');
+        let fixed = CosmosDbKeyEscape.escapeKey(longKey);
+
+        assert(fixed.length <= 255, 'long key with special char was truncated improperly');
+
+        // create a short key
+        let shortKey = new Array(16).join('#');
+        let fixed2 = CosmosDbKeyEscape.escapeKey(shortKey);
+
+        assert(fixed2.length <= 255, 'short key with special char was truncated improperly');
+    });
+
 });
