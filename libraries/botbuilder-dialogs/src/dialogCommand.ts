36890242886d318c3399194ffa63074ac600053c
Steven Ickman <stevenic@microsoft.com>
2019-02-10 06:35:23 -0800

    Added logic to pop all commands off, not just last command

--- libraries/botbuilder-dialogs/src/dialogCommand.ts
+++ libraries/botbuilder-dialogs/src/dialogCommand.ts
@@ -16,17 +16,31 @@ export abstract class DialogCommand<O extends object = {}> extends Dialog<O> {
     }
 
     protected async endParentDialog(dc: DialogContext, result?: any): Promise<DialogTurnResult> {
-        dc.stack.pop();
+        this.popCommands(dc);
         return await dc.endDialog(result);
     }    
 
     protected async replaceParentDialog(dc: DialogContext, dialogId: string, options?: object): Promise<DialogTurnResult> {
-        dc.stack.pop();
+        this.popCommands(dc);
         return await dc.replaceDialog(dialogId, options);
     }
 
     protected async cancelAllParentDialogs(dc: DialogContext): Promise<DialogTurnResult> {
-        dc.stack.pop();
+        this.popCommands(dc);
         return await dc.cancelAllDialogs();
+    }
+
+    private popCommands(dc: DialogContext): void {
+        // Pop all commands off the stack.
+        while (dc.stack.length > 1) {
+            // Commands store the index of the state they're inheriting so we can tell a command
+            // by looking to see if its state of type 'number'.
+            const i = dc.stack.length - 1;
+            if (typeof dc.stack[i].state === 'number') {
+                dc.stack.pop();
+            } else {
+                break;
+            }
+        }
     }
 }
