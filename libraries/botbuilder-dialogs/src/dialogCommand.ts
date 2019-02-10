681acdad4dabe6338fc24e1bbbc0d1ef62d08978
Steven Ickman <stevenic@microsoft.com>
2019-02-10 05:58:31 -0800

    Update dialogCommand.ts

--- libraries/botbuilder-dialogs/src/dialogCommand.ts
+++ libraries/botbuilder-dialogs/src/dialogCommand.ts
@@ -5,8 +5,28 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { Dialog } from './dialog';
+import { Dialog, DialogTurnResult } from './dialog';
+import { DialogContext } from './dialogContext';
 
 export abstract class DialogCommand<O extends object = {}> extends Dialog<O> {
+    protected abstract onRunCommand(dc: DialogContext): Promise<DialogTurnResult>;
 
+    public beginDialog(dc: DialogContext): Promise<DialogTurnResult> {
+        return this.onRunCommand(dc);
+    }
+
+    protected async endParentDialog(dc: DialogContext, result?: any): Promise<DialogTurnResult> {
+        dc.stack.pop();
+        return await dc.endDialog(result);
+    }    
+
+    protected async replaceParentDialog(dc: DialogContext, dialogId: string, options?: object): Promise<DialogTurnResult> {
+        dc.stack.pop();
+        return await dc.replaceDialog(dialogId, options);
+    }
+
+    protected async cancelAllParentDialogs(dc: DialogContext): Promise<DialogTurnResult> {
+        dc.stack.pop();
+        return await dc.cancelAllDialogs();
+    }
 }
