e5da367f4f33b4a483b58f7478b919184c42183c
Fei Chen <feich@microsoft.com>
2019-03-21 15:55:38 +0800

    fix typo bug if operands index

--- libraries/botframework-expression/src/buildInFunctions.ts
+++ libraries/botframework-expression/src/buildInFunctions.ts
@@ -5,7 +5,7 @@ import { EvaluationDelegate } from './methodBinder';
 export abstract class BuildinFunctions {
     public static Add = (operands: any[]): EvaluationDelegate =>
         typeof operands[0] === 'number' && typeof operands[1] === 'number' ? <any>(operands[0] + operands[1]) :
-        typeof operands[0] === 'string' && typeof operands[2] === 'string' ? <any>(operands[0] + operands[1]) :
+        typeof operands[0] === 'string' && typeof operands[1] === 'string' ? <any>(operands[0] + operands[1]) :
         new Error()
 
     public static Sub = (operands: any[]): EvaluationDelegate =>
