3d25259342fa9a8b47ccfa18c3911d56cf1c6ad9
Steven Ickman <stevenic@microsoft.com>
2019-03-22 13:51:56 -0700

    Added CodeStep sample

---
+++ samples/07.codeStep/lib/index.js
@@ -0,0 +1,43 @@
+"use strict";
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const botbuilder_1 = require("botbuilder");
+const botbuilder_rules_1 = require("botbuilder-rules");
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open echobot.bot file in the Emulator.`);
+});
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+const adapter = new botbuilder_1.BotFrameworkAdapter({
+    appId: process.env.microsoftAppID,
+    appPassword: process.env.microsoftAppPassword,
+});
+// Create bot and bind to state storage
+const bot = new botbuilder_rules_1.Bot();
+bot.storage = new botbuilder_1.MemoryStorage();
+// Listen for incoming activities.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route activity to bot.
+        await bot.onTurn(context);
+    });
+});
+// Initialize bots root dialog
+const dialogs = new botbuilder_rules_1.AdaptiveDialog();
+bot.rootDialog = dialogs;
+// Add a default rule for handling incoming messages
+dialogs.addRule(new botbuilder_rules_1.DefaultRule([
+    new botbuilder_rules_1.CodeStep(async (dc) => {
+        const count = dc.state.getValue('conversation.count') || 0;
+        dc.state.setValue('conversation.count', count + 1);
+        return await dc.endDialog();
+    }),
+    new botbuilder_rules_1.SendActivity('{conversation.count}: You said: {utterance}')
+]));
+//# sourceMappingURL=index.js.map
---
+++ samples/07.codeStep/src/index.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as restify from 'restify';
+import { BotFrameworkAdapter, MemoryStorage } from 'botbuilder';
+import { Bot, AdaptiveDialog, DefaultRule, SendActivity, CodeStep } from 'botbuilder-rules';
+
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open echobot.bot file in the Emulator.`);
+});
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.microsoftAppID,
+    appPassword: process.env.microsoftAppPassword,
+});
+
+// Create bot and bind to state storage
+const bot = new Bot();
+bot.storage = new MemoryStorage();
+
+// Listen for incoming activities.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route activity to bot.
+        await bot.onTurn(context);
+    });
+});
+
+// Initialize bots root dialog
+const dialogs = new AdaptiveDialog();
+bot.rootDialog = dialogs;
+
+// Add a default rule for handling incoming messages
+dialogs.addRule(new DefaultRule([
+    new CodeStep(async (dc) => {
+        const count = dc.state.getValue('conversation.count') || 0;
+        dc.state.setValue('conversation.count', count + 1);
+        return await dc.endDialog();
+    }),
+    new SendActivity('{conversation.count}: You said: {utterance}')
+]));
