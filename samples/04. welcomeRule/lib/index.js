576da4c69d0523c9ad7b8232611ad7004ca43cde
Steven Ickman <stevenic@microsoft.com>
2019-03-22 10:36:49 -0700

    updated samples

--- samples/04. welcomeRule/lib/index.js
+++ samples/04. welcomeRule/lib/index.js
@@ -5,18 +5,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const restify = require("restify");
 const botbuilder_1 = require("botbuilder");
 const botbuilder_rules_1 = require("botbuilder-rules");
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+const adapter = new botbuilder_1.BotFrameworkAdapter({
+    appId: process.env.microsoftAppID,
+    appPassword: process.env.microsoftAppPassword,
+});
 // Create HTTP server.
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`\n${server.name} listening to ${server.url}`);
     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
     console.log(`\nTo talk to your bot, open echobot.bot file in the Emulator.`);
-});
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
-const adapter = new botbuilder_1.BotFrameworkAdapter({
-    appId: process.env.microsoftAppID,
-    appPassword: process.env.microsoftAppPassword,
 });
 // Create bot and bind to state storage
 const bot = new botbuilder_rules_1.Bot();
@@ -31,10 +31,15 @@ server.post('/api/messages', (req, res) => {
 // Initialize bots root dialog
 const dialogs = new botbuilder_rules_1.AdaptiveDialog();
 bot.rootDialog = dialogs;
-// Add rules
+// Greet User
+dialogs.addRule(new botbuilder_rules_1.WelcomeRule([
+    new botbuilder_rules_1.SendActivity(`Welcome! Say "hello" to get started.`)
+], `user.greeted`));
+// Send Default Response
 dialogs.addRule(new botbuilder_rules_1.DefaultRule([
-    new botbuilder_rules_1.SetProperty(`user.name = ''`),
-    new botbuilder_rules_1.TextInput('user.name', `Hi! what's your name?`),
+    new botbuilder_rules_1.IfProperty('!user.name', [
+        new botbuilder_rules_1.TextInput('user.name', `Hi! what's your name?`),
+    ]),
     new botbuilder_rules_1.SendActivity(`Hi {user.name}. It's nice to meet you.`)
 ]));
 //# sourceMappingURL=index.js.map
