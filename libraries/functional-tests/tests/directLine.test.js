f80e0afae20a7c8faf58e29ca764d77601e3f72f
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-05-09 15:33:45 -0300

    Update test logic to handle error 502

--- libraries/functional-tests/tests/directLine.test.js
+++ libraries/functional-tests/tests/directLine.test.js
@@ -10,7 +10,7 @@ const auths = {
     AuthorizationBotConnector: new Swagger.ApiKeyAuthorization('Authorization', 'BotConnector ' + directLineSecret, 'header'),
 };
 
-function getDirectLineClient() {
+function getDirectLineClient() {    
     return new Swagger({
         spec: directLineSpec,
         usePromise: true,
@@ -18,33 +18,40 @@ function getDirectLineClient() {
     });
 }
 
-function sendMessage(client, conversationId) {    
-    return client.apis.Conversations.Conversations_PostMessage({
-        conversationId: conversationId,
-        message: {
-            from: directLineClientName,
-            text: 'hi bot'
-        }
-    });    
+async function sendMessage(client, conversationId) {       
+    let status;
+    do{
+        await client.Conversations.Conversations_PostMessage({
+            conversationId: conversationId,
+            message: {
+                from: directLineClientName,
+                text: 'hi bot'
+            }
+        }).then((result) => {
+            status = result.status;
+        }).catch((err)=>{
+            status = err.status;
+        }); 
+    }while(status == 502);
 }
 
-function getMessages(client, conversationId) {
+function getMessages(client, conversationId) {    
     let watermark = null;
-    return client.apis.Conversations.Conversations_GetMessages({ conversationId: conversationId, watermark: watermark })
+    return client.Conversations.Conversations_GetMessages({ conversationId: conversationId, watermark: watermark })
         .then((response) => {            
             return response.obj.messages.filter((message) => message.from !== directLineClientName);       
         });
 }
 
 function getConversationId(client) {
-    return client.apis.Conversations.Conversations_NewConversation()
+    return client.Conversations.Conversations_NewConversation()
         .then((response) => response.obj.conversationId);
 }
 
 describe('Test Azure Bot', function(){
     this.timeout(30000);    
     it('Check deployed bot answer', async function(){
-        const directLineClient = await getDirectLineClient();
+        const directLineClient = await getDirectLineClient();    
         const conversationId = await getConversationId(directLineClient);
         await sendMessage(directLineClient, conversationId);
         const messages = await getMessages(directLineClient, conversationId); 
