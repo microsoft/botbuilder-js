a0bed103aef749cd5da1df52286c5cc2de11a259
Daniel Evans <dev@microsoft.com>
2019-05-28 12:52:15 -0700

    Fixes to header serializer tests

--- libraries/botframework-streaming-extensions-protocol/tests/HeaderSerializer.test.js
+++ libraries/botframework-streaming-extensions-protocol/tests/HeaderSerializer.test.js
@@ -9,8 +9,8 @@ describe('HeaderSerializer', () => {
 
     it('serializes and deserializes correctly', () => {
       
-        let header = new Header.Header(PayloadTypes.request, 168, '68e999ca-a651-40f4-ad8f-3aaf781862b4', true);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let header = new Header.Header(PayloadTypes.PayloadTypes.request, 168, '68e999ca-a651-40f4-ad8f-3aaf781862b4', true);
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
 
         HeaderSerializer.HeaderSerializer.serialize(header, buffer);
 
@@ -24,7 +24,7 @@ describe('HeaderSerializer', () => {
     });
 
     it('can parse an ASCII header', () => {
-        let buffer =Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer =Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n');
 
         let result =  HeaderSerializer.HeaderSerializer.deserialize(buffer);
@@ -39,9 +39,9 @@ describe('HeaderSerializer', () => {
     });
 
     it('deserializes unknown types', () => {
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('Z.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n');
-        let result =  HeaderSerializer.HeaderSerializer.deserialize(this.buffer);
+        let result =  HeaderSerializer.HeaderSerializer.deserialize(buffer);
 
         expect(result.PayloadType)
             .equal('Z');
@@ -49,7 +49,7 @@ describe('HeaderSerializer', () => {
 
     it('throws if the header is missing a part', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
@@ -58,7 +58,7 @@ describe('HeaderSerializer', () => {
 
     it('throws if the header has too many parts', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1.2\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
@@ -67,56 +67,56 @@ describe('HeaderSerializer', () => {
 
     it('throws if the header type is too long', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('ABCDE.000168.68e999ca-a651-40f4-ad8f-3aaf7b4.1\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-        .throws('header is malformed.');
+        .throws('Header Type is missing or malformed.');
     });
 
     it('throws if the header length is malformed', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.00b168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-        .throws('header is malformed.');
+        .throws('Header Length is missing or malformed.');
     });
 
     it('throws if the header length is to small', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.-00168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-        .throws('header is malformed.');
+        .throws('Length must be greater than 0');
     });
 
     it('throws if the header length is to big', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.1111111.8e999ca-a651-40f4-ad8f-3aaf781862b4.1\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-        .throws('header is malformed.');
+        .throws('Length must be less than 999999');
     });
 
     it('throws if the header terminator is malformed', () => {
         // expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.2\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-        .throws('header is malformed.');
+        .throws('Header End is missing or not a valid value.');
     });
 
     it('throws if the header ID is malformed', () => {
         //  expect.assertions(1);
-        let buffer = Buffer.alloc(Number(TransportContants.MaxHeaderLength));
+        let buffer = Buffer.alloc(Number(TransportContants.TransportContants.MaxHeaderLength));
         buffer.write('A.000168.68e9p9ca-a651-40f4-ad8f-3aaf781862b4.1\n');
 
         expect(() =>  HeaderSerializer.HeaderSerializer.deserialize(buffer))
-            .throws('header is malformed.');
+            .throws('Header ID is missing or malformed.');
     });
 
 });
