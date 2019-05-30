7cbc3e94f137e6b7a3ec862bb19ee6a0f0ea20e1
Daniel Evans <dev@microsoft.com>
2019-05-30 13:35:22 -0700

    cleanup and complete index

--- libraries/botframework-streaming-extensions-protocol/src/index.ts
+++ libraries/botframework-streaming-extensions-protocol/src/index.ts
@@ -1,23 +1,54 @@
+export * from './CancellationToken';
+export * from './CancellationTokenSource';
+export * from './ContentStream';
+export * from './HttpContentStream';
 export * from './IStreamingTransportClient';
 export * from './IStreamingTransportServer';
-export * from './RequestHandler';
+export * from './ProtocolAdapter';
 export * from './ReceiveRequest';
+export * from './ReceiveResponse';
+export * from './Request';
+export * from './RequestHandler';
+export * from './Response';
+export * from './Stream';
+
 export * from './PayloadTransport/IPayloadReceiver';
 export * from './PayloadTransport/IPayloadSender';
 export * from './PayloadTransport/PayloadReceiver';
 export * from './PayloadTransport/PayloadSender';
+export * from './PayloadTransport/SendPacket';
+export * from './PayloadTransport/TransportDisconnectedEventArgs';
+export * from './PayloadTransport/TransportDisconnectedEventHandler';
+
+export * from './Payloads/HeaderSerializer';
+export * from './Payloads/IRequestManager';
+export * from './Payloads/IStreamManager';
+export * from './Payloads/PayloadAssemblerManager';
 export * from './Payloads/RequestManager';
-export * from './ReceiveResponse';
-export * from './ProtocolAdapter';
-export * from './Request';
-export * from './Response';
-export * from './CancellationToken';
-export * from './CancellationTokenSource';
+export * from './Payloads/SendOperations';
+export * from './Payloads/StreamManager';
+
+export * from './Payloads/Assemblers/ContentStreamAssembler';
+export * from './Payloads/Assemblers/PayloadAssembler';
+export * from './Payloads/Assemblers/ReceiveRequestAssembler';
+export * from './Payloads/Assemblers/ReceiveResponseAssembler';
+
+export * from './Payloads/Disassemblers/CancelDisassembler';
+export * from './Payloads/Disassemblers/HttpContentStreamDisassembler';
+export * from './Payloads/Disassemblers/PayloadDisassembler';
+export * from './Payloads/Disassemblers/RequestDisassembler';
+export * from './Payloads/Disassemblers/ResponseDisassembler';
+export * from './Payloads/Disassemblers/StreamWrapper';
+
+export * from './Payloads/Models/Header';
+export * from './Payloads/Models/PayloadTypes';
+export * from './Payloads/Models/RequestPayload';
+export * from './Payloads/Models/ResponsePayload';
+export * from './Payloads/Models/StreamDescription';
+
+export * from './Transport/ITransport';
 export * from './Transport/ITransportReceiver';
 export * from './Transport/ITransportSender';
-export * from './HttpContentStream';
-export * from './Stream';
-export * from './ContentStream';
-export * from './ReceiveResponse';
-export * from './Response';
-export * from './Request';
+export * from './Transport/TransportConstants';
+
+export * from './Utilities/protocol-base';
