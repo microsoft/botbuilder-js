f1389d5cd03726099bacb9155f893db8382b93ab
Daniel Evans <dev@microsoft.com>
2019-05-31 11:28:18 -0700

    full test coverage for payload sender

--- libraries/botframework-streaming-extensions-protocol/tests/PayloadSender.test.js
+++ libraries/botframework-streaming-extensions-protocol/tests/PayloadSender.test.js
@@ -5,6 +5,7 @@ var expect = chai.expect;
 class MockSocket{
     send(buffer){
         let buff = buffer;
+        return buff.length;
     };
     
     close(){};
@@ -33,7 +34,54 @@ describe('PayloadSender', () => {
         let header = new protocol.Header(protocol.PayloadTypes.request, '42', '100', true);
         let packet = new protocol.SendPacket(header, stream, undefined);
 
-        ps.writePacket(packet);
+        expect(ps.writePacket(packet)).to.not.throw;
     });
+
+    it('calls the packet sent callback.', (done) => {
+        let ps = new protocol.PayloadSender();
+        ps.connect(new MockSocket);
+        expect(ps.isConnected).to.equal(true);
+
+        let stream = new protocol.Stream();
+        stream.write('This is a test stream.');
+        let header = new protocol.Header(protocol.PayloadTypes.request, '22', '100', true);
+        let packet = new protocol.SendPacket(header, stream, () => done());
+
+        ps.writePacket(packet);
+
+        expect(done);
+    });
+
+    it('disconnects when header length is longer than packet length.', () => {
+        let ps = new protocol.PayloadSender();
+        ps.connect(new MockSocket);
+        expect(ps.isConnected).to.equal(true);
+
+        let stream = new protocol.Stream();
+        stream.write('This is a test stream.');
+        let header = new protocol.Header(protocol.PayloadTypes.request, '42', '100', true);
+        let packet = new protocol.SendPacket(header, stream, undefined);
+
+        ps.writePacket(packet);
+
+        expect(ps.isConnected).to.equal(false);
+    });
+
+    it('gracefully fails when trying to write before connecting.', (done) => {
+        let ps = new protocol.PayloadSender();
+        ps.disconnected = () => done();
+        expect(ps.isConnected).to.equal(false);
+        ps.connect(new MockSocket);
+        expect(ps.isConnected).to.equal(true);
+        expect(ps.disconnected).to.not.be.undefined;
+
+        let stream = new protocol.Stream();
+        stream.write('This is a test stream.');
+        let header = new protocol.Header(protocol.PayloadTypes.request, '42', '100', true);
+        let packet = new protocol.SendPacket(header, stream, undefined);
+
+        expect(ps.writePacket(packet)).to.not.throw;
+    });
+
 });
 
