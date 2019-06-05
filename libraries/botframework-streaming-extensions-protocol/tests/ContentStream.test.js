8a6584fdba3bc607a0f8588f454f20ef2509cb6e
Daniel Evans <dev@microsoft.com>
2019-06-05 15:13:40 -0700

    contentstream coverage

--- libraries/botframework-streaming-extensions-protocol/tests/ContentStream.test.js
+++ libraries/botframework-streaming-extensions-protocol/tests/ContentStream.test.js
@@ -2,9 +2,28 @@ const  ContentStream  = require('../lib/ContentStream');
 const  ContentStreamAssembler  = require('../lib/Payloads/Assemblers/ContentStreamAssembler');
 const  chai  = require('chai');
 const StreamManager = require('../lib/Payloads/StreamManager');
+const protocol = require('../lib');
 var expect = chai.expect;
 
-describe('ContentStream ', () => {
+class TestContentStreamAssembler{
+    constructor(){
+        this.stream1 = new protocol.Stream();
+        this.stream1.write('hello');
+        this.contentType = 'application/text';
+        this.contentLength = 5;
+    }
+
+    getPayloadStream(){
+        return this.stream1;
+    }
+
+    close(){}
+
+    
+
+}
+
+describe('Streaming Extensions ContentStream Tests ', () => {
     it('assigns ID when constructed', () => {
       let csa = new ContentStreamAssembler.ContentStreamAssembler(new StreamManager.StreamManager(), 'csa1', 'stream', 42);
         let cs = new ContentStream.ContentStream('1', csa);
@@ -77,5 +96,54 @@ describe('ContentStream ', () => {
                     .to
                     .equal('SyntaxError: Unexpected end of JSON input');
             });
+    });
+
+    it('reads a stream as a string',  () => {
+        let cs = new protocol.ContentStream('cs1', new TestContentStreamAssembler());
+        let result = cs.readAsString();
+
+        result.then(function(data) {
+        expect(data).to.equal('hello');
+        
+        });
+    });
+
+        it('reads a stream as a json',  () => {
+        let cs = new protocol.ContentStream('cs1', new TestContentStreamAssembler());
+        let result = cs.readAsJson();
+
+        result.then(function(data) {
+        expect(data).to.equal('hello');
+        
+        });
+    });
+
+    it('reads a stream before receiving all the bits',  () => {
+        let tcsa = new TestContentStreamAssembler();
+        tcsa.contentLength = 10;
+        let cs = new protocol.ContentStream('cs1', tcsa);
+        let result = cs.readAsString();
+
+        result.then(function(data) {
+        expect(data).to.equal('hello');
+        
+        });
+    });
+
+    it('reads a stream as a buffer',  () => {
+        let cs = new protocol.ContentStream('cs1', new TestContentStreamAssembler());
+        let result = cs.readAsBuffer();
+
+        result.then(function(data) {
+        expect(data).to.equal('hello');
+        
+        });
+    });
+
+    it('can cancel',  () => {
+        let cs = new protocol.ContentStream('cs1', new TestContentStreamAssembler());
+        let result = cs.readAsString();
+
+        expect(cs.cancel()).to.not.throw;                
     });
 });
