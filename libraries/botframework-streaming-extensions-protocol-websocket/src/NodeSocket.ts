124187f3e49bb86434f4fc549fb05af1ce80e358
Daniel Evans <dev@microsoft.com>
2019-06-10 17:28:25 -0700

    implement node socket client connect

--- libraries/botframework-streaming-extensions-protocol-websocket/src/NodeSocket.ts
+++ libraries/botframework-streaming-extensions-protocol-websocket/src/NodeSocket.ts
@@ -1,3 +1,4 @@
+import * as http from 'http';
 import { WaterShed } from 'watershed';
 import { Socket } from './Socket';
 
@@ -16,11 +17,29 @@ export class NodeSocket implements Socket {
   }
 
   public async connectAsync(serverAddress): Promise<void> {
+    // following template from https://github.com/joyent/node-watershed#readme
     let shed = new WaterShed();
     let wskey = shed.generateKey();
     let options = {
+      port: 8082,
+      hostname: serverAddress,
+      headers: {
+      'connection': 'upgrade',
+      'Sec-WebSocket-Key': wskey,
+      'Sec-WebSocket-Version': '13'
+      }
+    };
+    let req = http.request(options);
+    req.end();
+    req.on('upgrade', function(res, socket, head) {
+      let wsc = shed.connect(res, socket, head, wskey);
+    });
 
-    }
+    return new Promise((resolve, reject) => {
+      req.on('close', resolve);
+      req.on('error', reject);
+    });
+
   }
 
   public setOnMessageHandler(handler: (x: any) => void) {
