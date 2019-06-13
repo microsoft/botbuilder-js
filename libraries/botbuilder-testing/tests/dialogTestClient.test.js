ac054d60d79679e88ae59c80a0b4736845043fae
Ben Brown <benbro@microsoft.com>
2019-06-13 12:56:05 -0500

    tests for dialogtestclient

--- libraries/botbuilder-testing/tests/dialogTestClient.test.js
+++ libraries/botbuilder-testing/tests/dialogTestClient.test.js
@@ -1,5 +1,5 @@
 const { DialogTestClient } = require('../');
-const { WaterfallDialog, DialogTurnStatus } = require('botbuilder-dialogs');
+const { ComponentDialog, TextPrompt, WaterfallDialog, DialogTurnStatus, DialogSet } = require('botbuilder-dialogs');
 const assert = require('assert');
 
 
@@ -15,7 +15,7 @@ describe('DialogTestClient', function() {
         let dialog = new WaterfallDialog('waterfall', [
             async(step) => {
                 await step.context.sendActivity('hello');
-                return step.next();
+                return step.endDialog();
             }
         ]);
 
@@ -36,7 +36,7 @@ describe('DialogTestClient', function() {
             },
             async(step) => {
                 await step.context.sendActivity('hello 2');
-                return step.next();
+                return step.endDialog();
             },
         ]);
 
@@ -46,6 +46,50 @@ describe('DialogTestClient', function() {
         reply = await client.getNextReply();
         assert(reply.text == 'hello 2', 'dialog responded with incorrect 2nd message');
         assert(client.dialogTurnResult.status == DialogTurnStatus.empty, 'dialog did not end properly');
+    });
+
+
+    it('should process a component dialog', async function() {
+
+        class MainDialog extends ComponentDialog {
+            constructor(id) {
+                super(id);
+
+                let dialog = new WaterfallDialog('waterfall', [
+                    async(step) => {
+                        return step.prompt('textPrompt', 'Tell me something');
+                    },
+                    async(step) => {
+                        await step.context.sendActivity('you said: ' + step.result);
+                        return step.next();
+                    },
+                ]);
+        
+                this.addDialog(dialog);
+                this.addDialog(new TextPrompt('textPrompt'));
+            }
+
+            async run(turnContext, accessor) {
+                const dialogSet = new DialogSet(accessor);
+                dialogSet.add(this);
+        
+                const dialogContext = await dialogSet.createContext(turnContext);
+                const results = await dialogContext.continueDialog();
+                if (results.status === DialogTurnStatus.empty) {
+                    await dialogContext.beginDialog(this.id);
+                }
+            }
+        }
+
+
+        let component = new MainDialog('component');
+
+        let client = new DialogTestClient(component);
+        let reply = await client.sendActivity('hello');
+        assert(reply.text == 'Tell me something','dialog responded with incorrect message');
+        reply = await client.sendActivity('foo');
+        assert(reply.text == 'you said: foo', 'dialog responded with incorrect 2nd message');
+        assert(client.dialogTurnResult.status == DialogTurnStatus.complete, 'dialog did not end properly');
     });
 
 });
