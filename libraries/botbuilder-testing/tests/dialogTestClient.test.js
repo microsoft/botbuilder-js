2b75ddea5f068b271db37c85b4714d163ee6cb80
Ben Brown <benbro@microsoft.com>
2019-06-25 14:43:05 -0500

    copyright header

--- libraries/botbuilder-testing/tests/dialogTestClient.test.js
+++ libraries/botbuilder-testing/tests/dialogTestClient.test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 const { DialogTestClient, DialogTestLogger } = require('../');
 const { ComponentDialog, TextPrompt, WaterfallDialog, DialogTurnStatus, DialogSet } = require('botbuilder-dialogs');
 const assert = require('assert');
