23770f545c176e5f637c0c18f23fc70170624135
Ben Brown <benbro@microsoft.com>
2019-06-26 15:23:00 -0500

    add more tests

--- libraries/testbot/tests/testData/bookingDialogTestCases.js
+++ libraries/testbot/tests/testData/bookingDialogTestCases.js
@@ -4,6 +4,7 @@
  */
 let now = new Date();
 
+let today = formatDate(new Date());
 let tomorrow = formatDate(new Date().setDate(now.getDate() + 1));
 
 // Source: https://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd
@@ -21,18 +22,6 @@ function formatDate(date) {
 
 module.exports = [
     {
-        name: 'Happy Path',
-        initialData: {
-            destination: 'Seattle',
-            origin: 'New York',
-            travelDate: '2022-02-22'
-        },
-        steps: [
-            ['hi', 'Please confirm, I have you traveling to: Seattle from: New York on: 2022-02-22. (1) Yes or (2) No'],
-            ['yes', null],
-        ],
-    },
-    {
         name: 'Full Run Path',
         initialData: {
         },
@@ -41,8 +30,57 @@ module.exports = [
             ['hi', 'To what city would you like to travel?'],
             ['Seattle','From what city will you be travelling?'],
             ['New York', 'On what date would you like to travel?'],
-            ['2022-02-22', 'Please confirm, I have you traveling to: Seattle from: New York on: 2022-02-22. (1) Yes or (2) No'],
+            ['tomorrow', `Please confirm, I have you traveling to: Seattle from: New York on: ${ tomorrow }. (1) Yes or (2) No`],
             ['yes',null]
+        ],
+    },
+    {
+        name: 'Full flow with \'no\' at confirmation',
+        initialData: {
+        },
+        expectedResult: null,
+        steps: [
+            ['hi', 'To what city would you like to travel?'],
+            ['Seattle','From what city will you be travelling?'],
+            ['New York', 'On what date would you like to travel?'],
+            ['tomorrow', `Please confirm, I have you traveling to: Seattle from: New York on: ${ tomorrow }. (1) Yes or (2) No`],
+            ['no',null]
+        ],
+    },
+    {
+        name: 'Destination given',
+        initialData: {
+            destination: 'Bahamas',
+        },
+        steps: [
+            ['hi','From what city will you be travelling?'],
+            ['New York', 'On what date would you like to travel?'],
+            ['tomorrow', `Please confirm, I have you traveling to: Bahamas from: New York on: ${ tomorrow }. (1) Yes or (2) No`],
+            ['yes',null]
+        ],
+    },
+    {
+        name: 'Destination and origin given',
+        initialData: {
+            destination: 'Bahamas',
+            origin: 'New York',
+        },
+        steps: [
+            ['hi', 'On what date would you like to travel?'],
+            ['tomorrow', `Please confirm, I have you traveling to: Bahamas from: New York on: ${ tomorrow }. (1) Yes or (2) No`],
+            ['yes',null]
+        ],
+    },
+    {
+        name: 'All booking details given for today',
+        initialData: {
+            destination: 'Seattle',
+            origin: 'New York',
+            travelDate: today
+        },
+        steps: [
+            ['hi', `Please confirm, I have you traveling to: Seattle from: New York on: ${ today }. (1) Yes or (2) No`],
+            ['yes', null],
         ],
     },
     {
