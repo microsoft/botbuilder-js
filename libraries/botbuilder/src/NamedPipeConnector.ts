065e5bb69caf4d26bfd3d8f58a08547e3bf2ef25
Daniel Evans <dev@microsoft.com>
2019-07-09 16:03:15 -0700

    Rework namedpipeconnector, fix middleware bug

--- libraries/botbuilder/src/NamedPipeConnector.ts
+++ libraries/botbuilder/src/NamedPipeConnector.ts
@@ -5,17 +5,22 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { ActivityHandler } from 'botbuilder-core';
+import { ActivityHandler, MiddlewareHandler, Middleware } from 'botbuilder-core';
 import { BotFrameworkAdapterSettings } from './botFrameworkAdapter';
-import { NamedPipeServer } from 'botframework-streaming-extensions';
 import { StreamingRequestHandler } from './StreamingRequestHandler';
 
 export class NamedPipeConnector {
+/*  The default named pipe all instances of DL ASE listen on is named bfv4.pipes
+    Unfortunately this name is no longer very discriptive, but for the time being
+    we're unable to change it without coordinated updates to DL ASE, which we
+    currently are unable to perform.
+*/
+  private readonly defaultPipeName = 'bfv4.pipes';
   private readonly pipeName: string;
   private readonly bot: ActivityHandler;
-  private readonly defaultPipeName = 'bfv4.pipes';
+  private readonly middleWare: (MiddlewareHandler|Middleware)[];
 
-  constructor(bot: ActivityHandler, pipeName?: string) {
+  constructor(bot: ActivityHandler, pipeName?: string, middleWare?: (MiddlewareHandler|Middleware)[]) {
     if (bot === undefined) {
       throw new Error('Undefined Argument: Bot can not be undefined.');
     } else {
@@ -28,13 +33,12 @@ export class NamedPipeConnector {
       this.pipeName = pipeName;
     }
 
+    this.middleWare = middleWare;
   }
 
   public async processAsync(settings: BotFrameworkAdapterSettings) {
-    let handler = new StreamingRequestHandler(this.bot);
-    let server = new NamedPipeServer(this.pipeName, handler);
-    handler.setServer(server);
-    handler.adapterSettings = settings;
-    await server.startAsync();
+    let handler = new StreamingRequestHandler( this.bot, undefined, settings, this.middleWare);
+
+    await handler.startNamedPipeAsync(this.pipeName);
   }
 }
