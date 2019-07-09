d38c10e7129ffc603d1f7c02104b34f7d25427fa
Daniel Evans <dev@microsoft.com>
2019-07-09 14:25:23 -0700

    Remove unused logger from namedpipeconnector

--- libraries/botbuilder/src/NamedPipeConnector.ts
+++ libraries/botbuilder/src/NamedPipeConnector.ts
@@ -11,22 +11,15 @@ import { NamedPipeServer } from 'botframework-streaming-extensions';
 import { StreamingRequestHandler } from './StreamingRequestHandler';
 
 export class NamedPipeConnector {
-  private logger;
   private readonly pipeName: string;
   private readonly bot: ActivityHandler;
   private readonly defaultPipeName = 'bfv4.pipes';
 
-  constructor(bot: ActivityHandler, pipeName?: string, logger?) {
+  constructor(bot: ActivityHandler, pipeName?: string) {
     if (bot === undefined) {
       throw new Error('Undefined Argument: Bot can not be undefined.');
     } else {
       this.bot = bot;
-    }
-
-    if (logger === undefined) {
-      this.logger = console;
-    } else {
-      this.logger = logger;
     }
 
     if (pipeName === undefined) {
@@ -38,13 +31,10 @@ export class NamedPipeConnector {
   }
 
   public async processAsync(settings: BotFrameworkAdapterSettings) {
-
     let handler = new StreamingRequestHandler(this.bot);
-    this.logger.log('Creating server for Named Pipe connection.');
     let server = new NamedPipeServer(this.pipeName, handler);
     handler.setServer(server);
     handler.adapterSettings = settings;
-    this.logger.log(`Listening on Named Pipe: ${this.pipeName}`);
     await server.startAsync();
   }
 }
