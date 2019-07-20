fc2e7e5653c7667572d59eafc7aee1270e95acdf
Gabo Gilabert <ggilaber@microsoft.com>
2019-07-20 16:27:46 -0400

    Removed debug messages

--- tools/util/updateDependenciesInPackageJsons.js
+++ tools/util/updateDependenciesInPackageJsons.js
@@ -23,14 +23,8 @@ function updateDependencies(filePath) {
         var result = '';
         dependencies.forEach(function (dependency) {
             var findString = new RegExp('("dependencies":)(.+?)("' + dependency + '":\\s*")([^\/"]+)', "gms");
-            console.log('findString =');
-            console.log(findString);
             var replaceString = '$1$2$3' + newVersion;
-            console.log('replaceString =');
-            console.log(replaceString);
             result = data.replace(findString, replaceString);
-            console.log('result =');
-            console.log(result);
             data = result;
         });
 
