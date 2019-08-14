7080d1f0f910ccbbc0bdc16d7001f79757698b14
Daniel Evans <dev@microsoft.com>
2019-08-14 11:22:36 -0700

    More coverage for streaming response

--- libraries/botframework-streaming-extensions/tests/StreamingResponse.test.js
+++ libraries/botframework-streaming-extensions/tests/StreamingResponse.test.js
@@ -16,4 +16,17 @@ describe('Streaming Extensions Response Tests', () => {
         expect(r).to.be.instanceOf(StreamingResponse.StreamingResponse);
         expect(r.statusCode).to.equal(200);
     });
+
+    it('can set the response body', () => {
+        let stream1 = new SubscribableStream.SubscribableStream();
+        stream1.write('hello');
+        let headers = {contentLength: '5', contentType: 'text/plain'};
+        let hc = new HttpContentStream.HttpContent(headers, stream1);
+        let r = StreamingResponse.StreamingResponse.create(200, hc);
+        r.setBody('This is a new body.');
+
+        expect(r).to.be.instanceOf(StreamingResponse.StreamingResponse);
+        expect(r.streams[1].content.stream.bufferList[0].toString()).to.contain('This is a new body.');
+        expect(r.statusCode).to.equal(200);
+    });
 });
