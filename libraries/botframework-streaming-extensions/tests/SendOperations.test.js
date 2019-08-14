cf7a372802ea601caf2290115cf2fe14093b7636
Daniel Evans <dev@microsoft.com>
2019-08-14 12:15:41 -0700

    coverage for send operations

--- libraries/botframework-streaming-extensions/tests/SendOperations.test.js
+++ libraries/botframework-streaming-extensions/tests/SendOperations.test.js
@@ -3,6 +3,7 @@ const PayloadSender = require('../lib/PayloadTransport/PayloadSender');
 const SubscribableStream = require('../lib/SubscribableStream');
 const SendOperations = require('../lib/Payloads/SendOperations');
 const StreamingRequest = require('../lib/StreamingRequest');
+const StreamingResponse = require('../lib/StreamingResponse');
 const  chai  = require('chai');
 var expect = chai.expect;
 
@@ -25,6 +26,35 @@ describe('Streaming Extension SendOperations Tests', () => {
         r.addStream(hc);
         expect(so).to.be.instanceOf(SendOperations.SendOperations);
         so.sendRequest('test1', r).then(done());
+    });
+
+    it('processes a send response with streams operation', (done) => {
+        let ps = new PayloadSender.PayloadSender();
+        let so = new SendOperations.SendOperations(ps);
+        let stream1 = new SubscribableStream.SubscribableStream();
+        stream1.write('hello');
+        let headers = {contentLength: '5', contentType: 'text/plain'};
+        let hc = new HttpContent.HttpContent(headers, stream1);
+        let r = StreamingResponse.StreamingResponse.create(200, hc);
+        r.setBody('This is a new body.');
+
+        expect(r).to.be.instanceOf(StreamingResponse.StreamingResponse);
+        expect(r.streams[1].content.stream.bufferList[0].toString()).to.contain('This is a new body.');
+        expect(r.statusCode).to.equal(200);
+
+        so.sendResponse('test1', r).then(done());
+    });
+
+    it('processes a send response with no streams operation', (done) => {
+        let ps = new PayloadSender.PayloadSender();
+        let so = new SendOperations.SendOperations(ps);
+        let stream1 = new SubscribableStream.SubscribableStream();
+        stream1.write('hello');
+        let headers = {contentLength: '5', contentType: 'text/plain'};
+        let hc = new HttpContent.HttpContent(headers, stream1);
+        let r = StreamingResponse.StreamingResponse.create(200, hc);
+
+        so.sendResponse('test1', r).then(done());
     });
 
     it('processes a cancel stream operation', async (done) => {
