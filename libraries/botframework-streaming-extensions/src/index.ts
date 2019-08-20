e5096173948086898db3fa8335261993265a66c9
Daniel Evans <dev@microsoft.com>
2019-08-20 10:27:04 -0700

    exposing classes used by preview version of directlinejs

--- libraries/botframework-streaming-extensions/src/index.ts
+++ libraries/botframework-streaming-extensions/src/index.ts
@@ -5,9 +5,13 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+export { ContentStream } from './ContentStream'; // Temporary for DirectLineJS integration
+export { HttpContent } from './HttpContentStream'; // Temporary for DirectLineJS integration
 export * from './Integration';
+export { IReceiveRequest } from './Interfaces/IReceiveRequest'; // Temporary for DirectLineJS integration
 export { NamedPipeClient, NamedPipeServer } from './NamedPipe';
 export { RequestHandler } from './RequestHandler';
 export { StreamingRequest } from './StreamingRequest';
 export { StreamingResponse } from './StreamingResponse';
+export { SubscribableStream } from './SubscribableStream'; // Temporary for DirectLineJS integration
 export { BrowserWebSocket, NodeWebSocket, WebSocketClient, WebSocketServer } from './WebSocket';
