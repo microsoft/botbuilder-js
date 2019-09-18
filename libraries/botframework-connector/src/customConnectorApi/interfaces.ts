fd051a4e2532442631d67c3defb8ac42a3bfe6b4
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-18 16:31:27 -0300

    [BFConnector] Add new custom interfaces [WIP].
    Add interfaces GetAttachmentResponse and GetAttachmentInfoResponse, in order to make compliant the custom API with the current implementation of attachmentsAPI.

--- libraries/botframework-connector/src/customConnectorApi/interfaces.ts
+++ libraries/botframework-connector/src/customConnectorApi/interfaces.ts
@@ -1018,3 +1018,78 @@ export declare enum ActionTypes {
     Payment = "payment",
     MessageBack = "messageBack"
 }
+
+/**
+ * Contains response data for the getAttachment operation.
+ */
+export type GetAttachmentResponse = {
+  /**
+   * BROWSER ONLY
+   *
+   * The response body as a browser Blob.
+   * Always undefined in node.js.
+   */
+  blobBody: Promise<Blob>;
+
+  /**
+   * NODEJS ONLY
+   *
+   * The response body as a node.js Readable stream.
+   * Always undefined in the browser.
+   */
+  readableStreamBody: NodeJS.ReadableStream;
+
+  /**
+   * The underlying HTTP response.
+   */
+  _response: http.IncomingMessage;
+};
+
+export interface AttachmentView {
+  /**
+   * Id of the attachment
+   */
+  viewId: string;
+  /**
+   * Size of the attachment
+   */
+  size: number;
+}
+
+/**
+ * Metadata for an attachment
+ */
+export interface AttachmentInfo {
+  /**
+   * Name of the attachment
+   */
+  name: string;
+  /**
+   * ContentType of the attachment
+   */
+  type: string;
+  /**
+   * attachment views
+   */
+  views: AttachmentView[];
+}
+
+/**
+ * Contains response data for the getAttachmentInfo operation.
+ */
+export type GetAttachmentInfoResponse = AttachmentInfo & {
+  /**
+   * The underlying HTTP response.
+   */
+  _response: http.IncomingMessage & {
+    /**
+     * The response body as text (string format)
+     */
+    bodyAsText: string;
+
+    /**
+     * The response body as parsed JSON or XML
+     */
+    parsedBody: AttachmentInfo;
+  };
+};
