88d91c13162e6f28a6cb158fb1b9f06ff7345d47
Santiago Grangetto <santiagograngetto@gmail.com>
2019-09-20 17:46:44 -0300

    Add default userAgent in customTokenApi

--- libraries/botframework-connector/src/customTokenApi/customTokenApiClient.ts
+++ libraries/botframework-connector/src/customTokenApi/customTokenApiClient.ts
@@ -8,6 +8,16 @@ import { UserTokenApi } from './userTokenApi';
 import { BotSignInApi } from './botSignInApi';
 import * as Model from "./model";
 import { CustomMicrosoftAppCredentials } from '../auth'
+import * as os from 'os';
+
+const ARCHITECTURE: any = os.arch();
+const TYPE: any = os.type();
+const RELEASE: any = os.release();
+const NODE_VERSION: any = process.version;
+const pjson: any = require('../../package.json');
+const packageName = "botframework-Token";
+const packageVersion = "4.0.0";
+const defaultUserAgent = `botframework-connector/${ pjson.version } Node/${ NODE_VERSION } OS/(${ ARCHITECTURE }-${ TYPE }-${ RELEASE })`
 
 class CustomTokenApiClient {
   // Operation groups
@@ -23,14 +33,13 @@ class CustomTokenApiClient {
    * @param credentials Subscription credentials which uniquely identify client subscription.
    * @param [options] The parameter options
    */
-  constructor( CustomCredentials: CustomMicrosoftAppCredentials, options?: { baseUri: string, userAgent: string }) {
+  constructor( CustomCredentials: CustomMicrosoftAppCredentials, options?: { baseUri: string, userAgent?: string }) {
+    options.userAgent = `${packageName}/${packageVersion} ${defaultUserAgent} ${options.userAgent || ''}`
     this.credentials = CustomCredentials;
     this.userAgent = options.userAgent;
     this.baseUri = options.baseUri || this.baseUri || "https://token.botframework.com";
     this.botSignIn = new BotSignInApi(this);
-    this.userToken = new UserTokenApi(this);    
-    
-   
+    this.userToken = new UserTokenApi(this);
   }
 }
 
