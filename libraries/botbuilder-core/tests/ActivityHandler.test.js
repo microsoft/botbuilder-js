e74a909f47481aae174f900d02f70c90844cf4c6
stgum <14935595+stevengum@users.noreply.github.com>
2019-09-27 17:00:51 -0700

    add new tests for default dispatching logic

--- libraries/botbuilder-core/tests/ActivityHandler.test.js
+++ libraries/botbuilder-core/tests/ActivityHandler.test.js
@@ -212,4 +212,155 @@ describe('ActivityHandler', function() {
         processActivity({type: 'foo'}, bot);
     });
 
+    describe('should by default', () => {
+        let onTurnCalled = false;
+        let onMessageCalled = false;
+        let onConversationUpdateCalled = false;
+        let onMembersAddedCalled = false;
+        let onMembersRemovedCalled = false;
+        let onMessageReactionCalled = false;
+        let onReactionsAddedCalled = false;
+        let onReactionsRemovedCalled = false;
+        let onEventCalled = false;
+        let onTokenResponseEventCalled = false;
+        let onUnrecognizedActivityTypeCalled = false;
+        let onDialogCalled = false;
+
+        afterEach(function () {
+            onTurnCalled = false;
+            onMessageCalled = false;
+            onConversationUpdateCalled = false;
+            onMembersAddedCalled = false;
+            onMembersRemovedCalled = false;
+            onMessageReactionCalled = false;
+            onReactionsAddedCalled = false;
+            onReactionsRemovedCalled = false;
+            onEventCalled = false;
+            onTokenResponseEventCalled = false;
+            onUnrecognizedActivityTypeCalled = false;
+            onDialogCalled = false;
+        });
+
+        function assertContextAndNext(context, next) {
+            assert(context, 'context not found');
+            assert(next, 'next not found');
+        }
+
+        function assertFalseFlag(flag, ...args) {
+            assert(!flag, `${args[0]}Called should not be true before the ${args.join(', ')} handlers are called.`);
+        }
+
+        function assertTrueFlag(flag, ...args) {
+            assert(flag, `${args[0]}Called should be true after the ${args[0]} handlers are called.`);
+        }
+
+        function assertTrueFlagBeforeSubTypes(flag, ...args) {
+            assert(flag, `${args[0]}Called should be true after the ${args[0]} handler is called but not before the ${args.splice(1).join(', ')} handlers are called.`);
+        }
+
+        it('call "onTurn" handlers then dispatch by Activity Type for "ConversationUpdate"', async () => {
+            const bot = new ActivityHandler();
+            bot.onTurn(async (context, next) => {
+                assertContextAndNext(context, next);
+                assertFalseFlag(onTurnCalled, 'onTurn');
+                onTurnCalled = true;
+                assertFalseFlag(onConversationUpdateCalled, 'onConversationUpdate', 'onTurn');
+                await next();
+            });
+
+            bot.onConversationUpdate(async (context, next) => {
+                assertContextAndNext(context, next);
+                assertTrueFlag(onTurnCalled, 'onTurn');
+                assertFalseFlag(onConversationUpdateCalled, 'onConversationUpdate', 'onTurn');
+                onConversationUpdateCalled = true;
+                await next();
+            });
+    
+
+            processActivity({type: ActivityTypes.ConversationUpdate}, bot);
+            assertTrueFlag(onTurnCalled, 'onTurn');
+            assertTrueFlag(onConversationUpdateCalled, 'onConversationUpdate');
+        });
+
+        it('call "onTurn" handlers then dispatch by Activity Type "Message"', async () => {
+            const bot = new ActivityHandler();
+            bot.onTurn(async (context, next) => {
+                assertContextAndNext(context, next);
+                assertFalseFlag(onTurnCalled, 'onTurn');
+                onTurnCalled = true;
+                assertFalseFlag(onConversationUpdateCalled, 'onMessage', 'onTurn');
+                await next();
+            });
+
+            bot.onMessage(async (context, next) => {
+                assertContextAndNext(context, next);
+                assertTrueFlag(onTurnCalled, 'onTurn');
+                assertFalseFlag(onConversationUpdateCalled, 'onMessage', 'onTurn');
+                assert(!onMessageCalled, 'onMessage should not be true before onTurn and onMessage handlers complete.');
+                onMessageCalled = true;
+                await next();
+            });
+    
+
+            processActivity({type: ActivityTypes.Message}, bot);
+            assertTrueFlag(onTurnCalled, 'onTurn');
+            assertTrueFlag(onMessageCalled, 'onMessage');
+        });
+
+        xit('call "onTurn" handlers then dispatch by Activity Type "MembersAdded"', async () => {
+            const bot = new ActivityHandler();
+            bot.onTurn(async (context, next) => {
+                assertContextAndNext(context, next);
+                assert(!onTurnCalled, 'onTurnCalled should not be true before the onTurn handlers are called.');
+                onTurnCalled = true;
+                assert(!onMembersAddedCalled, 'onMembersAdded should not be true before onTurn and onMembersAdded handlers complete.');
+                await next();
+            });
+
+            bot.onMembersAdded(async (context, next) => {
+                assertContextAndNext(context, next);
+                assert(onTurnCalled, 'onTurnCalled should not be false before the onMembersAdded handlers are called.');
+                assert(!onMembersAddedCalled, 'onMembersAdded should not be true before onTurn and onMembersAdded handlers complete.');
+                onMembersAddedCalled = true;
+                await next();
+            });
+
+            processActivity({type: ActivityTypes.ConversationUpdate, membersAdded: [{id: 1}]}, bot);
+            assert(onTurnCalled, 'onTurnCalled should be true after the onTurn handlers are called.');
+            assert(onMembersAddedCalled, 'onTurnCalled should be true after the onMembersAdded handlers are called.');
+        });
+
+        xit('call "onTurn" handlers then dispatch by Activity Type "MembersAdded"', async () => {
+            const bot = new ActivityHandler();
+            bot.onTurn(async (context, next) => {
+                assertContextAndNext(context, next);
+                assert(!onTurnCalled, 'onTurnCalled should not be true before the onTurn handlers are called.');
+                onTurnCalled = true;
+                assert(!onMessageReactionCalled, 'onMessageReaction should not be true before onTurn and onMessageReaction handlers complete.');
+                assert(!onReactionsRemovedCalled, 'onMessageReaction should not be true before onTurn and onMessageReaction handlers complete.');
+                await next();
+            });
+
+            bot.onMessageReaction(async (context, next) => {
+                assertContextAndNext(context, next);
+                assert(onTurnCalled, 'onTurnCalled should not be false before the onMessageReaction handlers are called.');
+                assert(!onMessageReactionCalled, 'onMembersAdded should not be true before onTurn and onMembersAdded handlers complete.');
+                onMessageReactionCalled = true;
+                await next();
+            });
+
+            bot.onReactionsRemoved(async (context, next) => {
+                assertContextAndNext(context, next);
+                assert(onTurnCalled, 'onTurnCalled should not be false before the onReactionsRemoved handlers are called.');
+                assert(!onReactionsRemovedCalled, 'onReactionsRemoved should not be true before onTurn and onReactionsRemoved handlers complete.');
+                onReactionsRemovedCalled = true;
+                await next();
+            });
+            processActivity({type: ActivityTypes.MessageReaction, reactionsRemoved: [{type: 'like'}]}, bot);
+
+            assert(onTurnCalled, 'onTurnCalled should be true after the onTurn handlers are called.');
+            assert(onMembersAddedCalled, 'onTurnCalled should be true after the onMembersAdded handlers are called.');
+        });
+    });
+
 });
