d7a12318dcd6e7068b9d4a202ee1fd60e68069b2
Daniel Evans <dev@microsoft.com>
2019-10-07 16:27:11 -0700

    Add IReceiveResponse to exports

--- libraries/botframework-streaming-extensions/src/index.ts
+++ libraries/botframework-streaming-extensions/src/index.ts
@@ -5,12 +5,12 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-export { ContentStream } from './ContentStream'; // Temporary for DirectLineJS integration
-export { HttpContent } from './HttpContentStream'; // Temporary for DirectLineJS integration
-export { IStreamingTransportServer, IStreamingTransportClient, ISocket, IReceiveRequest } from './Interfaces' // Temporary for DirectLineJS integration
+export { ContentStream } from './ContentStream';
+export { HttpContent } from './HttpContentStream';
+export { IStreamingTransportServer, IStreamingTransportClient, ISocket, IReceiveRequest, IReceiveResponse } from './Interfaces'
 export { NamedPipeClient, NamedPipeServer } from './NamedPipe';
 export { RequestHandler } from './RequestHandler';
 export { StreamingRequest } from './StreamingRequest';
 export { StreamingResponse } from './StreamingResponse';
-export { SubscribableStream } from './SubscribableStream'; // Temporary for DirectLineJS integration
+export { SubscribableStream } from './SubscribableStream';
 export { BrowserWebSocket, NodeWebSocket, WebSocketClient, WebSocketServer } from './WebSocket';
