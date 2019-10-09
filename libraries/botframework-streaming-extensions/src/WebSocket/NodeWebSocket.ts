e3ca12f91e82a15a9f7785251fcff074e2624dd6
William Wong <compulim@hotmail.com>
2019-10-09 13:41:12 -0700

    Fix as event target

--- libraries/botframework-streaming-extensions/src/WebSocket/NodeWebSocket.ts
+++ libraries/botframework-streaming-extensions/src/WebSocket/NodeWebSocket.ts
@@ -9,10 +9,22 @@ import WebSocket from 'ws';
 
 import { IsomorphicWebSocket } from './IsomorphicWebSocket';
 
+// TODO: This is not a full EventTarget implementation, missing "onmessage", etc.
+//       We should implement it.
+class WebSocketWithEventTarget extends WebSocket {
+    addEventListener(name, listener) {
+        super.on(name, listener);
+    }
+
+    removeEventListener(name, listener) {
+        super.off(name, listener);
+    }
+}
+
 export class NodeWebSocket extends IsomorphicWebSocket {
     constructor(webSocket?: WebSocket) {
         super({
-            ponyfill: { WebSocket },
+            ponyfill: { WebSocket: WebSocketWithEventTarget },
             webSocket
         });
     }
