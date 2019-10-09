ccb818c85e884bc6efc7a9be39199474c71af4d2
William Wong <compulim@hotmail.com>
2019-10-09 14:53:45 -0700

    Fix WebSocket import

--- libraries/botframework-streaming-extensions/src/WebSocket/NodeWebSocket.ts
+++ libraries/botframework-streaming-extensions/src/WebSocket/NodeWebSocket.ts
@@ -5,13 +5,30 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import WebSocket from 'ws';
+
+import * as WebSocket from 'ws';
 
 import { IsomorphicWebSocket } from './IsomorphicWebSocket';
 
-// TODO: This is not a full EventTarget implementation, missing "onmessage", etc.
-//       We should implement it.
 class WebSocketWithEventTarget extends WebSocket {
+    constructor(...args) {
+        super(...args);
+
+        // Bridging legacy "onclose", "onerror", "onmessage", and "onopen" event handlers.
+        [
+            'close',
+            'error',
+            'message',
+            'open'
+        ].forEach(name => {
+            super.on(name, (...args) => {
+                const handler = this[`on${ name }`];
+
+                handler && handler.apply(this, args);
+            });
+        });
+    }
+
     addEventListener(name, listener) {
         super.on(name, listener);
     }
