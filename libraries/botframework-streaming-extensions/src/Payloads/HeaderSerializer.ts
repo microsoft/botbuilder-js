96f941377cf2a10831b4c40d0af32d808a6216e0
Daniel Evans <dev@microsoft.com>
2019-10-11 17:06:56 -0700

    relax the rules for what counts as a well formed guid

--- libraries/botframework-streaming-extensions/src/Payloads/HeaderSerializer.ts
+++ libraries/botframework-streaming-extensions/src/Payloads/HeaderSerializer.ts
@@ -59,7 +59,7 @@ export class HeaderSerializer {
             throw Error('Header Type is missing or malformed.');
         }
 
-        if (!header.id || !header.id.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i) || header.id.length !==  this.IdLength) {
+        if (!header.id || !header.id.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i) || header.id.length !==  this.IdLength) {
             throw Error('Header ID is missing or malformed.');
         }
 
