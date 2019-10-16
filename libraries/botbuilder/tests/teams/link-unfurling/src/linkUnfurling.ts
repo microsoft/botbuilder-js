02afa192c5c78910d2d3e7dd541838d3a92fe1ae
Tien Suwandy <tiens@microsoft.com>
2019-10-15 17:06:02 -0700

    updated code to match update on handler

--- libraries/botbuilder/tests/teams/link-unfurling/src/linkUnfurling.ts
+++ libraries/botbuilder/tests/teams/link-unfurling/src/linkUnfurling.ts
@@ -2,14 +2,15 @@
 // Licensed under the MIT License.
 
 import {
+    AppBasedLinkQuery,
     MessagingExtensionResponse,
     MessagingExtensionResult,
     TeamsActivityHandler,
-    InvokeResponseTyped,
     CardFactory, 
     ThumbnailCard,
     CardImage,
-    HeroCard } 
+    HeroCard,
+    TurnContext } 
 from 'botbuilder';
 
 export class LinkUnfurlingBot extends TeamsActivityHandler {
@@ -23,14 +24,15 @@ export class LinkUnfurlingBot extends TeamsActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
 
-        // "Link Unfurling"
-        // This handler is used for the processing of "composeExtension/queryLink" activities from Teams.
-        // https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/messaging-extensions/search-extensions#receive-requests-from-links-inserted-into-the-compose-message-box
-        // By specifying domains under the messageHandlers section in the manifest, the bot can receive
-        // events when a user enters in a domain in the compose box.
-        this.onAppBasedLinkQuery(async (context, query, next) => {
-            const attachment = CardFactory.thumbnailCard('Thumbnail Card', query.url, ["https://raw.githubusercontent.com/microsoft/botframework-sdk/master/icon.png"]);
+     // "Link Unfurling"
+    // This handler is used for the processing of "composeExtension/queryLink" activities from Teams.
+    // https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/messaging-extensions/search-extensions#receive-requests-from-links-inserted-into-the-compose-message-box
+    // By specifying domains under the messageHandlers section in the manifest, the bot can receive
+    // events when a user enters in a domain in the compose box.   
+    protected async onTeamsAppBasedLinkQuery(context: TurnContext, query: AppBasedLinkQuery): Promise<MessagingExtensionResponse> {
+        const attachment = CardFactory.thumbnailCard('Thumbnail Card', query.url, ["https://raw.githubusercontent.com/microsoft/botframework-sdk/master/icon.png"]);
 
             const result: MessagingExtensionResult = {
                 attachmentLayout: 'list',
@@ -42,14 +44,8 @@ export class LinkUnfurlingBot extends TeamsActivityHandler {
                 composeExtension: result,
             }
 
-            const invoke_response: InvokeResponseTyped<MessagingExtensionResponse> = {
-                status: 200,
-                body: response,
-                };
             // For Invoke activities from Teams, we're currently not continuing the chain of handlers.
             // await next();
-            return Promise.resolve(invoke_response);
-                
-        });
+            return Promise.resolve(response);
     }
 }
