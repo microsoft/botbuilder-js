a942952fb43dfbb26aab0605b7cf806b5b7fbf4f
Joe <jose.ques@southworks.com>
2019-11-05 17:38:25 -0300

    Add browser-functional-tests - Add browser echo-bot sample - Setup Nightwatch to run with Chrome - Add script to run the tests

---
+++ libraries/browser-functional-tests/tests/message-handling-tests.js
@@ -0,0 +1,58 @@
+var botPage;
+module.exports = {
+    botPage: {},
+    before: function(browser) {
+        botPage = browser.page.echoBotPage();
+    },
+    afer: function(browser) {
+        // End current page session
+        botPage.end();
+    },
+    'Echo bot webchat is loaded': function(browser) {
+        // Navigate to Echo Bot page
+        // This step is made in the first test since navigation can't be done in the 'before' hook
+        botPage.navigate();
+
+        // Check Webchat container is initialized and is present in the page
+        botPage
+            .assert.elementPresent('@webchatContainer');
+    },
+    'Echo bot webchat sends messages': async function(browser) {
+        // Type 'Hello' in webchat input box and send it to the bot
+        botPage
+            .setValue('@webchatMessageInput', 'Hello')
+            .click('@webchatMessageInputSubtmitButton', function(result) {
+                // Assertion to check the button was clickable and got triggered
+                this.assert.strictEqual(result.status, 0, 'Message input working');
+            })
+            .pause(250);
+
+        await assertMessageIsPresentInPage(botPage, 'Hello', 'Webchat contains user message');
+    },
+    'Echo bot webchat echoes messages': async function(browser) {
+        await assertMessageIsPresentInPage(botPage, '1: You said “Hello”', 'Webchat contains bot reply');
+    }
+};
+
+async function assertMessageIsPresentInPage(pageInstance, textSearch, assertMessage) {
+    let messagesListPromises = [];
+    // Get messages list from webchat
+    await pageInstance.api.elements('@webchatMessagesList', function(messagesWebElements) {
+        for (let index = 0; (index < messagesWebElements.value.length); index++) {
+            const webElement = messagesWebElements.value[index];
+            messagesListPromises.push(new Promise(function(resolve){
+                pageInstance.api.elementIdText(webElement.ELEMENT, function(elementText) {
+                    resolve(elementText.value == textSearch);
+                });
+            }));
+        }
+    }).then(function(){
+        Promise.all(messagesListPromises)
+        .then(function (results) {
+            let messageExists = results.some(function (value) {
+                return value;
+            });
+            pageInstance.assert.strictEqual(messageExists, true, assertMessage);
+        });
+    });
+}
