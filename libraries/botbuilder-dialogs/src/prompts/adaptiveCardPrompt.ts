2d0b290f4f48417f5c04210cdbb326d0b099b7b0
mdrichardson <v-micric@microsoft.com>
2019-11-18 14:43:22 -0800

    - replaced failure messages with enum error codes - removed displayOnAttempt reprompt

--- libraries/botbuilder-dialogs/src/prompts/adaptiveCardPrompt.ts
+++ libraries/botbuilder-dialogs/src/prompts/adaptiveCardPrompt.ts
@@ -13,14 +13,6 @@ export interface AdaptiveCardPromptSettings {
     card?: Attachment;
 
     /**
-     * Message sent (if not null) when user uses text input instead of Adaptive Card Input
-     * 
-     * @remarks
-     * Defaults to: 'Please fill out the Adaptive Card'
-     */
-    inputFailMessage?: string|null|undefined;
-
-    /**
      * Array of strings matching IDs of required input fields
      * 
      * @remarks
@@ -37,27 +29,6 @@ export interface AdaptiveCardPromptSettings {
     requiredInputIds?: string[];
 
     /**
-     * Message sent (if not null) when user doesn't submit a required input
-     * <Each, Missing, Input> gets appended to the end of the string
-     * 
-     * @remarks
-     * Defaults to: The following inputs are required'
-     */
-    missingRequiredInputsMessage?: string|null|undefined;
-
-    /**
-     * Card will not be redisplayed/re-prompted unless:
-     *   * PromptOptions includes a retryPrompt with a card, or
-     *   * Number of attempts per displayed card equals this value
-     * 
-     * @remarks
-     * This is meant to prevent the user from providing input to the original, 
-     *   and not the re-prompted card
-     * Defaults to 3
-     */
-    attemptsBeforeCardRedisplayed?: number;
-
-    /**
      * ID specific to this prompt.
      * 
      * @remarks
@@ -66,6 +37,12 @@ export interface AdaptiveCardPromptSettings {
      *   If set manually, will not change between reprompts.
      */
     promptId?: string;
+}
+
+export enum AdaptiveCardInputPromptErrors {
+    UserInputDoesNotMatchCardId = 'userInputDoesNotMatchCardId',
+    MissingRequiredIds = 'missingRequiredIds',
+    UserUsedTextInput = 'userUsedTextInput'
 }
 
 /**
@@ -81,10 +58,7 @@ export interface AdaptiveCardPromptSettings {
  */
 export class AdaptiveCardPrompt extends Dialog {
     private validator: PromptValidator<object>;
-    private _inputFailMessage: string|null|undefined;
     private _requiredInputIds: string[];
-    private _missingRequiredInputsMessage: string|null|undefined;
-    private _attemptsBeforeCardRedisplayed: number;
     private _promptId: string;
     private usesCustomPromptId: boolean = false;
     private _card: Attachment;
@@ -102,12 +76,8 @@ export class AdaptiveCardPrompt extends Dialog {
         settings = typeof(settings) === 'object' ? settings : {};
 
         this.validator = validator;
-        this._inputFailMessage = settings.inputFailMessage || 'Please fill out the Adaptive Card';
 
         this._requiredInputIds = settings.requiredInputIds || [];
-        this._missingRequiredInputsMessage = settings.missingRequiredInputsMessage || 'The following inputs are required';
-
-        this._attemptsBeforeCardRedisplayed = settings.attemptsBeforeCardRedisplayed || 3;
 
         this._card = settings.card;
 
@@ -117,36 +87,12 @@ export class AdaptiveCardPrompt extends Dialog {
         }
     }
 
-    public get inputFailMessage(): string|null|undefined {
-        return this._inputFailMessage;
-    }
-
-    public set inputFailMessage(message: string|null|undefined) {
-        this._inputFailMessage = message;
-    }
-
     public get requiredInputIds(): string[] {
         return this._requiredInputIds;
     }
 
     public set requiredInputIds(ids: string[]) {
         this._requiredInputIds = ids;
-    }
-
-    public get missingRequiredInputsMessage(): string|null|undefined {
-        return this._missingRequiredInputsMessage;
-    }
-
-    public set missingRequiredInputsMessage(message: string|null|undefined) {
-        this._missingRequiredInputsMessage = message;
-    }
-
-    public get attemptsBeforeCardRedisplayed(): number {
-        return this._attemptsBeforeCardRedisplayed;
-    }
-
-    public set attemptsBeforeCardRedisplayed(attempts: number) {
-        this._attemptsBeforeCardRedisplayed = attempts;
     }
 
     public get promptId(): string {
@@ -198,7 +144,7 @@ export class AdaptiveCardPrompt extends Dialog {
 
         this.validateIsCard(card, isRetry);
 
-        prompt.attachments = [this.addPromptIdToCard(card)];
+        prompt.attachments = [card];
 
         await context.sendActivity(prompt, undefined, InputHints.ExpectingInput);
     }
@@ -235,11 +181,9 @@ export class AdaptiveCardPrompt extends Dialog {
         if (isValid) {
             return await dc.endDialog(recognized.value);
         } else {
-            // Re-prompt, conditionally display card again
-            if (state.state['attemptCount'] % this._attemptsBeforeCardRedisplayed === 0) {
-                await this.onPrompt(dc.context, state.state, state.options, true);
-            }
-            return await Dialog.EndOfTurn;
+            // Re-prompt
+            await this.onPrompt(dc.context, state.state, state.options, true);
+            return Dialog.EndOfTurn;
         }
     }
 
@@ -248,7 +192,7 @@ export class AdaptiveCardPrompt extends Dialog {
         if (!context.activity.text && context.activity.value) {
             // Validate it comes from the correct card - This is only a worry while the prompt/dialog has not ended
             if (context.activity.value && context.activity.value['promptId'] != this._promptId) {
-                return { succeeded: false };
+                return { succeeded: false, value: { error: AdaptiveCardInputPromptErrors.UserInputDoesNotMatchCardId }};
             }
             // Check for required input data, if specified in AdaptiveCardPromptSettings
             let missingIds = [];
@@ -257,20 +201,14 @@ export class AdaptiveCardPrompt extends Dialog {
                     missingIds.push(id);
                 }
             });
-            // Alert user to missing data
+            // User did not submit inputs that were required
             if (missingIds.length > 0) {
-                if (this._missingRequiredInputsMessage) {
-                    await context.sendActivity(`${ this._missingRequiredInputsMessage }: ${ missingIds.join(', ') }`);
-                }
-                return { succeeded: false };
+                return { succeeded: false, value: { error: AdaptiveCardInputPromptErrors.MissingRequiredIds, missingIds } };
             }
             return { succeeded: true, value: context.activity.value };
         } else {
-            // User used text input instead of card input or is missing required Inputs
-            if (this._inputFailMessage) {
-                await context.sendActivity(this._inputFailMessage);
-            }
-            return { succeeded: false };
+            // User used text input instead of card input
+            return { succeeded: false, value: { error: AdaptiveCardInputPromptErrors.UserUsedTextInput } };
         }
     }
 
@@ -285,52 +223,6 @@ export class AdaptiveCardPrompt extends Dialog {
             `Ensure card.contentType is '${ adaptiveCardType }'\n`+
             `and card.content contains the card json`);
         }
-    }
-
-    private addPromptIdToCard(cardAttachment: Attachment): Attachment {
-        cardAttachment.content = this.deepSearchJsonForActionsAndAddPromptId(cardAttachment.content);
-        return cardAttachment;
-    }
-
-    private deepSearchJsonForActionsAndAddPromptId(json: object): object {
-        for (const key in json) {
-            // Search for all submits in actions
-            if (key === 'actions') {
-                for (const action in json[key]) {
-                    json[key][action] = this.checkAction(json[key][action]);
-                }
-
-            // Search for all submits in selectActions
-            } else if (key === 'selectAction') {
-                json[key] = this.checkAction(json[key]);
-
-            // Recursively search all other objects
-            } else if (json[key] && typeof json[key] === 'object') {
-                json[key] = this.deepSearchJsonForActionsAndAddPromptId(json[key]);
-            }
-        }
-
-        return json;
-    }
-
-    private checkAction(action: { type: string; data: object|string }): object {
-        const submitAction = 'Action.Submit';
-        const showCardAction = 'Action.ShowCard';
-
-        if (typeof(action.data) === 'string') {
-            throw new Error('Submit action data cannot be a string in an Adaptive Card prompt');
-        }
-
-        if (action.type && action.type === submitAction) {
-            action.data = { ...action.data, promptId: this._promptId };
-        } else if (action.type && action.type === showCardAction) {
-            // Recursively search Action.ShowCard for Submits within the nested card.
-            // Note that there can't be a nested card in a select action
-            // because Action.ShowCard is not supported in select actions.
-            return this.deepSearchJsonForActionsAndAddPromptId(action);
-        }
-
-        return action;
     }
 }
 
