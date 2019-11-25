668f91c54e3d686cf9a09d1ae7233e8ffd1a140e
Chris Anderson <chrande@microsoft.com>
2019-11-25 11:33:47 -0800

    fix: missing path

--- libraries/botbuilder-dialogs/src/i18n.ts
+++ libraries/botbuilder-dialogs/src/i18n.ts
@@ -5,14 +5,14 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-const Chinese = require('cldr-data/main/zh/numbers.json');
-const English = require('cldr-data/main/en/numbers.json');
-const German = require('cldr-data/main/de/numbers.json');
-const Dutch = require('cldr-data/main/nl/numbers.json');
-const Japanese = require('cldr-data/main/ja/numbers.json');
-const LikelySubtags = require('cldr-data/supplemental/likelySubtags.json');
-const NumberingSystem = require('cldr-data/supplemental/numberingSystems.json');
-const Portuguese = require('cldr-data/main/pt/numbers.json');
-const Spanish = require('cldr-data/main/es/numbers.json');
+const Chinese = require('../vendor/cldr-data/main/zh/numbers.json');
+const English = require('../vendor/cldr-data/main/en/numbers.json');
+const German = require('../vendor/cldr-data/main/de/numbers.json');
+const Dutch = require('../vendor/cldr-data/main/nl/numbers.json');
+const Japanese = require('../vendor/cldr-data/main/ja/numbers.json');
+const LikelySubtags = require('../vendor/cldr-data/supplemental/likelySubtags.json');
+const NumberingSystem = require('../vendor/cldr-data/supplemental/numberingSystems.json');
+const Portuguese = require('../vendor/cldr-data/main/pt/numbers.json');
+const Spanish = require('../vendor/cldr-data/main/es/numbers.json');
 
 export { Chinese, English, German, Dutch, Japanese, LikelySubtags, NumberingSystem, Portuguese, Spanish }
