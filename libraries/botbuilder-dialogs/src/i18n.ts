579a336018973aa105447ab4dcda9617cb506950
Chris Anderson <chrande@microsoft.com>
2019-11-25 11:12:30 -0800

    refactor: remove direct cldr-data dep

---
+++ libraries/botbuilder-dialogs/src/i18n.ts
@@ -0,0 +1,18 @@
+/**
+ * @module botbuilder-dialogs
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const Chinese = require('cldr-data/main/zh/numbers.json');
+const English = require('cldr-data/main/en/numbers.json');
+const German = require('cldr-data/main/de/numbers.json');
+const Dutch = require('cldr-data/main/nl/numbers.json');
+const Japanese = require('cldr-data/main/ja/numbers.json');
+const LikelySubtags = require('cldr-data/supplemental/likelySubtags.json');
+const NumberingSystem = require('cldr-data/supplemental/numberingSystems.json');
+const Portuguese = require('cldr-data/main/pt/numbers.json');
+const Spanish = require('cldr-data/main/es/numbers.json');
+
+export { Chinese, English, German, Dutch, Japanese, LikelySubtags, NumberingSystem, Portuguese, Spanish }
