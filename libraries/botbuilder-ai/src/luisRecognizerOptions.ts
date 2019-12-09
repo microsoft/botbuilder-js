0802841111dcb777495d907d53499d95e53555fc
Emilio Munoz <emmunozp@microsoft.com>
2019-12-09 09:32:38 -0800

    Adding missing file header

--- libraries/botbuilder-ai/src/luisRecognizerOptions.ts
+++ libraries/botbuilder-ai/src/luisRecognizerOptions.ts
@@ -1,3 +1,11 @@
+/**
+ * @module botbuilder-ai
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import {LuisApplication, LuisRecognizerOptions}  from './luisRecognizer'
 import { RecognizerResult, TurnContext } from 'botbuilder-core';
 
