d48762e48995f590efdad5b8e7e5e4d53914b680
Shuai Wang <shuwan@microsoft.com>
2020-02-04 22:14:21 +0800

    retrigger

--- libraries/botframework-expressions/tests/expression.test.js
+++ libraries/botframework-expressions/tests/expression.test.js
@@ -93,7 +93,7 @@ const dataSource = [
     ['replaceIgnoreCase(nullObj, \'L\', \'k\')', ''],
     ['replaceIgnoreCase(\'hello\', \'L\', nullObj)', 'heo'],
     ['split(\'hello\',\'e\')', ['h', 'llo']],
-    ['split(\'hell\')', ['h', 'e', 'l', 'l']],
+    ['split(\'hell\')', ['h', 'e', 'l', 'l', 'o']],
     ['split(nullObj,\'e\')', ['']],
     ['split(\'hello\',nullObj)', ['h', 'e', 'l', 'l', 'o']],
     ['split(nullObj,nullObj)', []],
