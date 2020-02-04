7f52d3762412cf19c43096f6725c409fa41cdf6a
Shuai Wang <shuwan@microsoft.com>
2020-02-04 14:16:50 +0800

    add a test

--- libraries/botframework-expressions/tests/expression.test.js
+++ libraries/botframework-expressions/tests/expression.test.js
@@ -290,6 +290,7 @@ const dataSource = [
     ['formatDateTime(\'2018-03-15T00:00:00.000Z\', \'yyyy\')', '2018'],
     ['formatDateTime(\'2018-03-15T00:00:00.000Z\', \'yyyy-MM-dd-\\d\')', '2018-03-15-4'],
     ['formatDateTime(\'2018-03-15T00:00:00.010Z\', \'FFFF\')', '0100'],
+    ['formatDateTime(\'2018-03-15T00:00:00.010Z\', \'FFFFFF\')', '010000'],
     ['formatDateTime(\'2018-03-15T00:00:00.010Z\', \'FFF\')', '010'],
     ['formatDateTime(\'2018-03-15T09:00:00.010\', \'hh\')', '09'],
     ['formatDateTime(\'2018-03-15T09:00:00.010\', \'MMMM\')', 'March'],
