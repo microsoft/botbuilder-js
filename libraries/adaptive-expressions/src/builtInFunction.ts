edfb269693ef9172c9ff9102aaefc8595a353021
Shuai Wang <shuwan@microsoft.com>
2020-02-19 12:00:56 +0800

    parity extend foreach and where work with object (#1733)

--- libraries/adaptive-expressions/src/builtInFunction.ts
+++ libraries/adaptive-expressions/src/builtInFunction.ts
@@ -944,19 +944,26 @@ export class BuiltInFunctions {
     private static foreach(expression: Expression, state: MemoryInterface): { value: any; error: string } {
         let result: any[];
         let error: string;
-        let collection: any;
+        let instance: any;
 
-        ({ value: collection, error } = expression.children[0].tryEvaluate(state));
+        ({ value: instance, error } = expression.children[0].tryEvaluate(state));
 
         if (!error) {
             // 2nd parameter has been rewrite to $local.item
-            if (!Array.isArray(collection)) {
-                error = `${ expression.children[0] } is not a collection to run foreach`;
+            const iteratorName = (expression.children[1].children[0] as Constant).value as string;
+            let arr = [];
+            if (Array.isArray(instance)) {
+                arr = instance;
+            } else if (typeof instance === 'object') {
+                Object.keys(instance).forEach(u => arr.push({key: u, value: instance[u]}));
             } else {
-                const iteratorName = (expression.children[1].children[0] as Constant).value as string;
+                error = `${ expression.children[0] } is not a collection or structure object to run foreach`;
+            }
+
+            if (!error) {
                 const stackedMemory = StackedMemory.wrap(state);
                 result = [];
-                for (const item of collection) {
+                for (const item of arr) {
                     const local: Map<string, any> = new Map<string, any>([
                         [iteratorName, item]
                     ]);
@@ -976,21 +983,29 @@ export class BuiltInFunctions {
     }
 
     private static where(expression: Expression, state: MemoryInterface): { value: any; error: string } {
-        let result: any[];
+        let result: any;
         let error: string;
-        let collection: any;
+        let instance: any;
 
-        ({ value: collection, error } = expression.children[0].tryEvaluate(state));
+        ({ value: instance, error } = expression.children[0].tryEvaluate(state));
 
         if (!error) {
-            const iteratorName: string = (expression.children[1].children[0] as Constant).value as string;
-            if (!Array.isArray(collection)) {
-                error = `${ expression.children[0] } is not a collection to run where`;
+            const iteratorName = (expression.children[1].children[0] as Constant).value as string;
+            let arr: any[] = [];
+            let isInstanceArray = false;
+            if (Array.isArray(instance)) {
+                arr = instance;
+                isInstanceArray = true;
+            } else if (typeof instance === 'object') {
+                Object.keys(instance).forEach(u => arr.push({key: u, value: instance[u]}));
             } else {
-                const iteratorName = (expression.children[1].children[0] as Constant).value as string;
+                error = `${ expression.children[0] } is not a collection or structure object to run foreach`;
+            }
+
+            if (!error) {
                 const stackedMemory = StackedMemory.wrap(state);
-                result = [];
-                for (const item of collection) {
+                const arrResult = [];
+                for (const item of arr) {
                     const local: Map<string, any> = new Map<string, any>([
                         [iteratorName, item]
                     ]);
@@ -1003,8 +1018,20 @@ export class BuiltInFunctions {
                     }
 
                     if ((Boolean(r))) {
-                        result.push(local.get(iteratorName));
+                        arrResult.push(local.get(iteratorName));
                     }
+                }
+
+                //reconstruct object if instance is object, otherwise, return array result
+                if (!isInstanceArray) {
+                    let objResult = {};
+                    for(const item of arrResult) {
+                        objResult[item.key] = item.value;
+                    }
+
+                    result = objResult;
+                } else {
+                    result = arrResult;
                 }
             }
         }
