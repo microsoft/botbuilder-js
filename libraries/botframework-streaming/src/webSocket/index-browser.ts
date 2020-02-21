034d4643fa5eac93b4ef3fc83ee7c870c23a2018
stgum <14935595+stevengum@users.noreply.github.com>
2020-02-21 11:13:08 -0800

    create browser (es5) output in botframework-streaming

---
+++ libraries/botframework-streaming/src/webSocket/index-browser.ts
@@ -0,0 +1,12 @@
+/**
+ * @module botframework-streaming
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './browserWebSocket';
+export * from './webSocketClient';
+export * from './webSocketServer';
+export * from './webSocketTransport';
