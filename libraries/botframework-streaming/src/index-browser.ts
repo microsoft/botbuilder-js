2b9b62bb6095fafd174615a6690484175552111e
Steven Gum <14935595+stevengum@users.noreply.github.com>
2020-02-24 13:03:20 -0800

    create browser (es5) output in botframework-streaming (#1764)
    * create browser (es5) output in botframework-streaming
    
    * include new browser folder in packed files
    
    * apply PR feedback around exports

---
+++ libraries/botframework-streaming/src/index-browser.ts
@@ -0,0 +1,29 @@
+/**
+ * @module botframework-streaming
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export { ContentStream } from './contentStream';
+export { HttpContent } from './httpContentStream';
+export {
+    INodeBuffer,
+    INodeIncomingMessage,
+    INodeSocket,
+    IReceiveRequest,
+    IReceiveResponse,
+    ISocket,
+    IStreamingTransportClient,
+    IStreamingTransportServer,
+} from './interfaces';
+export { RequestHandler } from './requestHandler';
+export { StreamingRequest } from './streamingRequest';
+export { StreamingResponse } from './streamingResponse';
+export { SubscribableStream } from './subscribableStream';
+export {
+    BrowserWebSocket,
+    WebSocketClient,
+    WebSocketServer
+} from './webSocket/index-browser';
