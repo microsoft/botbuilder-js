164d1326b4911c511ddc8cd93f5787b1033be44c
stevengum <14935595+stevengum@users.noreply.github.com>
2020-03-02 13:23:28 -0800

    fix import in testbot

--- libraries/testbot/bots/dialogAndWelcomeBot.js
+++ libraries/testbot/bots/dialogAndWelcomeBot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { CardFactory } = require('botbuilder-core');
+const { CardFactory } = require('botbuilder');
 const { DialogBot } = require('./dialogBot');
 const WelcomeCard = require('./resources/welcomeCard.json');
 
