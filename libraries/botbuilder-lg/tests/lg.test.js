0e4a0d9dc13faea471e7c6ea9c8785adb2c4d258
Shuai Wang <shuwan@microsoft.com>
2020-03-18 17:47:28 +0800

    undo comment

--- libraries/botbuilder-lg/tests/lg.test.js
+++ libraries/botbuilder-lg/tests/lg.test.js
@@ -57,14 +57,14 @@ describe('LG', function() {
     it('TestMultiLineExprLG', function() {
         let templates = Templates.parseFile(GetExampleFilePath('MultiLineExpr.lg'));
 
-        // let evaled = templates.evaluate('ExprInCondition', { userName: 'Henry', day: 'Monday' });
-        // assert.strictEqual(evaled === 'Not today', true, `Evaled is ${ evaled }`);
+        let evaled = templates.evaluate('ExprInCondition', { userName: 'Henry', day: 'Monday' });
+        assert.strictEqual(evaled === 'Not today', true, `Evaled is ${ evaled }`);
 
-        // evaled = templates.evaluate('definition');
-        // assert.strictEqual(evaled, 10);
+        evaled = templates.evaluate('definition');
+        assert.strictEqual(evaled, 10);
 
-        // evaled = templates.evaluate('template');
-        // assert.strictEqual(evaled, 15);
+        evaled = templates.evaluate('template');
+        assert.strictEqual(evaled, 15);
 
 
     });
