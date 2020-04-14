9dd36d0b7bf38fd6b26ef2229edeba893d251ce0
Zichuan Ma <zim@microsoft.com>
2020-04-14 17:54:28 +0800

    when running npm test, should run adaptive tests as well

--- libraries/botbuilder-dialogs-adaptive-tests/src/recognizerSet.test.ts
+++ libraries/botbuilder-dialogs-adaptive-tests/src/recognizerSet.test.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import 'mocha';
+import * as path from 'path';
 import { TestRunner } from './testing';
 
 describe('RecognizerSetTests', function() {
     this.timeout(5000);
-    const testRunner = new TestRunner('resources/RecognizerSetTests');
+    const testRunner = new TestRunner(path.join(__dirname,  '../resources/RecognizerSetTests'));
 
     it('Merge', async () => {
         await testRunner.runTestScript('RecognizerSetTests_Merge');
