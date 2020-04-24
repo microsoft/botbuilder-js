f5b2410acf970c6bd1a53fea529bb39e4a9d572f
Michael Richardson <v-micric@microsoft.com>
2020-04-24 12:03:12 -0700

    added locale to ConversationReference

--- libraries/botframework-schema/src/index.ts
+++ libraries/botframework-schema/src/index.ts
@@ -257,6 +257,14 @@ export interface ConversationReference {
    */
   user?: ChannelAccount;
   /**
+   * A locale name for the contents of the text field.
+   * The locale name is a combination of an ISO 639 two- or three-letter
+   * culture code associated with a language and an ISO 3166 two-letter
+   * subculture code associated with a country or region.
+   * The locale name can also correspond to a valid BCP-47 language tag.
+   */
+  locale?: string;
+  /**
    * Bot participating in this conversation
    */
   bot: ChannelAccount;
