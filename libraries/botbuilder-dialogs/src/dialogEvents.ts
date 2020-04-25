7e2a1bc7d45915a7287f5a60798894a978025aab
Steven Ickman <stevenic@microsoft.com>
2020-04-25 11:10:03 -0700

    Updates to match parity with C#

--- libraries/botbuilder-dialogs/src/dialogEvents.ts
+++ libraries/botbuilder-dialogs/src/dialogEvents.ts
@@ -7,10 +7,33 @@
  */
 
 export class DialogEvents {
+    /**
+     * Event fired by a dialog to indicate that its `beginDialog()` method has been called.
+     */
     static readonly beginDialog = "beginDialog";
+
+    /**
+     * Event fired when `DialogContext.repromptDialog()` is called.
+     */
     static readonly repromptDialog = "repromptDialog";
+
+    /**
+     * Event fired when a dialog is being canceled.
+     */
     static readonly cancelDialog = "cancelDialog";
+
+    /**
+     * Event fired when an activity is received from the adapter (or a request to reprocess an activity.)
+     */
     static readonly activityReceived = "activityReceived";
+
+    /**
+     * Event fired when the system has detected that deployed code has changed the execution of dialogs between turns.
+     */
     static readonly versionChanged = "versionChanged";
+
+    /**
+     * Event fired when there was an exception thrown in the system.
+     */
     static readonly error = "error";
 }
