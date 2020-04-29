7beab9009063e869c328358faa242cfdd9250131
Michael Richardson <v-micric@microsoft.com>
2020-04-29 09:28:41 -0700

    revert to single-test timeout increase

--- libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
+++ libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
@@ -37,7 +37,7 @@ async function getTurnContext(locale, generator) {
 }
 
 describe('LGLanguageGenerator', function() {
-    this.timeout(5000);
+    this.timeout(3000);
 
     it('TestNotFoundTemplate', async function() {
         const context = getTurnContext('');
@@ -46,6 +46,7 @@ describe('LGLanguageGenerator', function() {
     });
 
     it('TestMultiLangImport', async function() {
+        this.timeout(5000);
         const lgResourceGroup = await LanguageResourceLoader.groupByLocale(resourceExplorer);
 
         let resource = await resourceExplorer.getResource('a.en-US.lg');
