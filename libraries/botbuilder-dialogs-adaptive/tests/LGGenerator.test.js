5f07a841fa90904fa405e2b525089d14f65b6c72
Michael Richardson <v-micric@microsoft.com>
2020-04-29 09:16:47 -0700

    increase timeout for suite, not test

--- libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
+++ libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
@@ -37,7 +37,7 @@ async function getTurnContext(locale, generator) {
 }
 
 describe('LGLanguageGenerator', function() {
-    this.timeout(3000);
+    this.timeout(5000);
 
     it('TestNotFoundTemplate', async function() {
         const context = getTurnContext('');
@@ -46,7 +46,6 @@ describe('LGLanguageGenerator', function() {
     });
 
     it('TestMultiLangImport', async function() {
-        this.timeout(5000);
         const lgResourceGroup = await LanguageResourceLoader.groupByLocale(resourceExplorer);
 
         let resource = await resourceExplorer.getResource('a.en-US.lg');
@@ -76,8 +75,6 @@ describe('LGLanguageGenerator', function() {
 
         result = await generator1.generate(getTurnContext(), '${greeting()}', undefined);
         assert.strictEqual(result, 'hi');
-
-
     });
 
     describe('TestMultiLangGenerator', () => {
