649dd25a2ccbed9e33575d061f0ad5869dfd6fa1
Steven Ickman <stevenic@microsoft.com>
2020-05-01 17:33:44 -0700

    Increased timeout

--- libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
+++ libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
@@ -37,7 +37,7 @@ async function getTurnContext(locale, generator) {
 }
 
 describe('LGLanguageGenerator', function() {
-    this.timeout(3000);
+    this.timeout(5000);
 
     it('TestNotFoundTemplate', async function() {
         const context = getTurnContext('');
