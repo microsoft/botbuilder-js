c04577b3788f7bfa15c78d626f0d60104b54edfe
Shuai Wang <shuwan@microsoft.com>
2020-05-07 11:21:31 +0800

    [Port]  [LG] Inject LG templates as a global expression function (#2188)
    * port inject lg to Expression
    
    * remove only in test
    
    * retrigger

--- libraries/botbuilder-lg/tests/lg.test.js
+++ libraries/botbuilder-lg/tests/lg.test.js
@@ -1052,4 +1052,32 @@ describe('LG', function() {
         var evaled = templates.evaluate('custom', {});
         assert.equal(evaled, 6.0);
     });
+
+    it('TestInjectLG', function() {
+        var templates = Templates.parseFile(GetExampleFilePath('./injectionTest/inject.lg'));
+
+        var {value: evaled, error} = Expression.parse('foo.bar()').tryEvaluate();
+        assert.strictEqual(3, evaled);
+
+        var {value: evaled, error} = Expression.parse('foo.cool(2)').tryEvaluate();
+        assert.strictEqual(3, evaled);
+
+        var {value: evaled, error} = Expression.parse('common.looking()').tryEvaluate();
+        assert.strictEqual('John', evaled);
+
+        var scope1 = { a: ['cat', 'dog'], b: 12.10, c: ['lion'] };
+        var {value: evaled, error} = Expression.parse('common.countTotal(a, c)').tryEvaluate(scope1);
+        assert.strictEqual(error, undefined);
+        assert.strictEqual(3, evaled);
+
+        var {value: evaled, error} = Expression.parse('common.countTotal()').tryEvaluate(scope1);
+        assert.strictEqual(error, `list1 is not a list or array. [countTotal]  Error occurred when evaluating '-\${count(union(list1,list2))}'.`);
+
+        var {value: evaled, error} = Expression.parse('common.countTotal(a, b, c)').tryEvaluate(scope1);
+        assert.strictEqual(error, `list2 is not a list or array. [countTotal]  Error occurred when evaluating '-\${count(union(list1,list2))}'.`);
+
+        var scope2 = { i: 1, j: 2, k: 3, l: 4 };
+        var {value: evaled, error} = Expression.parse('common.sumFourNumbers(i, j, k, l)').tryEvaluate(scope2);
+        assert.strictEqual(10, evaled);
+    });
 });
