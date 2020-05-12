91a54cbe83ea0fc865e7a3246a4d509029763e9b
hond <hond@microsoft.com>
2020-05-12 14:57:00 +0800

    fix ci

--- libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
+++ libraries/botbuilder-dialogs-adaptive/tests/LGGenerator.test.js
@@ -37,7 +37,7 @@ async function getTurnContext(locale, generator) {
 }
 
 describe('LGLanguageGenerator', function() {
-    this.timeout(6000);
+    this.timeout(8000);
 
     it('TestNotFoundTemplate', async function() {
         const context = getTurnContext('');
