3dbb060e8487541789e240bff2b3a83a7d997494
Zichuan Ma <zim@microsoft.com>
2020-06-18 16:45:36 +0800

    updated test resources

--- libraries/botbuilder-dialogs-adaptive-tests/src/injectLG.test.ts
+++ libraries/botbuilder-dialogs-adaptive-tests/src/injectLG.test.ts
@@ -3,7 +3,7 @@ import 'mocha';
 import * as path from 'path';
 import { TestRunner } from './testing';
 
-describe('ActionScopeTests', function() {
+describe('InjectLGTests', function() {
     this.timeout(5000);
     const testRunner = new TestRunner(path.join(__dirname,  '../resources/InjectLGTests'));
 
--- libraries/botbuilder-dialogs-adaptive-tests/src/lgGenerators.test.ts
+++ libraries/botbuilder-dialogs-adaptive-tests/src/lgGenerators.test.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import 'mocha';
 import * as path from 'path';
 import { TestRunner } from './testing';
 
-describe('ActionScopeTests', function() {
+describe('LGGeneratorTests', function() {
     this.timeout(5000);
-    const testRunner = new TestRunner(path.join(__dirname,  '../resources/LGGeneratorTests'));
+    const testRunner = new TestRunner(path.join(__dirname, '../resources/LGGeneratorTests'));
 
     it('MultiLandE2E', async () => {
         await testRunner.runTestScript('MultiLangE2E');
