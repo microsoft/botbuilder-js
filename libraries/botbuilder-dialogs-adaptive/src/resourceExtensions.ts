9ec6481dcc696ef933a4eb6db25afaaa4a1c663e
Zichuan Ma <zim@microsoft.com>
2020-06-18 17:15:39 +0800

    useResourceExplorer() can return DialogManager

--- libraries/botbuilder-dialogs-adaptive/src/resourceExtensions.ts
+++ libraries/botbuilder-dialogs-adaptive/src/resourceExtensions.ts
@@ -1,15 +1,27 @@
+/**
+ * @module botbuilder-dialogs-adaptive
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { DialogManager } from 'botbuilder-dialogs';
 import { ResourceExplorer } from 'botbuilder-dialogs-declarative';
 
 declare module 'botbuilder-dialogs/lib/dialogManager' {
     export interface DialogManager {
-        useResourceExplorer(resourceExplorer: ResourceExplorer): void;
+        useResourceExplorer(resourceExplorer: ResourceExplorer): DialogManager;
     }
 }
 
 export const resourceExplorerKey = Symbol('ResourceExplorer');
 
-DialogManager.prototype.useResourceExplorer = function(resourceExplorer: ResourceExplorer): void {
+/**
+ * Register ResourceExplorer into DialogManager.
+ */
+DialogManager.prototype.useResourceExplorer = function(resourceExplorer: ResourceExplorer): DialogManager {
     const _self = this as DialogManager;
     _self.initialTurnState.set(resourceExplorerKey, resourceExplorer);
+    return _self;
 };
