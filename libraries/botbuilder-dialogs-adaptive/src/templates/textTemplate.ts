3021d163df6775bfc179da1a8f390490042e1789
Zichuan Ma <zim@microsoft.com>
2020-06-24 11:06:17 +0800

    added missing header to text template

--- libraries/botbuilder-dialogs-adaptive/src/templates/textTemplate.ts
+++ libraries/botbuilder-dialogs-adaptive/src/templates/textTemplate.ts
@@ -1,3 +1,11 @@
+/**
+ * @module botbuilder-dialogs-adaptive
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { DialogContext } from 'botbuilder-dialogs';
 import { TemplateInterface } from '../template';
 import { LanguageGenerator } from '../languageGenerator';
