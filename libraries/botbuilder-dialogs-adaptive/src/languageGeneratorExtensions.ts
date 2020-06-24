0538ee2031c003d067f304ea6843f4129fbc0686
Zichuan Ma <zim@microsoft.com>
2020-06-24 11:18:59 +0800

    added comments to language generator extensions

--- libraries/botbuilder-dialogs-adaptive/src/languageGeneratorExtensions.ts
+++ libraries/botbuilder-dialogs-adaptive/src/languageGeneratorExtensions.ts
@@ -13,6 +13,9 @@ import { LanguageGenerator } from './languageGenerator';
 import { resourceExplorerKey } from './resourceExtensions';
 import { LanguagePolicy } from './languagePolicy';
 
+/**
+ * redeclare module to inject functions
+ */
 declare module 'botbuilder-dialogs/lib/dialogManager' {
     export interface DialogManager {
         useLanguageGeneration(lg?: string | LanguageGenerator): DialogManager;
@@ -22,8 +25,10 @@ declare module 'botbuilder-dialogs/lib/dialogManager' {
 
 export const languageGeneratorKey = Symbol('LanguageGenerator');
 export const languageGeneratorManagerKey = Symbol('LanguageGeneratorManager');
-export const languagePolicyKey = Symbol('LanguagePolicy');
 
+/**
+ * Register default LG file or a language generator as default language generator.
+ */
 DialogManager.prototype.useLanguageGeneration = function(lg?: string | LanguageGenerator): DialogManager {
     const _self = this as DialogManager;
     const resourceExplorer: ResourceExplorer = _self.initialTurnState.get(resourceExplorerKey) || new ResourceExplorer();
@@ -48,6 +53,11 @@ DialogManager.prototype.useLanguageGeneration = function(lg?: string | LanguageG
     return _self;
 };
 
+export const languagePolicyKey = Symbol('LanguagePolicy');
+
+/**
+ * Register language policy as default policy.
+ */
 DialogManager.prototype.useLanguagePolicy = function(policy: LanguagePolicy): DialogManager {
     const _self = this as DialogManager;
     _self.initialTurnState.set(languagePolicyKey, policy);
