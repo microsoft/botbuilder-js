c286df36452b37acd9c040d7f81e3430b1891cfc
Joel Mut <62260472+sw-joelmut@users.noreply.github.com>
2020-07-07 12:00:20 -0300

    Encapsulate tests in constructor()

--- libraries/botbuilder/tests/statusCodeError.test.js
+++ libraries/botbuilder/tests/statusCodeError.test.js
@@ -8,39 +8,41 @@ const { StatusCodeError } = require('../');
 const { StatusCodes } = require('botbuilder-core');
 
 describe(`StatusCodeError`, function() {
-    it(`should work with a message.`, done => {
-        try {
-            const message = 'This is an error message';
-            const error = new StatusCodeError(StatusCodes.NOT_FOUND, message);
+    describe('constructor()', () => {
+        it(`should work with a message.`, done => {
+            try {
+                const message = 'This is an error message';
+                const error = new StatusCodeError(StatusCodes.NOT_FOUND, message);
 
-            assert.strictEqual(error.message, message, `message should be equal to "${message}".`)
-            assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
-            done();
-        } catch (error) {
-            done(error);
-        }
-    });
+                assert.strictEqual(error.message, message, `message should be equal to "${message}".`)
+                assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
+                done();
+            } catch (error) {
+                done(error);
+            }
+        });
 
-    it(`should work without a message.`, done => {
-        try {
-            const error = new StatusCodeError(StatusCodes.NOT_FOUND);
+        it(`should work without a message.`, done => {
+            try {
+                const error = new StatusCodeError(StatusCodes.NOT_FOUND);
 
-            assert.strictEqual(error.message, '', 'message should be empty.')
-            assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
-            done();
-        } catch (error) {
-            done(error);
-        }
-    });
+                assert.strictEqual(error.message, '', 'message should be empty.')
+                assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
+                done();
+            } catch (error) {
+                done(error);
+            }
+        });
 
-    it(`should statusCode be undefined if not passed as a parameter.`, done => {
-        try {
-            const error = new StatusCodeError();
+        it(`should statusCode be undefined if not passed as a parameter.`, done => {
+            try {
+                const error = new StatusCodeError();
 
-            assert.strictEqual(error.statusCode, undefined, 'statusCode should be undefined.')
-            done();
-        } catch (error) {
-            done(error);
-        }
+                assert.strictEqual(error.statusCode, undefined, 'statusCode should be undefined.')
+                done();
+            } catch (error) {
+                done(error);
+            }
+        });
     });
 });
