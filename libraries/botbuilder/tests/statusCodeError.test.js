897283bbe8a7e010c6b79286c82761a0971e8717
Joel Mut <62260472+sw-joelmut@users.noreply.github.com>
2020-07-07 11:51:19 -0300

    Add statusCodeError unit tests

---
+++ libraries/botbuilder/tests/statusCodeError.test.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const assert = require('assert');
+const { StatusCodeError } = require('../');
+const { StatusCodes } = require('botbuilder-core');
+
+describe(`StatusCodeError`, function() {
+    it(`should work with a message.`, done => {
+        try {
+            const message = 'This is an error message';
+            const error = new StatusCodeError(StatusCodes.NOT_FOUND, message);
+
+            assert.strictEqual(error.message, message, `message should be equal to "${message}".`)
+            assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+
+    it(`should work without a message.`, done => {
+        try {
+            const error = new StatusCodeError(StatusCodes.NOT_FOUND);
+
+            assert.strictEqual(error.message, '', 'message should be empty.')
+            assert.strictEqual(error.statusCode, StatusCodes.NOT_FOUND, `statusCode should be the code ${StatusCodes.NOT_FOUND}`)
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+
+    it(`should statusCode be undefined if not passed as a parameter.`, done => {
+        try {
+            const error = new StatusCodeError();
+
+            assert.strictEqual(error.statusCode, undefined, 'statusCode should be undefined.')
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
