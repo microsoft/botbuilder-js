0e189f335281d57674e4cad105e81b47e5f32a07
Zichuan Ma <zim@microsoft.com>
2020-07-08 16:33:24 +0800

    renamed botbuilder-dialogs-adaptive-tests

---
+++ libraries/botbuilder-dialogs-adaptive-testing/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @module botbuilder-dialogs-adaptive-testing
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './actions';
+export * from './adaptiveTestAdapter';
+export * from './testScript';
+export * from './testAction';
+export * from './testActions';
+export * from './testRunner';
---
+++ libraries/botbuilder-dialogs-adaptive-testing/src/testActions/userDelay.ts
@@ -0,0 +1,26 @@
+/**
+ * @module botbuilder-dialogs-adaptive-testing
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { TurnContext } from 'botbuilder-core';
+import { TestAction } from '../testAction';
+import { AdaptiveTestAdapter } from '../adaptiveTestAdapter';
+
+export interface UserDelayConfiguration {
+    timespan?: number;
+}
+
+export class UserDelay implements TestAction {
+    /**
+     * The timespan in milliseconds to delay.
+     */
+    public timespan: number;
+
+    public async execute(_testAdapter: AdaptiveTestAdapter, _callback: (context: TurnContext) => Promise<any>): Promise<any> {
+        await Promise.resolve(resolve => setTimeout(resolve, this.timespan));
+    }
+}
