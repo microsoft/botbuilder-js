59b9228166e53809c0a40a34295dbc77ed3b70d4
Song Zhaojie <songcs@live.com>
2020-07-09 09:31:11 +0800

    add luis event

--- libraries/botbuilder-dialogs-adaptive/src/luis/luisAdaptiveRecognizer.ts
+++ libraries/botbuilder-dialogs-adaptive/src/luis/luisAdaptiveRecognizer.ts
@@ -6,10 +6,10 @@
  * Licensed under the MIT License.
  */
 import { Recognizer } from '../recognizers';
-import { StringExpression, ArrayExpression } from 'adaptive-expressions';
-import { LuisPredictionOptions, LuisRecognizerOptionsV3, LuisRecognizer, LuisApplication } from 'botbuilder-ai';
+import { StringExpression, ArrayExpression, BoolExpression } from 'adaptive-expressions';
+import { LuisPredictionOptions, LuisRecognizerOptionsV3, LuisRecognizer, LuisApplication, LuisTelemetryConstants} from 'botbuilder-ai';
 import { Configurable, DialogContext } from 'botbuilder-dialogs';
-import { BotTelemetryClient, Activity, ActivityTypes } from 'botbuilder-core';
+import { BotTelemetryClient, Activity, NullTelemetryClient, RecognizerResult } from 'botbuilder-core';
 
 export class LuisAdaptiveRecognizer extends Configurable implements Recognizer {
     /**
@@ -21,6 +21,11 @@ export class LuisAdaptiveRecognizer extends Configurable implements Recognizer {
      * LUIS application ID.
      */
     public applicationId: StringExpression;
+
+    /**
+     * The flag to indicate in personal information should be logged in telemetry.
+     */
+    public logPersonalInformation: BoolExpression = new BoolExpression('=settings.telemetry.logPersonalInformation');
 
     /**
      * LUIS dynamic list.
@@ -55,9 +60,9 @@ export class LuisAdaptiveRecognizer extends Configurable implements Recognizer {
     /**
      * Telemetry client.
      */
-    public telemetryClient: BotTelemetryClient;
+    public telemetryClient: BotTelemetryClient = new NullTelemetryClient();
 
-    public async recognize(dialogContext: DialogContext, activity: Activity) {
+    public async recognize(dialogContext: DialogContext, activity: Activity, telemetryProperties?: { [key: string]: string }, telemetryMetrics?: { [key: string]: number }) {
         // Validate passed in activity matches turn activity 
         const context = dialogContext.context;
         const utteranceMatches: boolean = !activity || 
@@ -77,7 +82,15 @@ export class LuisAdaptiveRecognizer extends Configurable implements Recognizer {
 
         // Create and call wrapper
         const wrapper = new LuisRecognizer(application, this.recognizerOptions(dialogContext));
-        return await wrapper.recognize(context);
+
+        var result = await wrapper.recognize(context);
+        this.telemetryClient.trackEvent(
+            {
+                name: 'LuisResult',
+                properties: this.fillRecognizerResultTelemetryProperties(result, telemetryProperties, dialogContext),
+                metrics: telemetryMetrics
+            });
+        return result;
     }
 
     /**
@@ -93,5 +106,60 @@ export class LuisAdaptiveRecognizer extends Configurable implements Recognizer {
             options.dynamicLists = this.dynamicLists.getValue(dialogContext.state);
         }
         return options;
+    }
+    /**
+     * Fills the event properties for LuisResult event for telemetry.
+     * These properties are logged when the recognizer is called.
+     * @param recognizerResult Last activity sent from user.
+     * @param telemetryProperties Additional properties to be logged to telemetry with the LuisResult event.
+     * @param dialogContext Dialog context.
+     * @returns A dictionary that is sent as properties to BotTelemetryClient.trackEvent method for the LuisResult event.
+     */
+    protected  fillRecognizerResultTelemetryProperties(recognizerResult: RecognizerResult, telemetryProperties: { [key: string]: string }, dialogContext: DialogContext): { [key: string]: string } {
+        var logPersonalInfoVnE = this.logPersonalInformation.tryGetValue(dialogContext.state);
+        var logPersonalInfo = logPersonalInfoVnE.value;
+        var error = logPersonalInfoVnE.error;
+
+        var applicationIdVnE = this.applicationId.tryGetValue(dialogContext.state);
+        var applicationId = applicationIdVnE.value;
+        var error2 = applicationIdVnE.error;
+        
+        const topLuisIntent: string = LuisRecognizer.topIntent(recognizerResult);
+        const intentScore: number = (recognizerResult.intents[topLuisIntent] && 'score' in recognizerResult.intents[topLuisIntent]) ?
+            recognizerResult.intents[topLuisIntent].score : 0;
+
+        // Add the intent score and conversation id properties
+        const properties: { [key: string]: string } = {};
+        properties[LuisTelemetryConstants.applicationIdProperty] = applicationId;
+        properties[LuisTelemetryConstants.intentProperty] = topLuisIntent;
+        properties[LuisTelemetryConstants.intentScoreProperty] = intentScore.toString();
+        properties[LuisTelemetryConstants.fromIdProperty] = dialogContext.context.activity.from.id;
+
+        if (recognizerResult.sentiment) {
+            if (recognizerResult.sentiment.label) {
+                properties[LuisTelemetryConstants.sentimentLabelProperty] = recognizerResult.sentiment.label;
+            }
+
+            if (recognizerResult.sentiment.score) {
+                properties[LuisTelemetryConstants.sentimentScoreProperty] = recognizerResult.sentiment.score.toString();
+            }
+        }
+
+        // Log entity names
+        if (recognizerResult.entities) {
+            properties[LuisTelemetryConstants.entitiesProperty] = JSON.stringify(recognizerResult.entities);
+        }
+
+        // Use the logPersonalInfo flag to toggle logging PII data, text is a common example
+        if (logPersonalInfo && dialogContext.context.activity.text) {
+            properties[LuisTelemetryConstants.questionProperty] = dialogContext.context.activity.text;
+        }
+
+        // Additional Properties can override "stock" properties.
+        if (telemetryProperties) {
+            return Object.assign({}, properties, telemetryProperties);
+        }
+
+        return properties;
     }
 }
