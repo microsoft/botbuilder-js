cedc262ad790200ce9a823fef3a7cd371ec95fe4
Song Zhaojie <songcs@live.com>
2020-07-10 16:12:24 +0800

    bug fix

--- libraries/botbuilder-dialogs-adaptive/src/telemetryExtensions.ts
+++ libraries/botbuilder-dialogs-adaptive/src/telemetryExtensions.ts
@@ -15,16 +15,13 @@ export const telemetryClientKey = Symbol('telemetryClient');
 export class TelemetryExtensions {
     /**
      * Configures the telemetry client to use.
-     */
-
-    /**
      * @param dialogManager DialogManager to configure.
      * @param telemetryClient BotTelemetryClient instance to use.
      * @returns DialogManager.
      */
     public static useTelemetry(dialogManager: DialogManager, telemetryClient: BotTelemetryClient): DialogManager {
         dialogManager.initialTurnState.set(telemetryClientKey, telemetryClient);
-        dialogManager.dialogSet.telemetryClient = telemetryClient;
+        dialogManager.dialogs.telemetryClient = telemetryClient;
         return dialogManager;
     }
 }
