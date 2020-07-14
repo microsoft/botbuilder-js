f8808b7315c758eb54af795f0cd4e62d3bb6f04d
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-13 17:51:38 -0700

    Added zip-deployment step

--- libraries/streaming-e2e-tests/bot/bot.js
+++ libraries/streaming-e2e-tests/bot/bot.js
@@ -8,7 +8,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text } - streaming bot`;
+            const replyText = `Echo: ${ context.activity.text } - streaming bot!!`;
             await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
