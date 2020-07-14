d0c403e05c6e7bd056ae841dab00c1fb996a1868
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-14 16:08:30 -0700

    made 'botbuilder' string variable at job level to rid of magic string

--- libraries/streaming-e2e-tests/build/get-latest-bbjs-daily-version.ps1
+++ libraries/streaming-e2e-tests/build/get-latest-bbjs-daily-version.ps1
@@ -1,17 +1,15 @@
 param($AccessToken, $PackageName)
 
-$myGetPersonalAccessToken = $AccessToken;
 $myGetFeedName = "botbuilder-v4-js-daily";
-$packageName = $PackageName;
 
-$url = "https://botbuilder.myget.org/F/$myGetFeedName/auth/$myGetPersonalAccessToken/api/v2/feed-state";
+$url = "https://botbuilder.myget.org/F/$myGetFeedName/auth/$AccessToken/api/v2/feed-state";
 
-Write-Host "Get latest $packageName version number from MyGet $myGetFeedName";
+Write-Host "Get latest $PackageName version number from MyGet $myGetFeedName";
 $result = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/json";
 
-$package = $result.packages | Where-Object {$_.id -eq $packageName};
+$package = $result.packages | Where-Object {$_.id -eq $PackageName};
 [string]$latestVersion = $package.versions[-1];
 
 $package.id;
 $latestVersion;
-"##vso[task.setvariable variable=LatestVersion_$packageName;]$latestVersion";
+"##vso[task.setvariable variable=LatestVersion_$PackageName;]$latestVersion";
