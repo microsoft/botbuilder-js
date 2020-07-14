3e912d0ecfda16668de84f950ad6381e4f141104
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-13 17:52:40 -0700

    Added zip-deployment steps--with feeling! this time

--- libraries/streaming-e2e-tests/create-npmrc-for-myget-feed.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location -Path "$(System.DefaultWorkingDirectory)/samples/javascript_nodejs/02.echo-bot"
-
-New-Item -Path . -Name ".npmrc" -ItemType "file" -Value "registry=https://botbuilder.myget.org/F/botbuilder-v4-js-daily/npm/"
