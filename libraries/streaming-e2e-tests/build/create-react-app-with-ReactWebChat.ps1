a0c647b74acb69f67d4e75dbc69da24d92379f15
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-15 15:12:32 -0700

    Finished creating react app with ReactWebChat

--- libraries/streaming-e2e-tests/build/create-react-app-with-ReactWebChat.ps1
+++ libraries/streaming-e2e-tests/build/create-react-app-with-ReactWebChat.ps1
@@ -1,2 +1,18 @@
+param($RootReactPiecesDir)
+
+# Create React App
+npx create-react-app react-app
+
+# Install WebChat into react-app
+Set-Location -Path "$RootReactPiecesDir\react-app"
+npm install botframework-webchat
+
+# In react-app, delete files we will be replacing with our custom code that uses `ReactWebChat`
+Remove-Item src/App.js, src/index.js, public/index.html
+
+# Copy Custom files into react-app
+copy ..\custom-app-code\App.js, ..\custom-app-code\index.js .\src\
+copy ..\custom-app-code\index.html .\public
+
 
 
