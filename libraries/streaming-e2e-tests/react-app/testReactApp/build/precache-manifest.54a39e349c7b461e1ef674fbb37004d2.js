c6f4ff8455e9d2fa8d1e61d3cd8a5b67db123cca
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-17 00:14:40 -0700

    temporarily removed build/ folder from .gitignore for testing

---
+++ libraries/streaming-e2e-tests/react-app/testReactApp/build/precache-manifest.54a39e349c7b461e1ef674fbb37004d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72e5a3f3718edb8628652bc37e35c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac38adc4ce7438d1b26",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0586b98ee4e40396e87a",
+    "url": "/static/js/2.5cc09d09.chunk.js"
+  },
+  {
+    "revision": "cafd23aecec3f6c764e7e7a6f9780ae8",
+    "url": "/static/js/2.5cc09d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac38adc4ce7438d1b26",
+    "url": "/static/js/main.64d7dd89.chunk.js"
+  },
+  {
+    "revision": "56a8ae5b0e154bbf1c77",
+    "url": "/static/js/runtime-main.0b2b112a.js"
+  }
+]);
