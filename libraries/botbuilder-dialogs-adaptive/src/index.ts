5278827169521ecef260e07a0c315f459d5670fa
Zichuan Ma <zim@microsoft.com>
2020-07-24 12:46:26 +0800

    Scoped services (#2382)
    * reordered properties of dialog context
    
    * added services to dialog context
    
    * updated constructor of dialog context to align with c# sdk
    
    * map turn state into services in dialog manager
    
    * removed push/pop scoped services in adaptive dialog
    
    * promote initial turn state into dialog context's services
    
    * updated template interface to work with dialog context instead of turn context
    
    * updated language generator to reflect template changes
    
    * updated actions and inputs as well
    
    * udpated tests for generators to work with new api
    
    * swapped key/value in forEach()
    
    * extended dialog manager to use language generation
    
    * extended dialog manager to use resource explorer
    
    * use symbol as keys
    
    * require extension files to use their side effects
    
    * get lg/lgm from dialogcontext.services
    
    * updated language generator tests to reflect changes
    
    * updated test script and test runner to use dialog manager extensions
    
    * updated test resources
    
    * removed LanguageGenerationMiddleware
    
    * LanguageGeneratorManager's initialization should be synchronized
    
    * useResourceExplorer() can return DialogManager
    
    * updated tests for LG generator
    
    * updated test script to reflect changes
    
    * updated expression properties to stop parsing expression at setValue()
    
    * removed unused test resources
    
    * added required testing resources
    
    * extend dialog manage to use language policy
    
    * fixed lgm to check if resource is file resource
    
    * ignored expressionProperty.test.js
    
    * improved test coverage on expression properties
    
    * added missing comments for expression properties
    
    * added missing header to text template
    
    * removed non-existent parameter in dialog context
    
    * added comments to language generator extensions
    
    * use static funtions instead of extensions
    
    * updated test script to reflect changes
    
    * removed unused key
    
    * fixed build issues
    
    * Update libraries/botbuilder-dialogs-adaptive/src/languageGeneratorExtensions.ts
    
    Co-authored-by: Steven Gum <14935595+stevengum@users.noreply.github.com>
    
    * added docs
    
    * updated language policy to extend Map
    
    * adjusted import orders
    
    * fixed build issues
    
    * clean up code in lg extensions
    
    * type check in string expression
    
    * revert throwing error on unexpected type in StringExpression
    
    New discussion issue via #2568
    
    Co-authored-by: Steven Gum <14935595+stevengum@users.noreply.github.com>

--- libraries/botbuilder-dialogs-adaptive/src/index.ts
+++ libraries/botbuilder-dialogs-adaptive/src/index.ts
@@ -5,6 +5,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+
 export * from './actions';
 export * from './actionContext';
 export * from './adaptiveDialog';
@@ -16,19 +17,18 @@ export { DialogExpressionConverter } from './converters';
 export * from './expressions';
 export * from './generators';
 export * from './input';
+export * from './languageGenerator';
+export * from './languageGeneratorExtensions';
+export * from './languagePolicy';
+export * from './languageResourceLoader';
 export * from './luis';
+export * from './propertySchema';
+export * from './qnaMaker';
 export * from './recognizers';
+export * from './resourceExtensions';
+export * from './schemaHelper';
 export * from './selectors';
 export * from './skillExtensions';
 export * from './templates';
-export * from './adaptiveDialog';
-export * from './languageGenerationMiddleware';
-export * from './languageGenerator';
-export * from './languagePolicy';
-export * from './languageResourceLoader';
-export * from './qnaMaker';
-export * from './propertySchema';
-export * from './schemaHelper';
 export * from './template';
 export * from './triggerSelector';
-
