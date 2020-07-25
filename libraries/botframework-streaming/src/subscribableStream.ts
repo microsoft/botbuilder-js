52df2c619e553d08b0f56b5daa978ecdbb05a2d4
stevengum <14935595+stevengum@users.noreply.github.com>
2020-07-24 22:23:44 -0700

    replace stream dependency with stream-browserify

--- libraries/botframework-streaming/src/subscribableStream.ts
+++ libraries/botframework-streaming/src/subscribableStream.ts
@@ -5,9 +5,23 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { Duplex, DuplexOptions } from 'stream';
 
-export class SubscribableStream extends Duplex {
+import { Duplex, DuplexOptions, Readable } from 'stream-browserify';
+
+export interface SubscribableStream {
+    end?(cb?: () => void): void;
+    end?(chunk: any, cb?: () => void): void;
+    end?(chunk: any, encoding?: string, cb?: () => void): void;
+    push?(chunk: any): boolean;
+    push?(chunk: any, encoding: string): boolean;
+    push?(chunk: any, encoding?: string): boolean;
+    read(size?: number): any;
+    write?(chunk: any): boolean;
+    write?(chunk: any, encoding: string): boolean;
+    write?(chunk: any, encoding: string, callback: (error?: Error | null) => void): boolean;
+}
+
+export class SubscribableStream extends Duplex implements SubscribableStream {
     public length: number = 0;
 
     private readonly bufferList: Buffer[] = [];
@@ -18,7 +32,7 @@ export class SubscribableStream extends Duplex {
     }
 
     public _write(chunk: any, encoding: string, callback: (error?: Error | null) => void): void {
-        let buffer = Buffer.from(chunk);
+        const buffer = Buffer.from(chunk);
         this.bufferList.push(buffer);
         this.length += chunk.length;
         if (this._onData) {
@@ -30,12 +44,12 @@ export class SubscribableStream extends Duplex {
     public _read(size: number): void {
         if (this.bufferList.length === 0) {
             // null signals end of stream
-            this.push(null);
+            (this as Readable).push(null);
         } else {
             let total = 0;
             while (total < size && this.bufferList.length > 0) {
-                let buffer = this.bufferList[0];
-                this.push(buffer);
+                const buffer = this.bufferList[0];
+                (this as Readable).push(buffer);
                 this.bufferList.splice(0, 1);
                 total += buffer.length;
             }
