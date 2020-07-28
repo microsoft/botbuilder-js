8ee1531e06e4dfae812bda803af42b42ab54f4e1
Santiago Grangetto <santiago.grangetto@southworks.com>
2020-07-28 11:41:43 -0300

    Move storage var to be reused and fix eslint

--- libraries/botbuilder-core/tests/memoryTranscriptStore.test.js
+++ libraries/botbuilder-core/tests/memoryTranscriptStore.test.js
@@ -5,70 +5,64 @@ const base = require('./transcriptStoreBaseTest');
 
 const print = (o) => {
     return JSON.stringify(o, null, '  ');
-}
+};
 
-testStorage = function () {
+testStorage = function() {
+    const storage = new MemoryTranscriptStore();
 
-    it('bad args', function () {
-        let storage = new MemoryTranscriptStore();
+    it('bad args', function() {
         return base._badArgs(storage)
             .then(messages => {
                 assert(messages.every(message => message.startsWith('expected error')));
             })
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
-    it('log activity', function () {
-        let storage = new MemoryTranscriptStore();
+    it('log activity', function() {
         return base._logActivity(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
-    it('logs multiple activities', function () {
-        let storage = new MemoryTranscriptStore();
+    it('logs multiple activities', function() {
         return base._logMultipleActivities(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
     it('delete transcript', function() {
-        let storage = new MemoryTranscriptStore();
         return base._deleteTranscript(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
     it('get transcript activities', function() {
-        let storage = new MemoryTranscriptStore();
         return base._getTranscriptActivities(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
     it('get transcript activities with state date', function() {
-        let storage = new MemoryTranscriptStore();
         return base._getTranscriptActivitiesStartDate(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
 
     it('list transcripts', function() {
-        let storage = new MemoryTranscriptStore();
         return base._listTranscripts(storage)
             .then(() => assert(true))
             .catch(reason =>
-                assert(false, `should not throw: ${print(reason)}`))
-    })
-}
+                assert(false, `should not throw: ${ print(reason) }`));
+    });
+};
 
-describe('MemoryTranscriptStore', function () {
+describe('MemoryTranscriptStore', function() {
     this.timeout(10000);
     testStorage();
 });
