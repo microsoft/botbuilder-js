a3a242c3755fd73089729f6aaf65563baf251aea
Hongyang Du (hond) <hond@microsoft.com>
2020-07-28 09:12:38 +0800

    [PORT]Refactor AdaptiveExpressions method GetStandardFunctions() (#2502)
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * add comments
    
    * refine
    
    * refine
    
    * fix error
    
    * refine
    
    * small fix
    
    * retrigger
    
    * fix comments
    
    * fix comments
    
    * fix comments

---
+++ libraries/adaptive-expressions/src/builtinFunctions/removeProperty.ts
@@ -0,0 +1,36 @@
+/**
+ * @module adaptive-expressions
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Expression } from '../expression';
+import { EvaluateExpressionDelegate, ExpressionEvaluator } from '../expressionEvaluator';
+import { ExpressionType } from '../expressionType';
+import { FunctionUtils } from '../functionUtils';
+import { ReturnType } from '../returnType';
+
+/**
+ * Remove a property from an object and return the updated object.
+ */
+export class RemoveProperty extends ExpressionEvaluator {
+    public constructor() {
+        super(ExpressionType.RemoveProperty, RemoveProperty.evaluator(), ReturnType.Object, RemoveProperty.validator);
+    }
+
+    private static evaluator(): EvaluateExpressionDelegate {
+        return FunctionUtils.apply(
+            (args: any[]): any => {
+                const temp: any = args[0];
+                delete temp[String(args[1])];
+
+                return temp;
+            });
+    }
+
+    private static validator(expression: Expression): void {
+        FunctionUtils.validateOrder(expression, undefined, ReturnType.Object, ReturnType.String);
+    }
+}
