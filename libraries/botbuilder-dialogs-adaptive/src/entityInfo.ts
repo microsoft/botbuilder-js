210c959090c336dae20f685d4fb0e4b2bb79e8f2
Zichuan Ma <zim@microsoft.com>
2020-07-29 02:33:58 +0800

    [PORT] Add support for operations on arrays (#2535)
    * expand dialogPath to have expectedProperties
    
    * removed onClearProperty
    
    * added operation/property to EntityInfo
    
    * changed operation path in onAssignEntity
    
    * removed unused onClearProperty from adaptiveEvents
    
    * added EntityAssignments
    
    * removed EntityEvents
    
    * refactored EntityAssignment
    
    * refactored AdaptiveDialog
    
    * moved normalizeEntities() into adaptive dialog
    
    * copy $ like property in schema helper
    
    * initialize set of used entity type to have utterance
    
    * added docs
    
    * renamed nextAssignment to getter
    
    Co-authored-by: Steven Gum <14935595+stevengum@users.noreply.github.com>

--- libraries/botbuilder-dialogs-adaptive/src/entityInfo.ts
+++ libraries/botbuilder-dialogs-adaptive/src/entityInfo.ts
@@ -5,9 +5,10 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { TurnPath, DialogPath } from 'botbuilder-dialogs';
-import { ActionContext } from './actionContext';
 
+/**
+ * Extended information about an entity including $instance data.
+ */
 export interface EntityInfo {
     /**
      * Name of entity.
@@ -18,6 +19,16 @@ export interface EntityInfo {
      * Value of entity.
      */
     value: any;
+
+    /**
+     * Value of property if any.
+     */
+    property: string;
+
+    /**
+     * Operation to apply to entity.
+     */
+    operation: string;
 
     /**
      * Position where entity starts in utterance.
@@ -106,97 +117,6 @@ export class EntityInfo {
      */
     public static covers(_this: Partial<EntityInfo>, entity: Partial<EntityInfo>): boolean {
         return _this.start <= entity.start && _this.end >= entity.end && _this.end - _this.start > entity.end - entity.start;
-    }
-
-    /**
-     * @private
-     * Normalizes the entities returned from a recognizer.
-     * @remarks
-     * We have four kinds of ambiguity to deal with:
-     * - Entity: Ambiguous interpretation of entity value: (peppers -> [green peppers, red peppers]  Tell this by entity value is array.  Doesn't matter if property singleton or not. Ask.
-     * - Text: Ambiguous interpretation of text: (3 -> age or number) Identify by overlapping entities. Resolve by greater coverage, expected entity, ask.
-     * - Singleton: two different entities which could fill property singleton.  Could be same type or different types.  Resolve by rule priority.
-     * - Property: Which property should an entity go to?  Resolve by expected, then ask.
-     * @param recognized Recognizer results to normalize.
-     * @param eventCounter Watermark for the current turn.
-     */
-    public static normalizeEntities(actionContext: ActionContext): NormalizedEntityInfos {
-        const entityToInfo: NormalizedEntityInfos = {};
-        const recognized = actionContext.state.getValue(TurnPath.recognized);
-        const text = recognized.text;
-        const entities: { [name: string]: any[] } = recognized.entities || {};
-        const turn = actionContext.state.getValue(DialogPath.eventCounter);
-        const metaData = entities['$instance'] as object;
-        for (const name in entities) {
-            const values = entities[name];
-            if (!name.startsWith('$') && Array.isArray(values)) {
-                const instances: any[] = metaData ? metaData[name] : undefined;
-                for (var i = 0; i < values.length; ++i) {
-                    const val = values[i];
-                    const instance: any = instances ? instances[i] : {};
-
-                    // Create info(s) array on first access
-                    if (!entityToInfo.hasOwnProperty(name)) {
-                        entityToInfo[name] = [];
-                    }
-
-                    // Initialize info object
-                    const info: Partial<EntityInfo> = {
-                        whenRecognized: turn,
-                        name: name,
-                        value: val,
-                        start: instance['startIndex'] || 0,
-                        end: instance['endIndex'] || 0,
-                        text: instance['text'] || '',
-                        type: instance['type'],
-                        role: instance['role'],
-                        score: instance['score'] || 0.0,
-                    };
-
-                    // Set priority and calculate coverage
-                    info.priority = info.role == undefined ? 1 : 0;
-                    info.coverage = (info.end - info.start) / text.length;
-
-                    // Save to map
-                    entityToInfo[name].push(info);
-                }
-            }
-        }
-
-        // When there are multiple possible resolutions for the same entity that overlap, pick the 
-        // one that covers the most of the utterance.
-        for (const name in entityToInfo) {
-            const infos = entityToInfo[name];
-            infos.sort((entity1, entity2) => {
-                let val = 0;
-                if (entity1.start == entity2.start) {
-                    if (entity1.end > entity2.end) {
-                        val = -1;
-                    } else if (entity1.end < entity2.end) {
-                        val = +1;
-                    }
-                } else if (entity1.start < entity2.start) {
-                    val = -1;
-                } else {
-                    val = +1;
-                }
-
-                return val;
-            });
-            for (let i = 0; i < infos.length; ++i) {
-                const current = infos[i];
-                for (let j = i + 1; j < infos.length;) {
-                    const alt = infos[j];
-                    if (EntityInfo.covers(current, alt)) {
-                        infos.splice(j, 1);
-                    } else {
-                        ++j;
-                    }
-                }
-            }
-        }
-
-        return entityToInfo;
     }
 
     /**
