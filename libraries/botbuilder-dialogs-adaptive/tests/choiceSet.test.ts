ad48815854fdbf59bc295bcd2cd52e29f7aa61ba
Santiago Grangetto <santiago.grangetto@southworks.com>
2020-07-29 12:53:43 -0300

    move asserts to function and remove unused variables

--- libraries/botbuilder-dialogs-adaptive/tests/choiceSet.test.ts
+++ libraries/botbuilder-dialogs-adaptive/tests/choiceSet.test.ts
@@ -9,65 +9,63 @@ interface Bar {
     choices: ChoiceSet;
 }
 
-describe('ChoiceSetTests', function () {
+function testValues(value){
+    assert.equal('test1', value[0].value);
+    assert.equal('test2', value[1].value);
+    assert.equal('test3', value[2].value);
+}
+
+describe('ChoiceSetTests', function() {
     this.timeout(10000);
 
     it('TestExpression', async () => {
         const state = {
             choices: [
-                { value: "test1" } as Choice,
-                { value: "test2" } as Choice,
-                { value: "test3" } as Choice,
+                { value: 'test1' } as Choice,
+                { value: 'test2' } as Choice,
+                { value: 'test3' } as Choice,
             ]
-        }
-        const ep = new ObjectExpression<ChoiceSet>("choices");
-        const { value, error } = ep.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        };
+        const ep = new ObjectExpression<ChoiceSet>('choices');
+        const { value } = ep.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestValue', async () => {
-        const state = {}
+        const state = {};
         const ep = new ObjectExpression<ChoiceSet>(new ChoiceSet([{ value: 'test1' }, { value: 'test2' }, { value: 'test3' }]));
-        const { value, error } = ep.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        const { value } = ep.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestStringArrayAccess', async () => {
-        const state = {}
-        const stringArr = ['test1', 'test2', 'test3']
+        const state = {};
+        const stringArr = ['test1', 'test2', 'test3'];
         const ep = new ObjectExpression<ChoiceSet>(new ChoiceSet(stringArr));
-        const { value, error } = ep.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        const { value } = ep.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestConverterExpressionAccess', async () => {
         const state = {
             test: [
-                { value: "test1" } as Choice,
-                { value: "test2" } as Choice,
-                { value: "test3" } as Choice,
+                { value: 'test1' } as Choice,
+                { value: 'test2' } as Choice,
+                { value: 'test3' } as Choice,
             ]
-        }
+        };
 
         const sample = {
             choices: 'test'
-        }
+        };
 
         const ep = new ObjectExpression<ChoiceSet>(sample.choices);
-        const { value, error } = ep.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        const { value } = ep.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestConvertObjectAccess', async () => {
-        const state = {}
+        const state = {};
 
         const sample = {
             choices: [
@@ -75,21 +73,19 @@ describe('ChoiceSetTests', function () {
                 { value: 'test2' },
                 { value: 'test3' }
             ]
-        }
+        };
 
-        const json = JSON.stringify(sample)
-        const bar = JSON.parse(json) as Bar
+        const json = JSON.stringify(sample);
+        const bar = JSON.parse(json) as Bar;
         
         // TS doesn't run 'new' automatically unlike C#
         const choicesBar = new ObjectExpression<ChoiceSet>(bar.choices);
-        const { value, error } = choicesBar.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        const { value } = choicesBar.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestConvertStringAccess', async () => {
-        const state = {}
+        const state = {};
 
         const sample = {
             choices: [
@@ -97,33 +93,27 @@ describe('ChoiceSetTests', function () {
                 'test2',
                 'test3',
             ]
-        }
+        };
 
-        const json = JSON.stringify(sample)
-        const bar = JSON.parse(json) as Bar
-        bar.choices = new ChoiceSet(bar.choices)
+        const json = JSON.stringify(sample);
+        const bar = JSON.parse(json) as Bar;
+        bar.choices = new ChoiceSet(bar.choices);
 
         // TS doesn't run 'new' automatically unlike C#
         const choicesBar = new ObjectExpression<ChoiceSet>(bar.choices);
-        const { value, error } = choicesBar.tryGetValue(state);
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        const { value } = choicesBar.tryGetValue(state);
+        testValues(value);
     });
 
     it('TestConvertStringArray', async () => {
-        const state = {}
+        const sample = [];
+        sample.push('test1');
+        sample.push('test2');
+        sample.push('test3');
 
-        const sample = []
-        sample.push('test1')
-        sample.push('test2')
-        sample.push('test3')
+        const json = JSON.stringify(sample);
+        const value = new ChoiceSet(JSON.parse(json));
 
-        const json = JSON.stringify(sample)
-        const value = new ChoiceSet(JSON.parse(json))
-
-        assert.equal('test1', value[0].value);
-        assert.equal('test2', value[1].value);
-        assert.equal('test3', value[2].value);
+        testValues(value);
     });
 });
