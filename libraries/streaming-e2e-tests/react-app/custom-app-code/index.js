fd1a520ab3823df60f783954d75004f69b5eed80
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-31 09:49:37 -0700

    Using process.env.APPSETTING_WEBSITE_SITE_NAME in react app

--- libraries/streaming-e2e-tests/react-app/custom-app-code/index.js
+++ libraries/streaming-e2e-tests/react-app/custom-app-code/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { DirectLineStreaming } from 'botframework-directlinejs';
 
 (async function() {
-  const botHostname = process.env.BotName;
+  const botHostname = process.env.APPSETTING_WEBSITE_SITE_NAME;
   const res = await fetch(`https://${botHostname}.azurewebsites.net/api/token/directlinease`, {
     method: 'POST'
   });
