7cedd4fe879f9a324aa9e9a5765e05f2041b3756
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-31 14:51:26 -0700

    Trying to get chrome version -- commented back in install-chrome...

--- libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
+++ libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
@@ -28,7 +28,6 @@ describe('Chrome', function () {
 });
 
 function createDriver(browser, driverBinPath) {
-    console.log('createDriver');
     // For now, we are only using ChromeDriver
     // In future expansions on E2E streaming tests, we can expand to create options for multiple browsers
     const options = new Options()
@@ -43,16 +42,14 @@ function createDriver(browser, driverBinPath) {
 }
 
 async function echoMessageInBrowser(driver) {
-  console.log('echoMessageInBrowser');
   try {
     await driver.get('https://ash-react-app.azurewebsites.net/');
     // await driver.get(process.env.REACT_APP_ENDPOINT);
     await driver.sleep(2000);
 
     let wcSendBox = await driver.wait(until.elementLocated(By.className('webchat__send-box-text-box__input')), 15000);
-    console.log('get sendbox')
     await wcSendBox.sendKeys('Why hello there', Key.RETURN);
-    console.log('send message')
+
     return driver;
 
   } catch (error) {
@@ -61,8 +58,6 @@ async function echoMessageInBrowser(driver) {
 }
 
 async function getTranscriptMessages(driver, minNumMessages) {
-  console.log('getTranscriptMessages')
-  console.log('minNumActivitiesShown...')
   await driver.wait(minNumActivitiesShown(minNumMessages), 120000);
 
   const transcript = await getTranscript(driver);
@@ -98,11 +93,9 @@ function minNumActivitiesShown(numActivities) {
 }
 
 async function getTranscript(driver) {
-  console.log('getTranscript')
   return await driver.findElement(By.css('.webchat__basic-transcript'));
 }
 
 async function getBubbles(transcript) {
-  console.log('getBubbles')
   return await transcript.findElements(By.className('webchat__bubble__content'));
 }
