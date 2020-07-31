5098c67df8dbbb07053eab986ddb988da7b97a9d
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-31 14:13:21 -0700

    temp. 3 min timeout to test duration of test

--- libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
+++ libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
@@ -12,7 +12,7 @@ const pathToChromeDriver = process.env.CHROME_DRIVER;
 describe('Chrome', function () {
   it('should receive an echo after sending a message', async function () {
     console.log('describe - it');
-    this.timeout(60000);
+    this.timeout(180000);
 
     const driver = createDriver('chrome');
     await echoMessageInBrowser(driver);
