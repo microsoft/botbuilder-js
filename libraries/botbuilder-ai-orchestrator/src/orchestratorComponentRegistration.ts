f78d3520ce78f4dd1133520aab6440626b6e7b00
Vishwac Sena Kannan <vkannan@microsoft.com>
2020-07-31 15:04:20 -0700

    updates.

--- libraries/botbuilder-ai-orchestrator/src/orchestratorComponentRegistration.ts
+++ libraries/botbuilder-ai-orchestrator/src/orchestratorComponentRegistration.ts
@@ -6,6 +6,29 @@
  * Licensed under the MIT License.
  */
 
-export class OrchestratorComponentRegistration {
+import { ComponentRegistration, ResourceExplorer, BuilderRegistration } from "botbuilder-dialogs-declarative";
+import { OrchestratorAdaptiveRecognizer } from './orchestratorAdaptiveRecognizer';
+import { AdaptiveTypeBuilder } from "botbuilder-dialogs-adaptive";
+import { BoolExpressionConverter, StringExpressionConverter, NumberExpressionConverter } from 'adaptive-expressions';
 
+export class OrchestratorComponentRegistration implements ComponentRegistration {
+    private _builderRegistrations: BuilderRegistration[] = [];
+    private _resourceExplorer: ResourceExplorer;
+
+    getTypeBuilders(): BuilderRegistration[] {
+        return this._builderRegistrations;
+    }
+
+    public constructor(resourceExplorer: ResourceExplorer) {
+        this._resourceExplorer = resourceExplorer;
+
+        this._builderRegistrations.push(
+            new BuilderRegistration('Microsoft.LuisRecognizer', new AdaptiveTypeBuilder(OrchestratorAdaptiveRecognizer, this._resourceExplorer, {
+                'modelPath': new StringExpressionConverter(),
+                'snapshotPath': new StringExpressionConverter(),
+                'disambiguationScoreThreshold': new NumberExpressionConverter(),
+                'detectAmbiguousIntents': new BoolExpressionConverter(),
+            }))
+        );    
+    }
 };
