ef9cb630c4cf08d0e4307a5e77e1dd2adece4a59
Ashley Finafrock <a.finafrock@gmail.com>
2020-07-30 22:51:15 -0700

    Deleted dummy test

--- libraries/streaming-e2e-tests/react-app/dummy/test-env.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const dotenv = require('dotenv');
-const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
-
-const botName = process.env.BotName;
-console.log(botName);
