3fd19150ee90f7fa72bdc226d59f2e0d217e83c8
Michael Richardson <v-micric@microsoft.com>
2020-08-05 13:35:06 -0700

    routeConstants to class from namespace

--- libraries/botbuilder/src/routeConstants.ts
+++ libraries/botbuilder/src/routeConstants.ts
@@ -5,57 +5,57 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-/* eslint-disable @typescript-eslint/no-namespace */
 
 /**
  * Constants representing the API path that immediately follows the basePath.
+ * These are currently internal but we will re-evaluate making them public, in the future.
  * 
  * @example
  * RouteConstants.Activities = '/v3/conversations/:conversationId/activities'.
  */
-export namespace RouteConstants {
+export class RouteConstants {
     /**
      * Base API path for bot conversations.
      */
-    export const Conversations = '/v3/conversations';
+    public static readonly Conversations: string = '/v3/conversations';
 
     /**
      * API path for conversation history.
      */
-    export const ConversationHistory = '/v3/conversations/:conversationId/activities/history';
+    public static readonly ConversationHistory: string = '/v3/conversations/:conversationId/activities/history';
 
     /**
      * API path for all conversation members.
      */
-    export const ConversationMembers = '/v3/conversations/:conversationId/members';
+    public static readonly ConversationMembers: string = '/v3/conversations/:conversationId/members';
 
     /**
      * API path for page(s) of all conversation members.
      */
-    export const ConversationPagedMembers = '/v3/conversations/:conversationId/pagedmembers';
+    public static readonly ConversationPagedMembers: string = '/v3/conversations/:conversationId/pagedmembers';
 
     /**
      * API path for single conversation member.
      */
-    export const ConversationMember = '/v3/conversations/:conversationId/members/:memberId';
+    public static readonly ConversationMember: string = '/v3/conversations/:conversationId/members/:memberId';
 
     /**
      * API path for conversation attachments.
      */
-    export const Attachments = '/v3/conversations/:conversationId/attachments';
+    public static readonly Attachments: string = '/v3/conversations/:conversationId/attachments';
 
     /**
      * API path for all activities from conversation.
      */
-    export const Activities = '/v3/conversations/:conversationId/activities';
+    public static readonly Activities: string = '/v3/conversations/:conversationId/activities';
 
     /**
      * API path for single activity from conversation.
      */
-    export const Activity = '/v3/conversations/:conversationId/activities/:activityId';
+    public static readonly Activity: string = '/v3/conversations/:conversationId/activities/:activityId';
 
     /**
      * API path for all members from activity from conversation.
      */
-    export const ActivityMembers = '/v3/conversations/:conversationId/activities/:activityId/members';
+    public static readonly ActivityMembers: string = '/v3/conversations/:conversationId/activities/:activityId/members';
 }
