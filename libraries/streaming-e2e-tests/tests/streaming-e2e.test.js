34ed5af0e2ed5dca1af30cfd1897afc2af6e716c
Ashley Finafrock <a.finafrock@gmail.com>
2020-08-06 11:32:10 -0700

    Added more logs into tests for more visibility in Az Pipelines

--- libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
+++ libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
@@ -45,7 +45,9 @@ async function echoMessageInBrowser(driver) {
 
     await driver.sleep(7000);
 
+    console.log('Getting Web Chat sendbox...');
     let wcSendBox = await driver.wait(until.elementLocated(By.className('webchat__send-box-text-box__input')), 17000);
+    console.log('Sending user message...');
     await wcSendBox.sendKeys(userMessage, Key.RETURN);
 
     return driver;
