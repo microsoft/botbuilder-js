850e35ba5e042880a85ee45baeba5215a61681be
Steven Gum <14935595+stevengum@users.noreply.github.com>
2020-08-07 14:50:16 -0700

    revert Orchestrator require()

--- libraries/botbuilder-ai-orchestrator/src/orchestratorAdaptiveRecognizer.ts
+++ libraries/botbuilder-ai-orchestrator/src/orchestratorAdaptiveRecognizer.ts
@@ -15,7 +15,7 @@ import { Activity, Entity, RecognizerResult } from 'botbuilder-core';
 import { DialogContext } from 'botbuilder-dialogs';
 import { createRecognizerResult, EntityRecognizer, EntityRecognizerSet, Recognizer, TextEntity } from 'botbuilder-dialogs-adaptive';
 
-const oc: any = require('@microsoft/orchestrator-core');
+const oc: any = require('@microsoft/orchestrator-core/orchestrator-core.node');
 const ReadText: any = require('read-text-file');
 
 export class OrchestratorAdaptiveRecognizer extends Recognizer {
