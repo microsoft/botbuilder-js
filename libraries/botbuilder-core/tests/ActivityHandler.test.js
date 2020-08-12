fa3e1c970ffa18b38506c760c9fb42695a121cb0
Eric Dahlvang <EricDahlvang@gmail.com>
2020-08-11 18:34:48 -0700

    installation update add and remove (#2687) (#2688)
    Co-authored-by: johnataylor <johtaylo@microsoft.com>

--- libraries/botbuilder-core/tests/ActivityHandler.test.js
+++ libraries/botbuilder-core/tests/ActivityHandler.test.js
@@ -236,6 +236,32 @@ describe('ActivityHandler', function() {
         processActivity({type: ActivityTypes.InstallationUpdate}, bot, done);
     });
 
+    it(`should fire onInstallationUpdateAdd`, async function(done) {
+
+        const bot = new ActivityHandler();
+
+        bot.onInstallationUpdateAdd(async (context, next) => {
+            assert(true, 'onInstallationUpdateAdd not called');
+            done();
+            await next();
+        });
+
+        processActivity({type: ActivityTypes.InstallationUpdate, action: 'add'}, bot, done);
+    });
+
+    it(`should fire onInstallationUpdateRemove`, async function(done) {
+
+        const bot = new ActivityHandler();
+
+        bot.onInstallationUpdateRemove(async (context, next) => {
+            assert(true, 'onInstallationUpdateRemove not called');
+            done();
+            await next();
+        });
+
+        processActivity({type: ActivityTypes.InstallationUpdate, action: 'remove'}, bot, done);
+    });
+
     it(`should fire onUnrecognizedActivityType`, async function(done) {
 
         const bot = new ActivityHandler();
