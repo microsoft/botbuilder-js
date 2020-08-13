0d79fd97998e0d2986cbbf6cf2e4439614b8ed52
Michael Richardson <v-micric@microsoft.com>
2020-08-13 11:30:45 -0700

    reset event listeners between tests

--- libraries/botbuilder-dialogs-adaptive-testing/src/testRunner.ts
+++ libraries/botbuilder-dialogs-adaptive-testing/src/testRunner.ts
@@ -10,6 +10,7 @@ import { ResourceExplorer } from 'botbuilder-dialogs-declarative';
 import { AdaptiveDialogTestComponentRegistration } from './adaptiveDialogTestComponentRegistration';
 import { AdaptiveTestAdapter } from './adaptiveTestAdapter';
 import { TestScript } from './testScript';
+import { EventEmitter } from 'events';
 
 export class TestRunner {
     private resourceExplorer: ResourceExplorer;
@@ -29,5 +30,8 @@ export class TestRunner {
         script.description = script.description || testName;
         this.testAdapter.activeQueue = [];
         await script.execute(this.resourceExplorer, testName, this.testAdapter);
+        // We should clear the event listeners between each test. Avoids test pollution and MaxListenersExceeded warning.
+        // @ts-ignore: Property is private, but we need to reset it, only for testing.
+        this.resourceExplorer._eventEmitter = new EventEmitter();
     }
 }
