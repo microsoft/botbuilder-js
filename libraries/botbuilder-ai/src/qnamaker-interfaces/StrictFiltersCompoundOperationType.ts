cc99d2bd06ea7f40a6a4036427452f07f3ce34ea
vipeketi <vipeketi@microsoft.com>
2020-08-17 13:47:47 +0530

    StrictFilterCompoundOperationType

---
+++ libraries/botbuilder-ai/src/qnamaker-interfaces/StrictFiltersCompoundOperationType.ts
@@ -0,0 +1,24 @@
+/**
+ * @module botbuilder-ai
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Enumeration of types of StrickFilters Operation Types.
+ */
+export class StrictFiltersCompoundOperationType {
+
+    /**
+     * Join Operation Type is 'AND'.
+     */
+    public static readonly AND: string = 'AND';
+
+    /**
+     * Join Operation Type is 'OR'.
+     */
+    public static readonly OR: string = 'OR';
+
+}
