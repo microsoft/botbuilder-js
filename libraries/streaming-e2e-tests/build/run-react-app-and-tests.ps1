07ad7a55133cfda8c500fbf20ea70547ff6fff44
Ashley Finafrock <a.finafrock@gmail.com>
2020-08-21 12:00:03 -0700

    running run-react-app-and-tests.ps1

--- libraries/streaming-e2e-tests/build/run-react-app-and-tests.ps1
+++ libraries/streaming-e2e-tests/build/run-react-app-and-tests.ps1
@@ -1,13 +1,14 @@
-param()
-Write-Host "npm start in new powershell console..."
-Set-Location -Path "C:\Users\AshleyFinafrock\Work\pipeline-test-app\pipeline2-with-yarnlock"
-start powershell {npm start}
+param($ReactProjectDir, $TestsDir)
 
-Write-Host "sleeping..."
+Write-Host "npm start in new powershell console...";
+Set-Location -Path "$ReactProjectDir";
+$reactJob = start powershell {npm start};
+
+Write-Host "sleeping before running tests..."
 Start-Sleep -Seconds 60
 
 $reactJob | Select-Object -Property *
 
 Write-Host "npm test"
-Set-Location -Path "C:\Users\AshleyFinafrock\Work\pipeline-test-app\tests"
+Set-Location -Path "$TestsDir"
 npm test
