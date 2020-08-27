ae6138e164b5aa38960c03a5646b69833488b6be
LocalizationBuildProcess <v-bruhal@microsoft.com>
2020-08-27 14:39:55 -0700

    Add back the header info.

--- build/AnalyzeDependencies/AnalyzeDepsJS.ps1
+++ build/AnalyzeDependencies/AnalyzeDepsJS.ps1
@@ -1,3 +1,20 @@
+#
+# Adapted from https://github.com/microsoft/botbuilder-dotnet/blob/main/build/AnalyzeDeps/AnalyzeDeps.ps1
+#
+
+<#
+  .SYNOPSIS
+  Generate a dependency report for a set of .tgz files
+  .PARAMETER PackagesPath
+  The path to the package(s) to analyze. Globs are supported.
+  .PARAMETER LockfilePath
+  NOT USED. The path to the lockfile to analyze.
+  .PARAMETER OutPath
+  The path to write the HTML-formatted report.
+  .PARAMETER DumpPath
+  The path to write the JSONP-formatted dependency data file.
+#>
+
 Param(
   [Parameter(Mandatory = $true)][string]$PackagesPath,
   [Parameter(Mandatory = $false)][string]$LockfilePath,
