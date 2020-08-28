d18acef22a29dc00909389690e8ca2f4830c0eb9
Zichuan Ma <zim@microsoft.com>
2020-08-28 15:18:56 +0800

    updated OrchestratorComponentRegistration

--- libraries/botbuilder-ai-orchestrator/src/orchestratorComponentRegistration.ts
+++ libraries/botbuilder-ai-orchestrator/src/orchestratorComponentRegistration.ts
@@ -7,29 +7,20 @@
  */
 
 import { BoolExpressionConverter, NumberExpressionConverter, StringExpressionConverter } from 'adaptive-expressions';
-import { AdaptiveTypeBuilder } from 'botbuilder-dialogs-adaptive';
-import { BuilderRegistration, ComponentRegistration, ResourceExplorer } from 'botbuilder-dialogs-declarative';
+import { BuilderRegistration, ComponentRegistration, DefaultTypeBuilder, ResourceExplorer } from 'botbuilder-dialogs-declarative';
 
 import { OrchestratorAdaptiveRecognizer } from './orchestratorAdaptiveRecognizer';
 
 export class OrchestratorComponentRegistration implements ComponentRegistration {
-    private readonly _builderRegistrations: BuilderRegistration[] = [];
-    private _resourceExplorer: ResourceExplorer;
-
-    public getTypeBuilders(): BuilderRegistration[] {
-        return this._builderRegistrations;
-    }
-
-    public constructor(resourceExplorer: ResourceExplorer) {
-        this._resourceExplorer = resourceExplorer;
-
-        this._builderRegistrations.push(
-            new BuilderRegistration('Microsoft.OrchestratorRecognizer', new AdaptiveTypeBuilder(OrchestratorAdaptiveRecognizer, this._resourceExplorer, {
+    public getBuilderRegistrations(resourceExplorer: ResourceExplorer): BuilderRegistration[] {
+        return [{
+            kind: 'Microsoft.OrchestratorRecognizer',
+            builder: new DefaultTypeBuilder(OrchestratorAdaptiveRecognizer, resourceExplorer, {
                 modelPath: new StringExpressionConverter(),
                 snapshotPath: new StringExpressionConverter(),
                 disambiguationScoreThreshold: new NumberExpressionConverter(),
                 detectAmbiguousIntents: new BoolExpressionConverter(),
-            }))
-        );    
+            })
+        }];
     }
 };
