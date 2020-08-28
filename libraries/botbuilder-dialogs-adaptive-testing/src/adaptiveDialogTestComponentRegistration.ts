cb28c758190bb000ff6f164046f4682bc1ca4f0c
Zichuan Ma <zim@microsoft.com>
2020-08-28 15:16:14 +0800

    updated AdaptiveDialogTestComponentRegistration

--- libraries/botbuilder-dialogs-adaptive-testing/src/adaptiveDialogTestComponentRegistration.ts
+++ libraries/botbuilder-dialogs-adaptive-testing/src/adaptiveDialogTestComponentRegistration.ts
@@ -6,43 +6,50 @@
  * Licensed under the MIT License.
  */
 
-import { ComponentRegistration, BuilderRegistration, ResourceExplorer, TypeBuilder } from 'botbuilder-dialogs-declarative';
-import { AdaptiveTypeBuilder, DialogExpressionConverter } from 'botbuilder-dialogs-adaptive';
 import { ExpressionConverter, StringExpressionConverter } from 'adaptive-expressions';
+import { BuilderRegistration, ComponentRegistration, DefaultTypeBuilder, ResourceExplorer } from 'botbuilder-dialogs-declarative';
+import { DialogExpressionConverter } from 'botbuilder-dialogs-adaptive';
 import { AssertReply, AssertReplyActivity, AssertReplyOneOf, UserActivity, UserConversationUpdate, UserDelay, UserSays, UserTyping } from './testActions';
 import { AssertCondition } from './actions';
 import { TestScript } from './testScript';
 
 export class AdaptiveDialogTestComponentRegistration implements ComponentRegistration {
-    private _resourceExplorer: ResourceExplorer;
-    private _builderRegistrations: BuilderRegistration[] = [];
-
-    public constructor(resourceExplorer: ResourceExplorer) {
-        this._resourceExplorer = resourceExplorer;
-        this.registerBuilder('Microsoft.Test.AssertCondition', new AdaptiveTypeBuilder(AssertCondition, this._resourceExplorer, {
-            'condition': new ExpressionConverter(),
-            'description': new StringExpressionConverter()
-        }));
-        this.registerBuilder('Microsoft.Test.AssertReply', new AdaptiveTypeBuilder(AssertReply, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.AssertReplyActivity', new AdaptiveTypeBuilder(AssertReplyActivity, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.AssertReplyOneOf', new AdaptiveTypeBuilder(AssertReplyOneOf, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.UserActivity', new AdaptiveTypeBuilder(UserActivity, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.UserConversationUpdate', new AdaptiveTypeBuilder(UserConversationUpdate, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.UserDelay', new AdaptiveTypeBuilder(UserDelay, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.UserSays', new AdaptiveTypeBuilder(UserSays, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.UserTyping', new AdaptiveTypeBuilder(UserTyping, this._resourceExplorer, {}));
-        this.registerBuilder('Microsoft.Test.Script', new AdaptiveTypeBuilder(TestScript, this._resourceExplorer, {
-            'dialog': new DialogExpressionConverter(resourceExplorer)
-        }));
-    }
-
-    public getTypeBuilders(): BuilderRegistration[] {
-        return this._builderRegistrations;
-    }
-
-    private registerBuilder(name: string, builder: TypeBuilder): void {
-        this._builderRegistrations.push(
-            new BuilderRegistration(name, builder)
-        );
+    public getBuilderRegistrations(resourceExplorer: ResourceExplorer): BuilderRegistration[] {
+        return [{
+            kind: 'Microsoft.Test.AssertCondition',
+            builder: new DefaultTypeBuilder(AssertCondition, resourceExplorer, {
+                'condition': new ExpressionConverter(),
+                'description': new StringExpressionConverter()
+            })
+        }, {
+            kind: 'Microsoft.Test.AssertReply',
+            builder: new DefaultTypeBuilder(AssertReply, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.AssertReplyActivity',
+            builder: new DefaultTypeBuilder(AssertReplyActivity, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.AssertReplyOneOf',
+            builder: new DefaultTypeBuilder(AssertReplyOneOf, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.UserActivity',
+            builder: new DefaultTypeBuilder(UserActivity, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.UserConversationUpdate',
+            builder: new DefaultTypeBuilder(UserConversationUpdate, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.UserDelay',
+            builder: new DefaultTypeBuilder(UserDelay, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.UserSays',
+            builder: new DefaultTypeBuilder(UserSays, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.UserTyping',
+            builder: new DefaultTypeBuilder(UserTyping, resourceExplorer, {})
+        }, {
+            kind: 'Microsoft.Test.Script',
+            builder: new DefaultTypeBuilder(TestScript, resourceExplorer, {
+                'dialog': new DialogExpressionConverter(resourceExplorer)
+            })
+        }];
     }
 }
