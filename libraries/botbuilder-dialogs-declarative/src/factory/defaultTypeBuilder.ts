3e737774012aca6a3a85e0498f3f7fb356427958
Zichuan Ma <zim@microsoft.com>
2020-08-28 15:14:04 +0800

    AdaptiveTypeBuilder -> DefaultTypeBuilder

--- libraries/botbuilder-dialogs-declarative/src/factory/defaultTypeBuilder.ts
+++ libraries/botbuilder-dialogs-declarative/src/factory/defaultTypeBuilder.ts
@@ -6,13 +6,42 @@
  * Licensed under the MIT License.
  */
 
+import { Converter } from '../converter';
+import { ResourceExplorer } from '../resources/resourceExplorer';
 import { TypeBuilder } from './typeBuilder';
 
 export class DefaultTypeBuilder implements TypeBuilder {
+    private _factory: new () => object;
+    private _converters: { [key: string]: Converter } = {};
+    private _resourceExplorer: ResourceExplorer;
 
-    constructor(private factory: new () => any) {}
+    public constructor(factory: new () => object, resourceExplorer: ResourceExplorer, converters: { [key: string]: Converter }) {
+        this._factory = factory;
+        this._resourceExplorer = resourceExplorer;
+        this._converters = converters;
+    }
 
-    public build(config: object) : object {
-        return new this.factory();
+    public build(config: object): object {
+        const obj = new this._factory();
+        for (const key in config) {
+            if (config.hasOwnProperty(key)) {
+                const value = config[key];
+                const converter = this._converters[key];
+                if (converter) {
+                    if (Array.isArray(value) && Array.isArray(obj[key])) {
+                        obj[key] = value.map((item): any => converter.convert(this._resourceExplorer.buildType(item)));
+                    } else {
+                        obj[key] = converter.convert(this._resourceExplorer.buildType(value));
+                    }
+                } else {
+                    if (Array.isArray(value)) {
+                        obj[key] = value.map((item): any => this._resourceExplorer.buildType(item));
+                    } else {
+                        obj[key] = this._resourceExplorer.buildType(value);
+                    }
+                }
+            }
+        }
+        return obj;
     }
 }
