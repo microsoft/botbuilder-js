a54f598316d3f59b9f939a7f36e484184c3a3821
Zichuan Ma <zim@microsoft.com>
2020-09-01 10:49:24 +0800

    add header of botbulder-ai

--- libraries/botbuilder-ai/src/qnaMakerComponentRegistration.ts
+++ libraries/botbuilder-ai/src/qnaMakerComponentRegistration.ts
@@ -1,3 +1,10 @@
+/**
+ * @module botbuilder-ai
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
 import { StringExpressionConverter, IntExpressionConverter, NumberExpressionConverter, BoolExpressionConverter, ArrayExpressionConverter, ObjectExpressionConverter, EnumExpressionConverter } from 'adaptive-expressions';
 import { QnAMakerRecognizer } from './qnaMakerRecognizer';
