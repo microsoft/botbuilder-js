e8768637645ea163ae28fe80ec481b9f2c07b0d5
Zichuan Ma <zim@microsoft.com>
2020-09-09 19:22:33 +0800

    onCancelDialog test

--- libraries/botbuilder-dialogs-adaptive-testing/tests/conditionals.test.js
+++ libraries/botbuilder-dialogs-adaptive-testing/tests/conditionals.test.js
@@ -5,19 +5,23 @@ describe('ConditionalsTests', function() {
     this.timeout(5000);
     const testRunner = new TestRunner(path.join(__dirname, 'resources/ConditionalsTests'));
 
+    it('OnActivityTypes', async () => {
+        await testRunner.runTestScript('ConditionalsTests_OnActivityTypes');
+    });
+
+    it('OnCancelDialog', async () => {
+        await testRunner.runTestScript('ConditionalsTests_OnCancelDialog');
+    });
+
+    it('OnChooseIntent', async () => {
+        await testRunner.runTestScript('ConditionalsTests_OnChooseIntent');
+    });
+
     it('OnIntent', async () => {
         await testRunner.runTestScript('ConditionalsTests_OnIntent');
     });
 
     it('OnIntent with entities', async () => {
         await testRunner.runTestScript('ConditionalsTests_OnIntentWithEntities');
-    });
-
-    it('OnActivityTypes', async () => {
-        await testRunner.runTestScript('ConditionalsTests_OnActivityTypes');
-    });
-
-    it('OnChooseIntent', async () => {
-        await testRunner.runTestScript('ConditionalsTests_OnChooseIntent');
     });
 });
