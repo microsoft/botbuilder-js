2b8252478bf803de09199ab5ab66fd35a712ef6e
Ashley Finafrock <a.finafrock@gmail.com>
2020-09-14 17:59:08 -0700

    Added comment to react-app/App.js regarding nonce

--- libraries/streaming-e2e-tests/react-app/src/App.js
+++ libraries/streaming-e2e-tests/react-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactWebChat from 'botframework-webchat';
 
 function App({directLine}) {
-  // nonce addresses botbuilder-js content security issue #2762:
+  // nonce related to botbuilder-js content security issue #2762:
   // https://github.com/microsoft/botbuilder-js/issues/2762
   return (
     <ReactWebChat directLine={directLine} nonce="a1b2c3d" />
