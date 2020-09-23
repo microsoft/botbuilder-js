9e4c9d018851e474bbf3664d984bde8738ee493e
Zichuan Ma <zim@microsoft.com>
2020-09-23 11:14:33 +0800

    case expressions as a map

--- libraries/botbuilder-dialogs-adaptive/src/actions/switchCondition.ts
+++ libraries/botbuilder-dialogs-adaptive/src/actions/switchCondition.ts
@@ -40,7 +40,7 @@ export class SwitchCondition<O extends object = {}> extends Dialog<O> implements
      */
     public disabled?: BoolExpression;
 
-    private _caseExpresssions: { [key: string]: Expression };
+    private _caseExpresssions: Map<string, Expression>;
 
     private _defaultScope: ActionScope;
 
@@ -62,47 +62,47 @@ export class SwitchCondition<O extends object = {}> extends Dialog<O> implements
         }
 
         if (!this._caseExpresssions) {
-            this._caseExpresssions = {};
+            this._caseExpresssions = new Map<string, Expression>();
             for (let i = 0; i < this.cases.length; i++) {
                 const caseScope = this.cases[i];
                 const intVal = parseInt(caseScope.value);
                 if (!isNaN(intVal)) {
                     // you don't have to put quotes around numbers, "23" => 23 OR "23".
-                    this._caseExpresssions[caseScope.value] = Expression.orExpression(
+                    this._caseExpresssions.set(caseScope.value, Expression.orExpression(
                         Expression.equalsExpression(this.condition, new Constant(intVal)),
                         Expression.equalsExpression(this.condition, new Constant(caseScope.value))
-                    );
+                    ));
                     continue;
                 }
                 const floatVal = parseFloat(caseScope.value);
                 if (!isNaN(floatVal)) {
                     // you don't have to put quotes around numbers, "23" => 23 OR "23".
-                    this._caseExpresssions[caseScope.value] = Expression.orExpression(
+                    this._caseExpresssions.set(caseScope.value, Expression.orExpression(
                         Expression.equalsExpression(this.condition, new Constant(floatVal)),
                         Expression.equalsExpression(this.condition, new Constant(caseScope.value))
-                    );
+                    ));
                     continue;
                 }
                 if (caseScope.value.trim().toLowerCase() === 'true') {
                     // you don't have to put quotes around bools, "true" => true OR "true".
-                    this._caseExpresssions[caseScope.value] = Expression.orExpression(
+                    this._caseExpresssions.set(caseScope.value, Expression.orExpression(
                         Expression.equalsExpression(this.condition, new Constant(true)),
                         Expression.equalsExpression(this.condition, new Constant(caseScope.value))
-                    );
+                    ));
                     continue;
                 }
                 if (caseScope.value.trim().toLowerCase() === 'false') {
                     // you don't have to put quotes around bools, "false" => false OR "false".
-                    this._caseExpresssions[caseScope.value] = Expression.orExpression(
+                    this._caseExpresssions.set(caseScope.value, Expression.orExpression(
                         Expression.equalsExpression(this.condition, new Constant(false)),
                         Expression.equalsExpression(this.condition, new Constant(caseScope.value))
-                    );
+                    ));
                     continue;
                 }
                 // if someone does "=23" that will be numeric comparison or "='23'" that will be string comparison,
                 // or it can be a real expression bound to memory.
                 const { value } = new ValueExpression(caseScope.value).tryGetValue(dc.state);
-                this._caseExpresssions[caseScope.value] = Expression.equalsExpression(this.condition, new Constant(value));
+                this._caseExpresssions.set(caseScope.value, Expression.equalsExpression(this.condition, new Constant(value)));
             }
         }
 
@@ -110,7 +110,7 @@ export class SwitchCondition<O extends object = {}> extends Dialog<O> implements
 
         for (let i = 0; i < this.cases.length; i++) {
             const caseScope = this.cases[i];
-            const caseCondition = this._caseExpresssions[caseScope.value] as Expression;
+            const caseCondition = this._caseExpresssions.get(caseScope.value) as Expression;
             const { value, error } = caseCondition.tryEvaluate(dc.state);
             if (error) {
                 throw new Error(`Expression evaluation resulted in an error. Expression: ${ caseCondition.toString() }. Error: ${ error }`);
