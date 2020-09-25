19fc23842afb02f88da46f71f07dddbee4dc02fb
Josh Gummersall <jgummersall@microsoft.com>
2020-09-25 13:54:12 -0700

    Do not export TestSkillAdapter

--- libraries/botbuilder-core/src/index.ts
+++ libraries/botbuilder-core/src/index.ts
@@ -43,7 +43,6 @@ export * from './storage';
 export * from './stringUtils';
 export * from './telemetryLoggerMiddleware';
 export * from './testAdapter';
-export * from './testSkillAdapter';
 export * from './transcriptLogger';
 export * from './turnContext';
 export * from './turnContextStateCollection';
