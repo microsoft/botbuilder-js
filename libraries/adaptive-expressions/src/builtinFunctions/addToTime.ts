a0b808cbd0e4373b8997dc23a11e2f277f82ae22
Ian Scaltritti <ian.scaltritti@southworks.com>
2020-09-30 11:35:08 -0300

    FIx spacing warnings

--- libraries/adaptive-expressions/src/builtinFunctions/addToTime.ts
+++ libraries/adaptive-expressions/src/builtinFunctions/addToTime.ts
@@ -42,7 +42,7 @@ export class AddToTime extends ExpressionEvaluator {
             if (typeof (args[0]) === 'string' && Number.isInteger(args[1]) && typeof (args[2]) === 'string') {
                 ({ value, error } = AddToTime.evalAddToTime(args[0], args[1], args[2], format));
             } else {
-                error = `${expression} should contain an ISO format timestamp, a time interval integer, a string unit of time and an optional output format string.`;
+                error = `${ expression } should contain an ISO format timestamp, a time interval integer, a string unit of time and an optional output format string.`;
             }
         }
 
@@ -98,7 +98,7 @@ export class AddToTime extends ExpressionEvaluator {
                 }
 
                 default: {
-                    error = `${timeUnit} is not valid time unit`;
+                    error = `${ timeUnit } is not valid time unit`;
                     break;
                 }
             }
