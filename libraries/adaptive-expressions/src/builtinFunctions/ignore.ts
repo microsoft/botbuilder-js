41e25911c515e5eb2f2cbc364827039fe45f5257
Ian Scaltritti <ian.scaltritti@southworks.com>
2020-09-30 16:46:06 -0300

    Description fix
    Co-authored-by: Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>

--- libraries/adaptive-expressions/src/builtinFunctions/ignore.ts
+++ libraries/adaptive-expressions/src/builtinFunctions/ignore.ts
@@ -21,7 +21,7 @@ import { ReturnType } from '../returnType';
  */
 export class Ignore extends ExpressionEvaluator {
     /**
-     * Initializes a new instance of the `GreaterThan` class.
+     * Initializes a new instance of the `Ignore` class.
      */
     public constructor() {
         super(ExpressionType.Ignore, Ignore.evaluator, ReturnType.Boolean, FunctionUtils.validateUnaryBoolean);
