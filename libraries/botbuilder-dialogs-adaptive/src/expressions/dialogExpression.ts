c7505498f0fc6884836f05786daa2efd27cbec3c
Joel Mut <joel.mut@southworks.com>
2020-09-30 16:17:31 -0300

    Add missing documentation for botbuilder-dialogs-adaptive expressions folder

--- libraries/botbuilder-dialogs-adaptive/src/expressions/dialogExpression.ts
+++ libraries/botbuilder-dialogs-adaptive/src/expressions/dialogExpression.ts
@@ -11,18 +11,26 @@ import { Dialog } from 'botbuilder-dialogs';
 /**
  * Represents a property which is either a Dialog or a string expression for a dialogId.
  * @remarks
- * String values are always interpreted as a string with interpolation, unless it has '=' prefix 
+ * String values are always interpreted as a string with interpolation, unless it has '=' prefix
  * or not. The result is interpreted as a resource Id or dialogId.
  */
 export class DialogExpression extends ExpressionProperty<Dialog> {
+    /**
+     * Initializes a new instance of the `DialogExpression` class.
+     * @param value A `Dialog`, a `string` that is interpreted as a resource Id or dialogId, or an `Expression`.
+     */
     public constructor(value?: Dialog | string | Expression) {
         super(value);
     }
 
+    /**
+     * Sets the raw value of the expression property.
+     * @param value A `Dialog`, a `string` that is interpreted as a resource Id or dialogId, or an `Expression`.
+     */
     public setValue(value: Dialog | string | Expression): void {
         if (typeof value == 'string' && !value.startsWith('=')) {
             // Resource Id's will be resolved to actual dialog value
-            // if it's not a = then we want to convert to a constant string expressions to represent a 
+            // if it's not a = then we want to convert to a constant string expressions to represent a
             // external dialog id resolved by dc.FindDialog()
             value = `='${ value }'`;
         }
