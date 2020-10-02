c133a689b60b40ec950cbce7f953a0f60f6ce08a
Geoff Cox (Microsoft) <gcox@microsoft.com>
2020-10-02 15:51:40 -0700

    eslint --fix of botbuilder-core (#2865)
    * eslint --fix bb-applicationinsights and bb-azure
    
    * eslint --fix on botbuilder-core

--- libraries/botbuilder-core/src/userTokenSettings.ts
+++ libraries/botbuilder-core/src/userTokenSettings.ts
@@ -14,7 +14,7 @@ export interface TokenPollingSettings {
      * Polling timeout time in milliseconds. This is equivalent to login flow timeout.
      */
     timeout?: number;
-    
+
     /**
      * Time Interval in milliseconds between token polling requests.
      */
@@ -24,17 +24,17 @@ export interface TokenPollingSettings {
 /**
  * TurnState key for the OAuth login timeout
  */
-export const OAuthLoginTimeoutKey: string = 'loginTimeout';
+export const OAuthLoginTimeoutKey = 'loginTimeout';
 
 /**
  * Name of the token polling settings key.
  */
-export const TokenPollingSettingsKey: string = 'tokenPollingSettings';
- 
+export const TokenPollingSettingsKey = 'tokenPollingSettings';
+
 /**
  *  Default amount of time an OAuthCard will remain active (clickable and actively waiting for a token).
  *  After this time:
  *  (1) the OAuthCard will not allow the user to click on it.
  *  (2) any polling triggered by the OAuthCard will stop.
  */
- export const OAuthLoginTimeoutMsValue: number = 900000;
+export const OAuthLoginTimeoutMsValue = 900000;
