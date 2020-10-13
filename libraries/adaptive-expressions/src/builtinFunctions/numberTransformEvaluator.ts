1ae8593ea0a645376280c8d0f8a494137865fd7b
Ian Luca Scaltritti <ian.scaltritti@southworks.com>
2020-10-13 12:23:32 -0300

    Adds missing param documentation

--- libraries/adaptive-expressions/src/builtinFunctions/numberTransformEvaluator.ts
+++ libraries/adaptive-expressions/src/builtinFunctions/numberTransformEvaluator.ts
@@ -16,6 +16,8 @@ import { ReturnType } from '../returnType';
 export class NumberTransformEvaluator extends ExpressionEvaluator {
     /**
      * Initializes a new instance of the [NumberTransformEvaluator](xref:adaptive-expressions.NumberTransformEvaluator) class.
+     * @param type Name of the built-in function.
+     * @param func The comparison function, it takes a list of objects and returns a number.
      */
     public constructor(type: string, func: (args: any[]) => number) {
         super(type, NumberTransformEvaluator.evaluator(func), ReturnType.Number, FunctionUtils.validateUnaryNumber);
