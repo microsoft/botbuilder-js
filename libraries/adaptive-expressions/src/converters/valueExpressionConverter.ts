2879bccd134477117ea5f122bd3c784112484b19
Denise Scollo <dens.scollo@gmail.com>
2020-10-19 17:48:12 -0300

    [#2602] [JSDoc] Add missing documentation for adaptive-expressions/converters (#2854)
    * Adds missing documentation
    
    * Consistency fix
    
    * Adds xref links
    
    * fix class name
    
    Co-authored-by: Ian Scaltritti <ian.scaltritti@southworks.com>
    Co-authored-by: Santiago Grangetto <santiago.grangetto@southworks.com>

--- libraries/adaptive-expressions/src/converters/valueExpressionConverter.ts
+++ libraries/adaptive-expressions/src/converters/valueExpressionConverter.ts
@@ -8,7 +8,15 @@
 
 import { ValueExpression } from '../expressionProperties';
 
+/**
+ * `any` value to json [ValueExpression](xref:adaptive-expressions.ValueExpression) converter.
+ */
 export class ValueExpressionConverter {
+    /**
+     * Converts `any` value into a [ValueExpression](xref:adaptive-expressions.ValueExpression).
+     * @param value `any` value to convert.
+     * @returns The [ValueExpression](xref:adaptive-expressions.ValueExpression).
+     */
     public convert(value: unknown | ValueExpression): ValueExpression {
         return value instanceof ValueExpression ? value : new ValueExpression(value);
     }
