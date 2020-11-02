8a7a37a2840784353f04dd5407c4763f0b3f33a5
Zichuan Ma <zim@microsoft.com>
2020-11-02 16:56:37 +0800

    cache lgm initialization in lg extension

--- libraries/botbuilder-dialogs-adaptive/src/languageGeneratorExtensions.ts
+++ libraries/botbuilder-dialogs-adaptive/src/languageGeneratorExtensions.ts
@@ -36,6 +36,11 @@ export const languagePolicyKey = Symbol('LanguagePolicy');
  * Extension methods for language generator.
  */
 export class LanguageGeneratorExtensions {
+    private static readonly _languageGeneratorManagers: Map<ResourceExplorer, LanguageGeneratorManager> = new Map<
+        ResourceExplorer,
+        LanguageGeneratorManager
+    >();
+
     /**
      * Register default LG file or a language generator as default language generator.
      * @param dialogManager The dialog manager to add language generator to.
@@ -56,7 +61,11 @@ export class LanguageGeneratorExtensions {
             }
         }
 
-        const languageGeneratorManager: LanguageGeneratorManager = new LanguageGeneratorManager(resourceExplorer);
+        let languageGeneratorManager: LanguageGeneratorManager = this._languageGeneratorManagers.get(resourceExplorer);
+        if (!languageGeneratorManager) {
+            languageGeneratorManager = new LanguageGeneratorManager(resourceExplorer);
+            this._languageGeneratorManagers.set(resourceExplorer, languageGeneratorManager);
+        }
 
         dialogManager.initialTurnState.set(languageGeneratorKey, languageGenerator);
         dialogManager.initialTurnState.set(languageGeneratorManagerKey, languageGeneratorManager);
