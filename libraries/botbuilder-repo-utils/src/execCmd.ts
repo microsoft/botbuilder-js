481021d5a6c3cc04a8ccdf17383e017f2823cd98
Josh Gummersall <1235378+joshgummersall@users.noreply.github.com>
2020-11-10 15:49:20 -0800

    fix: small execute script tweaks (#3046)
    * fix: propagate flags to underlying command
    
    Supports something like `yarn execute lint -- --quiet`.
    
    * fix: clean up logging, error handling
    
    * feat: support streaming

--- libraries/botbuilder-repo-utils/src/execCmd.ts
+++ libraries/botbuilder-repo-utils/src/execCmd.ts
@@ -1,12 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import * as R from 'remeda';
 import minimist from 'minimist';
 import path from 'path';
 import util from 'util';
-import { Package } from './package';
 import { DependencyResolver, collectWorkspacePackages } from './workspace';
-import { exec } from 'child_process';
+import { Package } from './package';
+import { exec, spawn } from 'child_process';
 import { failure, run, success } from './run';
 import { gitRoot } from './git';
 import { readJsonFile } from './file';
@@ -15,7 +16,8 @@ const execp = util.promisify(exec);
 
 run(async () => {
     const { _: maybeCmd, ...flags } = minimist(process.argv.slice(2), {
-        boolean: ['continue', 'noPrivate', 'silent'],
+        '--': true,
+        boolean: ['continue', 'noPrivate', 'silent', 'stream'],
         default: { concurrency: '', npm: 'yarn' },
         string: ['concurrency', 'ignoreName', 'ignorePath', 'name', 'npm', 'script', 'path'],
     });
@@ -23,10 +25,13 @@ run(async () => {
     // Resolve npm args from the command, `npm` requires the extra 'run' arg
     const npmArgs = flags.npm.trim() === 'npm' ? ['npm', 'run'] : [flags.npm.trim()];
 
-    // If we just have --script, simply invoke that with the npm program
-    const cmd = flags.script ? [...npmArgs, flags.script] : maybeCmd;
+    // To pass flags to the resolved command use '--' in the command line invocation
+    const maybeFlags: string[] = flags['--'] ?? [];
 
-    if (!cmd.length) {
+    // If we just have --script, simply invoke that with the npm program
+    const command = R.compact(flags.script ? [...npmArgs, flags.script, ...maybeFlags] : [...maybeCmd, ...maybeFlags]);
+
+    if (!command.length) {
         throw new Error('must provide a command to execute');
     }
 
@@ -62,20 +67,49 @@ run(async () => {
 
     try {
         await dependencyResolver.execute(async (workspace) => {
-            console.log(`[${workspace.pkg.name}]: ${cmd.join(' ')}`);
+            const fmt = (message: string) => `[${workspace.pkg.name}]: ${message.trim()}`;
 
-            const { stdout, stderr } = await execp(cmd.join(' '), {
-                cwd: path.dirname(workspace.absPath),
-            });
+            const log = (message: string) => console.log(fmt(message));
 
-            if (stderr) {
+            const error = (message: string) => {
                 if (flags.continue) {
-                    console.error(`[${workspace.pkg.name}]: ${stderr.trim()}`);
+                    console.error(fmt(message));
                 } else {
-                    throw new Error(`[${workspace.pkg.name}]: ${stderr.trim()}`);
+                    throw new Error(fmt(message));
                 }
-            } else if (!flags.silent) {
-                console.log(`[${workspace.pkg.name}]: ${stdout.trim()}`);
+            };
+
+            log(command.join(' '));
+
+            try {
+                const opts = {
+                    cwd: path.dirname(workspace.absPath),
+                };
+
+                if (flags.stream) {
+                    const [bin, ...args] = command;
+                    const cmd = spawn(bin, args, opts);
+
+                    cmd.stdout.on('data', (buf) => console.log(buf.toString('utf8').trim()));
+                    cmd.stderr.on('data', (buf) => console.error(buf.toString('utf8').trim()));
+
+                    await new Promise((resolve, reject) => {
+                        cmd.on('close', resolve);
+                        cmd.on('error', reject);
+                    });
+
+                    log('done!');
+                } else {
+                    const { stdout, stderr } = await execp(command.join(' '), opts);
+
+                    if (stderr.trim()) {
+                        error(stderr);
+                    } else if (!flags.silent) {
+                        log(stdout.trim() || 'done!');
+                    }
+                }
+            } catch (err) {
+                error(err.message);
             }
         });
 
