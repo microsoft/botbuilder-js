d4784a7e87e6a014a2e476796025005fa51e6695
Josh Gummersall <jgummersall@microsoft.com>
2020-11-10 13:27:19 -0800

    fix: propagate flags to underlying command
    Supports something like `yarn execute lint -- --quiet`.

--- libraries/botbuilder-repo-utils/src/execCmd.ts
+++ libraries/botbuilder-repo-utils/src/execCmd.ts
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import * as R from 'remeda';
 import minimist from 'minimist';
 import path from 'path';
 import util from 'util';
-import { Package } from './package';
 import { DependencyResolver, collectWorkspacePackages } from './workspace';
+import { Package } from './package';
 import { exec } from 'child_process';
 import { failure, run, success } from './run';
 import { gitRoot } from './git';
@@ -15,6 +16,7 @@ const execp = util.promisify(exec);
 
 run(async () => {
     const { _: maybeCmd, ...flags } = minimist(process.argv.slice(2), {
+        '--': true,
         boolean: ['continue', 'noPrivate', 'silent'],
         default: { concurrency: '', npm: 'yarn' },
         string: ['concurrency', 'ignoreName', 'ignorePath', 'name', 'npm', 'script', 'path'],
@@ -23,10 +25,15 @@ run(async () => {
     // Resolve npm args from the command, `npm` requires the extra 'run' arg
     const npmArgs = flags.npm.trim() === 'npm' ? ['npm', 'run'] : [flags.npm.trim()];
 
-    // If we just have --script, simply invoke that with the npm program
-    const cmd = flags.script ? [...npmArgs, flags.script] : maybeCmd;
+    // To pass flags to the resolved command use '--' in the command line invocation
+    const maybeFlags: string[] = flags['--'] ?? [];
 
-    if (!cmd.length) {
+    // If we just have --script, simply invoke that with the npm program
+    const command = R.compact(
+        flags.script ? [...npmArgs, flags.script, ...maybeFlags] : [...maybeCmd, ...maybeFlags]
+    ).join(' ');
+
+    if (!command) {
         throw new Error('must provide a command to execute');
     }
 
@@ -64,18 +71,21 @@ run(async () => {
         await dependencyResolver.execute(async (workspace) => {
             console.log(`[${workspace.pkg.name}]: ${cmd.join(' ')}`);
 
-            const { stdout, stderr } = await execp(cmd.join(' '), {
+            let { stdout, stderr } = await execp(cmd.join(' '), {
                 cwd: path.dirname(workspace.absPath),
             });
 
+            stdout = stdout.trim();
+            stderr = stderr.trim();
+
             if (stderr) {
                 if (flags.continue) {
-                    console.error(`[${workspace.pkg.name}]: ${stderr.trim()}`);
+                    console.error(`[${workspace.pkg.name}]: ${stderr}`);
                 } else {
-                    throw new Error(`[${workspace.pkg.name}]: ${stderr.trim()}`);
+                    throw new Error(`[${workspace.pkg.name}]: ${stderr}`);
                 }
             } else if (!flags.silent) {
-                console.log(`[${workspace.pkg.name}]: ${stdout.trim()}`);
+                console.log(`[${workspace.pkg.name}]: ${stdout || 'done!'}`);
             }
         });
 
