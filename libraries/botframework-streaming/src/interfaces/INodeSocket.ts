2c143fd3451b823e7dc85f5a851b382a9b5707d5
stgum <14935595+stevengum@users.noreply.github.com>
2020-11-30 10:47:21 -0800

    address PR feedback re: INodeSocket

--- libraries/botframework-streaming/src/interfaces/INodeSocket.ts
+++ libraries/botframework-streaming/src/interfaces/INodeSocket.ts
@@ -14,33 +14,6 @@
 export interface INodeSocket {
     writable: boolean;
     write(str: string, cb?: Function): boolean;
-    readable: boolean;
-    readonly bytesRead: number;
-    readonly bufferSize: number;
-    readonly bytesWritten: number;
-    readonly localAddress: string;
-    readonly localPort: number;
-    readonly writableHighWaterMark: number;
-    readonly writableLength: number;
-    readonly readableHighWaterMark: number;
-    readonly readableLength: number;
-    readonly readableFlowing: boolean | null;
-
-    address(): AddressInfo | string;
-
-    connect(options: any, connectionListener?: () => void): any;
-    connect(port: number, host: string, connectionListener?: () => void): any;
-    connect(port: number, connectionListener?: () => void): any;
-    connect(path: string, connectionListener?: () => void): any;
-
-    cork(): void;
-    uncork(): void;
-
-    end(cb?: () => void): void; 
-    end(chunk: any, cb?: () => void): void; 
-    end(chunk: any, encoding?: string, cb?: () => void): void;
-    
-    _destroy(error: Error | null, callback: (error: Error | null) => void): void;
     destroy(error?: Error): void;
 }
 
