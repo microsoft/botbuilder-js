cc16dcee150c136cd4715f2b20514840ecf0a4e9
Michael Richardson <v-micric@microsoft.com>
2020-12-14 12:02:01 -0800

    fixed inheritance

--- libraries/botbuilder-dialogs-adaptive-teams/src/teamsComponentRegistration.ts
+++ libraries/botbuilder-dialogs-adaptive-teams/src/teamsComponentRegistration.ts
@@ -7,64 +7,16 @@
  */
 
 import { OnTeamsAppBasedLinkQuery } from './triggerConditions/onTeamsAppBasedLinkQuery';
-import { DialogConfiguration } from 'botbuilder-dialogs';
-import { ComponentRegistration } from 'botbuilder-core';
-import {
-    ComponentDeclarativeTypes,
-    CustomDeserializer,
-    DeclarativeType,
-    ResourceExplorer,
-} from 'botbuilder-dialogs-declarative';
-import { DynamicBeginDialog, DynamicBeginDialogDeserializer } from 'botbuilder-dialogs-adaptive';
+import { ComponentDeclarativeTypes } from 'botbuilder-dialogs-declarative';
+import { AdaptiveComponentRegistration, OnActivityConfiguration } from 'botbuilder-dialogs-adaptive';
 
-type Type<T> = {
-    $kind: string;
-    new (...args: unknown[]): T;
-};
-
-export class TeamsComponentRegistration extends ComponentRegistration implements ComponentDeclarativeTypes {
-    protected _declarativeTypes: DeclarativeType<unknown, unknown>[] = [];
+export class TeamsComponentRegistration extends AdaptiveComponentRegistration implements ComponentDeclarativeTypes {
     /**
-     * Initializes a new instance of `AdaptiveComponentRegistration`.
+     * Initializes a new instance of `TeamsComponentRegistration`.
      */
     public constructor() {
         super();
 
-        this._addDeclarativeType<OnTeamsAppBasedLinkQuery, DialogConfiguration>(OnTeamsAppBasedLinkQuery);
-    }
-
-    /**
-     * Gets adaptive `DeclarativeType` resources.
-     *
-     * @param resourceExplorer `ResourceExplorer` with expected path to get all resources.
-     * @returns Adaptive `DeclarativeType` resources.
-     */
-    public getDeclarativeTypes(resourceExplorer: ResourceExplorer): DeclarativeType[] {
-        const declarativeTypes: DeclarativeType[] = [...this._declarativeTypes];
-        resourceExplorer.getResources('.schema').forEach((schema) => {
-            const resourceId = schema.id.replace(/.schema$/, '');
-            if (resourceId.endsWith('.dialog')) {
-                declarativeTypes.push({
-                    kind: resourceId,
-                    type: DynamicBeginDialog,
-                    loader: new DynamicBeginDialogDeserializer(resourceExplorer, resourceId),
-                });
-            }
-        });
-        return declarativeTypes;
-    }
-
-    /**
-     * @param type
-     * @param loader
-     * @protected
-     */
-    protected _addDeclarativeType<T, C>(type: Type<T>, loader?: CustomDeserializer<T, C>): void {
-        const declarativeType: DeclarativeType<T, C> = {
-            kind: type.$kind,
-            type,
-            loader,
-        };
-        this._declarativeTypes.push(declarativeType);
+        this._addDeclarativeType<OnTeamsAppBasedLinkQuery, OnActivityConfiguration>(OnTeamsAppBasedLinkQuery);
     }
 }
