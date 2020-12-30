6251546935b326809c9fc9ca004eaf8d880d05b0
Michael Richardson <v-micric@microsoft.com>
2020-12-30 10:51:58 -0800

    clean imports

--- libraries/botbuilder-dialogs-adaptive-teams/tests/tests/actionTests.test.ts
+++ libraries/botbuilder-dialogs-adaptive-teams/tests/tests/actionTests.test.ts
@@ -2,17 +2,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 
 import 'mocha';
-import { ComponentRegistration, ConversationState, TestAdapter, useBotState } from 'botbuilder';
+import { ComponentRegistration, ConversationState, TestAdapter, useBotState, MemoryStorage } from 'botbuilder';
 import { ResourceExplorer } from 'botbuilder-dialogs-declarative';
 import { TeamsComponentRegistration } from '../../lib';
-import path = require('path');
 import { AdaptiveTestComponentRegistration, TestUtils } from 'botbuilder-dialogs-adaptive-testing';
 import { AdaptiveComponentRegistration } from 'botbuilder-dialogs-adaptive';
-import * as nock from 'nock';
+import { ConnectorClient, MicrosoftAppCredentials } from 'botframework-connector';
 import { ok } from 'assert';
-import { MemoryStorage } from 'botbuilder-core/src';
-import { ConnectorClient } from 'botframework-connector';
-import { MicrosoftAppCredentials } from 'botframework-connector/src';
+import path = require('path');
+import * as nock from 'nock';
 
 /**
  * Registers mocha hooks for proper usage
