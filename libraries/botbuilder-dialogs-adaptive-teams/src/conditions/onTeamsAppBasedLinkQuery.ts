fe3b0b283ef10fe93d01a26a2dc9539957fba8ce
Michael Richardson <v-micric@microsoft.com>
2021-01-12 09:45:24 -0800

    Merge branch 'adaptiveTeams' of https://github.com/mdrichardson/botbuilder-js into adaptiveTeams

--- libraries/botbuilder-dialogs-adaptive-teams/src/conditions/onTeamsAppBasedLinkQuery.ts
+++ libraries/botbuilder-dialogs-adaptive-teams/src/conditions/onTeamsAppBasedLinkQuery.ts
@@ -1,10 +1,13 @@
 /**
  * @module botbuilder-dialogs-adaptive-teams
  */
-// Licensed under the MIT License.
-// Copyright (c) Microsoft Corporation. All rights reserved.
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
 
-import { Dialog } from 'botbuilder-dialogs';
+import { Expression } from 'adaptive-expressions';
+import { Dialog, TurnPath } from 'botbuilder-dialogs';
 import { OnInvokeActivity } from 'botbuilder-dialogs-adaptive';
 
 /**
@@ -15,5 +18,13 @@ export class OnTeamsAppBasedLinkQuery extends OnInvokeActivity {
 
     constructor(actions: Dialog[] = [], condition?: string) {
         super(actions, condition);
+    }
+
+    protected createExpression(): Expression {
+        // if name is 'composeExtension/queryLink'
+        return Expression.andExpression(
+            Expression.parse(`${TurnPath.activity}.name == 'composeExtension/queryLink'`),
+            super.createExpression()
+        );
     }
 }
