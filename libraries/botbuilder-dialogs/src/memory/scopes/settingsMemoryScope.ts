c98b7d3e5f14bff813e91676064008d81b662520
Zichuan Ma <zim@microsoft.com>
2021-01-26 16:45:18 +0800

    added configuration to turn state

--- libraries/botbuilder-dialogs/src/memory/scopes/settingsMemoryScope.ts
+++ libraries/botbuilder-dialogs/src/memory/scopes/settingsMemoryScope.ts
@@ -8,6 +8,16 @@
 import { MemoryScope } from './memoryScope';
 import { ScopePath } from '../scopePath';
 import { DialogContext } from '../../dialogContext';
+import { DialogTurnStateConstants } from '../../dialogTurnStateConstants';
+import { merge } from 'lodash';
+
+/**
+ * The setting node.
+ */
+class Node {
+    public constructor(public value?: string) {}
+    public children: Node[] = [];
+}
 
 /**
  * SettingsMemoryScope maps "settings" -> dc.context.turnState['settings']
@@ -25,16 +35,110 @@ export class SettingsMemoryScope extends MemoryScope {
      * @param dc The [DialogContext](xref:botbuilder-dialogs.DialogContext) object for this turn.
      * @returns The memory for the scope.
      */
-    public getMemory(dc: DialogContext): object {
-        let settings: object = {};
+    public getMemory(dc: DialogContext): Record<string, unknown> {
+        let settings: Record<string, unknown> = {};
         if (dc.context.turnState.has(ScopePath.settings)) {
             settings = dc.context.turnState.get(ScopePath.settings);
-        }
-        for (const key in process.env) {
-            if (typeof process.env[key] == 'string') {
-                settings[key] = process.env[key];
+        } else {
+            const configuration = dc.context.turnState.get(DialogTurnStateConstants.configuration);
+            if (configuration) {
+                settings = SettingsMemoryScope.loadSettings(configuration);
+                dc.context.turnState.set(ScopePath.settings, settings);
             }
         }
+        const envSettings = SettingsMemoryScope.loadSettings(process.env);
+        merge(settings, envSettings);
         return settings;
+    }
+
+    /**
+     * Build a dictionary view of configuration providers.
+     *
+     * @param configuration The configuration that we are running with.
+     * @returns Projected dictionary for settings.
+     */
+    protected static loadSettings(configuration: Record<string, string>): Record<string, unknown> {
+        const settings = {};
+        if (configuration) {
+            // load configuration into settings
+            const root = this.convertFlattenSettingToNode(Object.entries(configuration));
+            root.children.forEach((u) => {
+                settings[u.value] = this.convertNodeToObject(u);
+            });
+        }
+        return settings;
+    }
+
+    private static convertFlattenSettingToNode(kvs: [string, string][]): Node {
+        const root = new Node();
+
+        kvs.forEach(([key, value]) => {
+            const keyChain = key.split(':');
+            let currentNode = root;
+            keyChain.forEach((item) => {
+                const matchItem = currentNode.children.find((u) => u?.value === item);
+                if (!matchItem) {
+                    // Remove all the leaf children
+                    currentNode.children = currentNode.children.filter((u) => u.children.length !== 0);
+
+                    // Append new child into current node
+                    const node = new Node(item);
+                    currentNode.children.push(node);
+                    currentNode = node;
+                } else {
+                    currentNode = matchItem;
+                }
+            });
+
+            currentNode.children.push(new Node(value));
+        });
+
+        return root;
+    }
+
+    private static convertNodeToObject(node: Node): unknown {
+        if (!node.children.length) {
+            return {};
+        }
+
+        // If the child is leaf node, return its value directly.
+        if (node.children.length === 1 && node.children[0].children.length === 0) {
+            return node.children[0].value;
+        }
+
+        // check if all the children are number format.
+        let pureNumberIndex = true;
+        let indexArray: number[] = [];
+        let indexMax = -1;
+        for (let i = 0; i < node.children.length; i++) {
+            const child = node.children[i];
+            if (/^-?\d+$/.test(child.value)) {
+                const num = parseInt(child.value);
+                if (!isNaN(num) && num >= 0) {
+                    indexArray.push(num);
+                    if (num > indexMax) {
+                        indexMax = num;
+                    }
+                    continue;
+                }
+            }
+            pureNumberIndex = false;
+            break;
+        }
+
+        if (pureNumberIndex) {
+            // all children are int numbers, treat it as array.
+            const listResult = new Array(indexMax + 1);
+            for (let i = 0; i < node.children.length; i++) {
+                listResult[indexArray[i]] = this.convertNodeToObject(node.children[i]);
+            }
+
+            return listResult;
+        }
+
+        // Convert all child into dictionary
+        const result = {};
+        node.children.forEach((u) => (result[u.value] = this.convertNodeToObject(u)));
+        return result;
     }
 }
