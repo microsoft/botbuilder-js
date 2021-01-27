512b54abc9d6790968a0d13d28aa318982e5d542
Josh Gummersall <jgummersall@microsoft.com>
2021-01-27 13:26:49 -0800

    fix: port Luis intent2 properties from dotnet
    Fixes #2398

--- libraries/botbuilder-ai/src/luisTelemetryConstants.ts
+++ libraries/botbuilder-ai/src/luisTelemetryConstants.ts
@@ -9,14 +9,16 @@
  * The BotTelemetryClient event and property names that logged by default.
  */
 export class LuisTelemetryConstants {
-    public static readonly luisResultEvent: string = 'LuisResult'; // Event name
-    public static readonly applicationIdProperty: string = 'applicationId';
-    public static readonly intentProperty: string = 'intent';
-    public static readonly intentScoreProperty: string = 'intentScore';
-    public static readonly entitiesProperty: string = 'entities';
-    public static readonly questionProperty: string = 'question';
-    public static readonly activityIdProperty: string = 'activityId';
-    public static readonly sentimentLabelProperty: string = 'sentimentLabel';
-    public static readonly sentimentScoreProperty: string = 'sentimentScore';
-    public static readonly fromIdProperty: string = 'fromId';
+    public static readonly luisResultEvent = 'LuisResult'; // Event name
+    public static readonly applicationIdProperty = 'applicationId';
+    public static readonly intentProperty = 'intent';
+    public static readonly intentScoreProperty = 'intentScore';
+    public static readonly intent2Property = 'intent2';
+    public static readonly intentScore2Property = 'intentScore2';
+    public static readonly entitiesProperty = 'entities';
+    public static readonly questionProperty = 'question';
+    public static readonly activityIdProperty = 'activityId';
+    public static readonly sentimentLabelProperty = 'sentimentLabel';
+    public static readonly sentimentScoreProperty = 'sentimentScore';
+    public static readonly fromIdProperty = 'fromId';
 }
