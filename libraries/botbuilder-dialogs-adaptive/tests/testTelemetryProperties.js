d7f5af3db1816cf020e0d007d13b363b087530b5
Ashley Finafrock <v-asho@microsoft.com>
2021-02-18 16:25:37 -0800

    Removed escap \ from test telemetry props

--- libraries/botbuilder-dialogs-adaptive/tests/testTelemetryProperties.js
+++ libraries/botbuilder-dialogs-adaptive/tests/testTelemetryProperties.js
@@ -3,9 +3,9 @@ function getCodeIntentProperties()
     return {
         TopIntent: 'codeIntent',
         TopIntentScore: '1',
-        Intents: '{\"codeIntent\":{\"score\":1,\"pattern\":\"(?<code>[a-z][0-9])\"}}',
+        Intents: '{"codeIntent":{"score":1,"pattern":"(?<code>[a-z][0-9])"}}',
         Entities:
-        '{\"code\":[\"a1\",\"b2\"],\"$instance\":{\"code\":[{\"startIndex\":7,\"endIndex\":9,\"score\":1,\"text\":\"a1\",\"type\":\"code\"},{\"startIndex\":10,\"endIndex\":12,\"score\":1,\"text\":\"b2\",\"type\":\"code\"}]}}',
+        '{"code":["a1","b2"],"$instance":{"code":[{"startIndex":7,"endIndex":9,"score":1,"text":"a1","type":"code"},{"startIndex":10,"endIndex":12,"score":1,"text":"b2","type":"code"}]}}',
         AdditionalProperties: undefined,
     };
 }
@@ -15,9 +15,9 @@ function getColorIntentProperties()
     return {
         TopIntent: 'colorIntent',
         TopIntentScore: '1',
-        Intents: '{\"colorIntent\":{\"score\":1,\"pattern\":\"(color|colour)\"}}',
+        Intents: '{"colorIntent":{"score":1,"pattern":"(color|colour)"}}',
         Entities:
-        '{\"color\":[\"red\",\"orange\"],\"$instance\":{\"color\":[{\"startIndex\":19,\"endIndex\":23,\"score\":1,\"text\":\"red\",\"type\":\"color\",\"resolution\":{}},{\"startIndex\":27,\"endIndex\":34,\"score\":1,\"text\":\"orange\",\"type\":\"color\",\"resolution\":{}}]}}',
+        '{"color":["red","orange"],"$instance":{"color":[{"startIndex":19,"endIndex":23,"score":1,"text":"red","type":"color","resolution":{}},{"startIndex":27,"endIndex":34,"score":1,"text":"orange","type":"color","resolution":{}}]}}',
         AdditionalProperties: undefined,
     };
 }
