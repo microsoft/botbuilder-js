1774d5104b1e40073e577955212ec859819418f2
Josh Gummersall <1235378+joshgummersall@users.noreply.github.com>
2021-03-02 09:07:46 -0800

    feat: core runtime (#3336)

--- libraries/botbuilder-dialogs-declarative/src/customDeserializer.ts
+++ libraries/botbuilder-dialogs-declarative/src/customDeserializer.ts
@@ -12,5 +12,5 @@ import { Newable } from 'botbuilder-stdlib';
  * Defines the contract for a deserializer from a configuration object to a typed object.
  */
 export interface CustomDeserializer<T, C> {
-    load(config: C, type: Newable<T>): T;
+    load(config: C, type: Newable<T, any[]>): T;
 }
---
+++ libraries/botbuilder-runtime-core/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export * from './configuration';
+export * from './plugin';
+export * from './serviceCollection';
+export * from './services';
