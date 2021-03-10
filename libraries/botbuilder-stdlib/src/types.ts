2d4b8396721868cdcf6a088df9a243cdfa53591c
Zichuan Ma <zim@microsoft.com>
2021-03-10 16:43:51 +0800

    added tests.unsafe.isObjectAs

--- libraries/botbuilder-stdlib/src/types.ts
+++ libraries/botbuilder-stdlib/src/types.ts
@@ -526,6 +526,25 @@ function makeTest<T>(assertion: Assertion<T>): Test<T> {
     };
 }
 
+/**
+ * **UNSAFE**
+ * Test if `val` is of type `object`.
+ * This assertion does not actually verify that `val` is of type `T`. It is useful as the first
+ * line in a nested assertion so that remaining assertion calls can leverage helpful intellisense.
+ * This method is only exported under the `unsafe` keyword as a constant reminder of this fact.
+ *
+ * @template T the type to cast `val` to, should extend `Dictionary<unknown>`, i.e. be itself an object
+ * @param {any} val value to test
+ * @returns {boolean} true if `val` is of type `object`
+ */
+function isObjectAs<T>(val: unknown): val is T {
+    if (isObject(val)) {
+        castObjectAs<T>(val, []);
+        return true;
+    }
+    return false;
+}
+
 export const tests = {
     isAny,
     isArray,
@@ -545,6 +564,8 @@ export const tests = {
 
     fromAssertion: makeTest,
     toAssertion: makeAssertion,
+
+    unsafe: { isObjectAs },
 };
 
 /**
