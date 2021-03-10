dbd6d4e25f918f0d89419205873ccaf90649edd5
Josh Gummersall <jgummersall@microsoft.com>
2021-03-10 10:11:21 -0800

    fix: idiomatic react, wait for elements

--- libraries/streaming-e2e-tests/react-app/src/App.js
+++ libraries/streaming-e2e-tests/react-app/src/App.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { Fragment, useCallback, useEffect, useMemo, useReducer } from 'react';
 import ReactWebChat from 'botframework-webchat';
+import { DirectLineStreaming } from 'botframework-directlinejs';
 
-function App({directLine}) {
-  // nonce related to botbuilder-js content security issue #2762:
-  // https://github.com/microsoft/botbuilder-js/issues/2762
-  return (
-    <ReactWebChat directLine={directLine} nonce="a1b2c3d" />
-  );
+const actions = {
+    error: 'error',
+    token: 'token',
+};
+
+function App({ bot }) {
+    if (!bot) {
+        throw new Error('bot prop is required');
+    }
+
+    const hostname = `https://${bot}.azurewebsites.net`;
+
+    const [state, dispatch] = useReducer(
+        (state, action) => {
+            switch (action.type) {
+                case actions.error:
+                    return { ...state, error: action.value, token: null };
+
+                case actions.token:
+                    return { ...state, error: null, token: action.value };
+
+                default:
+                    return state;
+            }
+        },
+        { error: null, token: null }
+    );
+
+    // Synchronize token with hostname
+    useEffect(() => {
+        (async () => {
+            try {
+                const resp = await fetch(`${hostname}/api/token/directlinease`, {
+                    method: 'POST',
+                });
+
+                const text = await resp.clone().text();
+
+                try {
+                    const token = await resp.json();
+                    dispatch({ type: actions.token, value: token.token });
+                } catch (err) {
+                    dispatch({type: actions.error, value: `${err.message} (raw body: ${text})`});
+                }
+            } catch (err) {
+                dispatch({ type: actions.error, value: err.message });
+            }
+        })();
+    }, [hostname, dispatch]);
+
+    // memo-ize directline client synchronized with hostname and token
+    const directLine = useMemo(
+        () =>
+            state.token
+                ? new DirectLineStreaming({
+                      domain: `${hostname}/.bot/v3/directline`,
+                      token: state.token,
+                  })
+                : null,
+        [hostname, state.token]
+    );
+
+    return (
+        <Fragment>
+            {state.error ? (
+                <div id="webchat-error">{state.error}</div>
+            ) : (
+                directLine && (
+                    <Fragment>
+                        <span id="react-webchat" />
+                        {/* nonce related to botbuilder-js content security issue #2762: https://github.com/microsoft/botbuilder-js/issues/2762 */}
+                        <ReactWebChat directLine={directLine} nonce="a1b2c3d" />
+                    </Fragment>
+                )
+            )}
+        </Fragment>
+    );
 }
 
 export default App;
--- libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
+++ libraries/streaming-e2e-tests/tests/streaming-e2e.test.js
@@ -11,22 +11,22 @@ const userMessage = 'Why hello there';
 const reactAppEndpoint = 'http://localhost:3000';
 
 describe('Chrome', function () {
-  it('should receive an echo after sending a message', async function () {
-    this.timeout(120000);
+    it('should receive an echo after sending a message', async function () {
+        this.timeout(120000);
 
-    const driver = createDriver('chrome');
-    await echoMessageInBrowser(driver);
-    const transcriptMessages = await getTranscriptMessages(driver, 2);
+        const driver = createDriver('chrome');
+        await echoMessageInBrowser(driver);
+        const transcriptMessages = await getTranscriptMessages(driver, 2);
 
-    await ensureNoBrowserErrors(driver);
-    
-    console.log('Transcript received. Asserting...');
-    assert.strictEqual(transcriptMessages[0], userMessage);
-    assert.strictEqual(transcriptMessages[1], `Streaming Echo: ${userMessage}.`);
-    assert.strictEqual(transcriptMessages.length, 2);
+        await ensureNoBrowserErrors(driver);
 
-    await driver.quit();
-  });
+        console.log('Transcript received. Asserting...');
+        assert.strictEqual(transcriptMessages[0], userMessage);
+        assert.strictEqual(transcriptMessages[1], `Streaming Echo: ${userMessage}.`);
+        assert.strictEqual(transcriptMessages.length, 2);
+
+        await driver.quit();
+    });
 });
 
 function createDriver(browser) {
@@ -37,90 +37,90 @@ function createDriver(browser) {
     preferences.setLevel(logging.Type.BROWSER, logging.Level.SEVERE);
     options.setLoggingPrefs(preferences);
 
-    const builder = new Builder()
-        .setChromeOptions(options)
-        .forBrowser(browser)
-        .build();
+    const builder = new Builder().setChromeOptions(options).forBrowser(browser).build();
 
-  return builder;
+    return builder;
 }
 
 async function echoMessageInBrowser(driver) {
-  try {
     console.log(`Navigating to "${reactAppEndpoint}"...`);
     await driver.get(reactAppEndpoint);
 
     await ensureNoBrowserErrors(driver);
 
-    console.log('Sleeping to allow Web Chat to load...');
-    await driver.sleep(7000);
+    console.log('Waiting for webchat to load or error is reported...');
+    await Promise.race([
+        driver
+            .wait(until.elementLocated(By.id('webchat-error')))
+            .getText()
+            .then((message) => {
+                throw new Error(`Found #webchat-error DOM element: ${message}`);
+            }),
+        driver.wait(until.elementLocated(By.id('react-webchat'))),
+    ]);
 
     console.log('Getting Web Chat sendbox...');
-    let wcSendBox = await driver.wait(until.elementLocated(By.className('webchat__send-box-text-box__input')), 17000);
+    let wcSendBox = await driver.wait(until.elementLocated(By.className('webchat__send-box-text-box__input')), 10000);
 
     console.log('Sending user message...');
     await wcSendBox.sendKeys(userMessage, Key.RETURN);
 
     return driver;
-
-  } catch (error) {
-    throw new Error(error.message);
-  }
 }
 
 async function ensureNoBrowserErrors(driver) {
-  console.log('Getting browser logs...');
-  const browserConsoleErrors = await driver.manage().logs().get(logging.Type.BROWSER);
+    console.log('Getting browser logs...');
+    const browserConsoleErrors = await driver.manage().logs().get(logging.Type.BROWSER);
 
-  if (browserConsoleErrors.length == 0) {
-    return;
-  }
-  browserConsoleErrors.forEach((error) => console.log(error.level.name, error.message));
-  
-  throw new Error('SEVERE-level error found in browser.');
+    if (browserConsoleErrors.length == 0) {
+        return;
+    }
+    browserConsoleErrors.forEach((error) => console.log(error.level.name, error.message));
+
+    throw new Error('SEVERE-level error found in browser.');
 }
 
 async function getTranscriptMessages(driver, minNumMessages) {
-  console.log('Waiting for activities to load...');
-  await driver.wait(minNumActivitiesShown(minNumMessages), 60000);
+    console.log('Waiting for activities to load...');
+    await driver.wait(minNumActivitiesShown(minNumMessages), 10000);
 
-  console.log('Activities loaded. Getting transcript...');
-  const transcript = await getTranscript(driver);
-  const messageBubbles = await getBubbles(transcript);
+    console.log('Activities loaded. Getting transcript...');
+    const transcript = await getTranscript(driver);
+    const messageBubbles = await getBubbles(transcript);
 
-  return await Promise.all(
-    messageBubbles.map(async (bubble) => {
-      return await bubble
-        .findElement(By.css('p'))
-        .getText();
-    }));
+    return await Promise.all(
+        messageBubbles.map(async (bubble) => {
+            return await bubble.findElement(By.css('p')).getText();
+        })
+    );
 }
 
 function minNumActivitiesShown(numActivities) {
-  return new Condition(`${numActivities} activities is shown`, async driver => {
-    // To run hooks (WebChatTest.runHook), the code internally creates an activity.
-    // Inside the activity renderer, it calls hooks, but returns empty visually.
-    // This activity is invisible and should not count towards "minNumActivitiesShown".
+    return new Condition(`${numActivities} activities is shown`, async (driver) => {
+        // To run hooks (WebChatTest.runHook), the code internally creates an activity.
+        // Inside the activity renderer, it calls hooks, but returns empty visually.
+        // This activity is invisible and should not count towards "minNumActivitiesShown".
 
-    const activityElements = await driver.findElements(By.css(`.webchat__basic-transcript__activity`));
+        const activityElements = await driver.findElements(By.css(`.webchat__basic-transcript__activity`));
 
-    const numActivitiesShown = await driver.executeScript(
-      activityElements => [].reduce.call(
-        activityElements,
-        (numActivitiesShown, child) => numActivitiesShown + (child.children.length ? 1 : 0),
-        0
-      ),
-      activityElements
-    );
+        const numActivitiesShown = await driver.executeScript(
+            (activityElements) =>
+                [].reduce.call(
+                    activityElements,
+                    (numActivitiesShown, child) => numActivitiesShown + (child.children.length ? 1 : 0),
+                    0
+                ),
+            activityElements
+        );
 
-    return numActivitiesShown >= numActivities;
-  });
+        return numActivitiesShown >= numActivities;
+    });
 }
 
 async function getTranscript(driver) {
-  return await driver.findElement(By.css('.webchat__basic-transcript'));
+    return await driver.findElement(By.css('.webchat__basic-transcript'));
 }
 
 async function getBubbles(transcript) {
-  return await transcript.findElements(By.className('webchat__bubble__content'));
+    return await transcript.findElements(By.className('webchat__bubble__content'));
 }
