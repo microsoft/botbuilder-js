f1dd66812677c09c981a83171207493b03c15a25
Michael Richardson <40401643+mdrichardson@users.noreply.github.com>
2021-03-17 16:12:35 -0700

    Add dialog:merge tests (#3404)
    * Add dialog:merge tests
    
    * replace bf cli install w/ npx
    
    * fix formatting
    
    * use appropriate mergeCommand
    
    * test schema differences in CI
    
    * node v10 doesn't like null coalesce
    
    * test equality the right way
    
    Co-authored-by: Michael Richardson <v-micric@microsoft.com>

---
+++ libraries/botbuilder-dialogs-declarative/tests/schemaMergeTest.js
@@ -0,0 +1,156 @@
+const { FolderResourceProvider, ResourceExplorer, FileResource } = require('../lib');
+const path = require('path');
+const assert = require('assert');
+const fs = require('fs');
+const Validator = require('jsonschema').Validator;
+// eslint-disable-next-line security/detect-child-process
+const { exec } = require('child_process');
+
+// Note: This file is intentionally not named *.test.js to ensure it isn't run
+// via `yarn run test` or `npm run test`.
+
+const maybeIt = process.platform === 'win32' ? it : it.skip;
+
+async function runCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(
+            command,
+            {
+                cwd: __dirname,
+            },
+            (err, stdout, stderr) => {
+                // stderr may be used by VS Code Debugger, even without errors, so we ignore it in DEBUG environment.
+                // Note: You have to manually set "env": { "DEBUG": "true" } in launch.json for this check to work.
+                if (err || (stderr && process.env.DEBUG !== 'true')) {
+                    reject(err || stderr);
+                } else {
+                    resolve(stdout);
+                }
+            }
+        );
+    });
+}
+
+describe('Schema Merge Tests', function () {
+    const resourceExplorer = new ResourceExplorer();
+    const resourceProvider = new FolderResourceProvider(
+        resourceExplorer,
+        path.join(__dirname, '..', '..'),
+        true,
+        false
+    );
+    const dialogResources = resourceProvider.getResources('dialog');
+    const dialogs = dialogResources
+        .filter((r) => !r.id.endsWith('.schema.dialog'))
+        .map((resource) => {
+            return {
+                id: resource.id,
+                fullName: resource.fullName,
+            };
+        });
+
+    const testsSchemaPath = path.join(__dirname, '..', '..', 'tests.schema');
+    const testsSchemaFileResource = new FileResource(testsSchemaPath);
+    const testsSchema = JSON.parse(testsSchemaFileResource.readText());
+
+    // This fixture creates or updates test.schema by calling bf dialog:merge on all the schema files in the solution.
+    // This will install the latest version of botframework-cli if the schema changed and npm is present.
+    // This only runs on Windows platforms.
+    maybeIt('should merge dialog schemas', async function () {
+        this.timeout(150000);
+        fs.unlinkSync(testsSchemaPath);
+
+        // Merge all schema files.
+        const sdkRoot = path.join(__dirname, '..', '..', '..');
+        const mergeCommand = [
+            `bf dialog:merge ${sdkRoot}/libraries/**/*.schema`,
+            `${sdkRoot}/libraries/**/*.uischema`,
+            '!**/testbot.schema',
+            `!${sdkRoot}/libraries/botbuilder-dialogs-adaptive/tests/schema/*.*`,
+            `-o ${testsSchemaPath}`,
+        ];
+        try {
+            await runCommand(mergeCommand.join(' '));
+        } catch (err) {
+            // We may get there because there was an error running bf dialog:merge.
+            // Try installing latest bf if the schema changed to make sure the
+            // discrepancy is not because we are using a different version of the CLI
+            // and we ensure it is installed while on it.
+            try {
+                // Rerun merge command.
+                await runCommand(
+                    [
+                        'npx -p @microsoft/botframework-cli@next', // invoke with npx to not alter repo dependencies
+                        ...mergeCommand,
+                    ].join(' ')
+                );
+            } catch (err2) {
+                assert.fail(`Unable to merge schemas.\nFirst error:\n${err}\nSecond error:\n${err2}`);
+            }
+        }
+
+        if (process.env.CI) {
+            // Check that newly-generated schema matches the schema from before the `dialog:merge`
+            // command was run. We only test for this in CI because these files are expected to differ
+            // when this test is run locally.
+            const newSchema = fs.existsSync(testsSchemaPath) && JSON.parse(testsSchemaFileResource.readText());
+            assert.deepStrictEqual(
+                newSchema,
+                testsSchema,
+                [
+                    'Generated schema differs from committed schema.',
+                    'Run this test locally and commit the tests.*schema files to upload the correct and updated schema.',
+                ].join('\n')
+            );
+        }
+
+        assert.ok(JSON.parse(testsSchemaFileResource.readText()));
+    });
+
+    it('dialog resources are valid for schema', function () {
+        this.timeout(100000);
+
+        for (const resource of dialogs) {
+            const fileResource = new FileResource(resource.fullName);
+            const json = JSON.parse(fileResource.readText());
+            const schema = json.$schema;
+
+            // Everything should have $schema
+            assert(schema);
+
+            if (schema.startsWith('http')) {
+                // NOTE: Some schemas are not local.  We don't validate against those because they often depend on the SDK itself.
+                return;
+            }
+
+            const folder = path.dirname(fileResource.fullName);
+            assert(fs.existsSync(path.join(folder, path.normalize(schema))), `$schema: ${schema}`);
+
+            const omit = [
+                'Action_SendActivity.test.dialog',
+                'Action_BeginSkill.test.dialog',
+                'Action_BeginSkillEndDialog.test.dialog',
+                'Action_SendTabAuthResponseErrorWithAdapter.test.dialog',
+                'Action_SendTaskModuleCardResponseError.test.dialog',
+                'Action_SendAppBasedLinkQueryResponseError.test.dialog',
+                'Action_SendTabCardResponseError.test.dialog',
+                'Action_SendMEAuthResponseError.test.dialog',
+                'Action_SendMESelectItemResponseError.test.dialog',
+                'Action_SendMEAuthResponseErrorWithAdapter.test.dialog',
+                'Action_SendMEMessageResponseError.test.dialog',
+                'Action_SendMEBotMessagePreviewResponseError.test.dialog',
+                'Action_SendMEConfigQuerySettingUrlResponseError.test.dialog',
+                'Action_SendTabAuthResponseError.test.dialog',
+                'TestScriptTests_OAuthInputLG.test.dialog',
+            ];
+
+            if (omit.some((e) => fileResource.fullName.includes(e))) {
+                // Schema is in the omit list, end the test.
+                return;
+            }
+
+            const v = new Validator();
+            v.validate(json, testsSchema);
+        }
+    });
+});
