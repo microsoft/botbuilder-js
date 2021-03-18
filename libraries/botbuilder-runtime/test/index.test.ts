c7afbd03c1497fef41f719b3466f935e1b985897
Josh Gummersall <jgummersall@microsoft.com>
2021-03-18 10:22:13 -0700

    fix: add blobs/cosmos storage options

--- libraries/botbuilder-runtime/test/index.test.ts
+++ libraries/botbuilder-runtime/test/index.test.ts
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { BotFrameworkAdapter } from 'botbuilder';
+import { BlobsStorage } from 'botbuilder-azure-blobs';
+import { BotFrameworkAdapter, MemoryStorage } from 'botbuilder';
 import { Configuration, getRuntimeServices } from '../src';
+import { CosmosDbPartitionedStorage } from 'botbuilder-azure';
 import { ok } from 'assert';
 import { plugin } from 'botbuilder-runtime-core';
 
@@ -39,5 +41,51 @@ describe('getRuntimeServices', () => {
 
         const customAdapter = await services.mustMakeInstance('customAdapters');
         ok(customAdapter.get('foo'));
+    });
+
+    describe('storage', () => {
+        it('defaults to memory storage', async () => {
+            const [services] = await getRuntimeServices(__dirname, __dirname);
+            ok(services);
+
+            const storage = await services.mustMakeInstance('storage');
+            ok(storage instanceof MemoryStorage);
+        });
+
+        it('supports blobs storage', async () => {
+            const configuration = new Configuration().argv().env();
+
+            configuration.set(['runtimeSettings', 'storage'], 'BlobsStorage');
+
+            configuration.set(['BlobsStorage'], {
+                connectionString: 'UseDevelopmentStorage=true',
+                containerName: 'containerName',
+            });
+
+            const [services] = await getRuntimeServices(__dirname, configuration);
+            ok(services);
+
+            const storage = await services.mustMakeInstance('storage');
+            ok(storage instanceof BlobsStorage);
+        });
+
+        it('supports cosmos storage', async () => {
+            const configuration = new Configuration().argv().env();
+
+            configuration.set(['runtimeSettings', 'storage'], 'CosmosDbPartitionedStorage');
+
+            configuration.set(['CosmosDbPartitionedStorage'], {
+                authKey: 'authKey',
+                cosmosDbEndpoint: 'cosmosDbEndpoint',
+                containerId: 'containerId',
+                databaseId: 'databaseId',
+            });
+
+            const [services] = await getRuntimeServices(__dirname, configuration);
+            ok(services);
+
+            const storage = await services.mustMakeInstance('storage');
+            ok(storage instanceof CosmosDbPartitionedStorage);
+        });
     });
 });
