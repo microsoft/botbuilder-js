102eb4a79c515025d339d0b1e114d05c5754f895
Josh Gummersall <jgummersall@microsoft.com>
2021-06-14 15:26:13 -0700

    reconcile core bot adapter changes

--- libraries/botbuilder-dialogs-adaptive-runtime/src/coreBotAdapter.ts
+++ libraries/botbuilder-dialogs-adaptive-runtime/src/coreBotAdapter.ts
@@ -1,18 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { BotFrameworkAuthentication, ClaimsIdentity, SkillValidation } from 'botframework-connector';
-
-import {
-    ActivityEx,
-    CloudAdapter,
-    ConversationState,
-    InputHints,
-    MessageFactory,
-    TurnContext,
-    UserState,
-    useBotState,
-} from 'botbuilder';
+import { BotFrameworkAuthentication } from 'botframework-connector';
+import { CloudAdapter, ConversationState, UserState, useBotState } from 'botbuilder';
 
 export class CoreBotAdapter extends CloudAdapter {
     constructor(
@@ -27,60 +17,16 @@ export class CoreBotAdapter extends CloudAdapter {
         this.onTurnError = async (context, err) => {
             console.error('[onTurnError] unhandled error', err);
 
-            await this.sendErrorMessage(context, err).catch(() => null);
-            await this.sendEoCToParentIfSkill(context, err).catch(() => null);
-            await this.clearConversationState(context).catch(() => null);
+            // Send the exception message to the user. Since the default behavior does not
+            // send logs or trace activities, the bot appears hanging without any activity
+            // to the user.
+            await context.sendActivity(err instanceof Error ? err.message : err).catch(() => null);
+
+            // Delete the conversationState for the current conversation to prevent the
+            // bot from getting stuck in a error-loop caused by being in a bad state.
+            await this.conversationState.delete(context);
 
             throw err; // re-throw to delegate error handling to integration libraries
         };
-    }
-
-    private async sendErrorMessage(context: TurnContext, error: Error): Promise<void> {
-        try {
-            let errorMessageText = 'The bot encountered an error or bug';
-            let errorMessage = MessageFactory.text(errorMessageText, errorMessageText, InputHints.IgnoringInput);
-            await context.sendActivity(errorMessage);
-
-            errorMessageText = 'To continue to run this bot, please fix the bot source code.';
-            errorMessage = MessageFactory.text(errorMessageText, errorMessageText, InputHints.ExpectingInput);
-            await context.sendActivity(errorMessage);
-
-            await context.sendTraceActivity(
-                'OnTurnError Trace',
-                error,
-                'https://www.botframework.com/schemas/error',
-                'TurnError'
-            );
-        } catch (err) {
-            console.error('[sendErrorMessage]:', err);
-        }
-    }
-
-    private async sendEoCToParentIfSkill(context: TurnContext, error: Error): Promise<void> {
-        if (!this.isSkillBot(context)) {
-            return;
-        }
-
-        try {
-            const endOfConversation = ActivityEx.createEndOfConversationActivity();
-            endOfConversation.code = 'SkillError';
-            endOfConversation.text = error.message;
-            await context.sendActivity(endOfConversation);
-        } catch (err) {
-            console.error('[sendEoCToParentIfSkill]:', err);
-        }
-    }
-
-    private async clearConversationState(context: TurnContext): Promise<void> {
-        try {
-            await this.conversationState.delete(context);
-        } catch (err) {
-            console.error('[clearConversationState]:', err);
-        }
-    }
-
-    private isSkillBot(context: TurnContext): boolean {
-        const claimsIdentity = context.turnState.get<ClaimsIdentity>(context.adapter.BotIdentityKey);
-        return SkillValidation.isSkillClaim(claimsIdentity?.claims ?? []);
     }
 }
