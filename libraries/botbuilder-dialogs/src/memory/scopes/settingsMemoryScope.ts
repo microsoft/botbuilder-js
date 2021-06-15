436b858cd6b1e14cf87215139b1c30660f1fa1ba
Hongyang Du (hond) <hond@microsoft.com>
2021-06-15 10:21:35 +0800

    cherry-pick (4.11): Fix double evaluation and filter sensitive settings #3748
    * fix conflict
    
    * fix error

--- libraries/botbuilder-dialogs/src/memory/scopes/settingsMemoryScope.ts
+++ libraries/botbuilder-dialogs/src/memory/scopes/settingsMemoryScope.ts
@@ -13,6 +13,19 @@ import { DialogContext } from '../../dialogContext';
  * SettingsMemoryScope maps "settings" -> dc.context.turnState['settings']
  */
 export class SettingsMemoryScope extends MemoryScope {
+    private static readonly blockingList = [
+        'MicrosoftAppPassword',
+        'cosmosDb:authKey',
+        'blobStorage:connectionString',
+        'BlobsStorage:connectionString',
+        'CosmosDbPartitionedStorage:authKey',
+        'applicationInsights:connectionString',
+        'applicationInsights:InstrumentationKey',
+        'runtimeSettings:telemetry:options:connectionString',
+        'runtimeSettings:telemetry:options:instrumentationKey',
+        'runtimeSettings:features:blobTranscript:connectionString',
+    ];
+
     /**
      * Initializes a new instance of the [SettingsMemoryScope](xref:botbuilder-dialogs.SettingsMemoryScope) class.
      */
@@ -26,12 +39,12 @@ export class SettingsMemoryScope extends MemoryScope {
      * @returns The memory for the scope.
      */
     public getMemory(dc: DialogContext): object {
-        let settings: object = {};
+        let settings: Record<string, unknown> = {};
         if (dc.context.turnState.has(ScopePath.settings)) {
             settings = dc.context.turnState.get(ScopePath.settings);
         }
         for (const key in process.env) {
-            if (typeof process.env[key] == 'string') {
+            if (!SettingsMemoryScope.blockingList.some(u => u.toLowerCase() === key.toLowerCase()) && typeof process.env[key] == 'string') {
                 settings[key] = process.env[key];
             }
         }
