0e2f7773743550466a3b04120fb0c55c44129013
Josh Gummersall <jgummersall@microsoft.com>
2021-06-21 09:40:13 -0700

    fix: better consumer test errors

--- testing/consumer-test/run.ts
+++ testing/consumer-test/run.ts
@@ -5,25 +5,33 @@ import { promisify } from 'util';
 const execp = promisify(exec);
 
 // TODO: ANTLR breaks 3.1
-const versions = [/*'3.1',*/ '3.2', '3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '4.0'];
+const versions = [/*'3.1', '3.2',*/ '3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '4.0'];
 
 (async () => {
     try {
         console.log(`Running typescript consumer test against ["${versions.join('", "')}"]`);
 
-        await pmap(
+        const results = await pmap(
             versions,
             (version) =>
                 Promise.all([
                     execp(`npx -p typescript@${version} tsc -p tsconfig-test.json`),
                     execp(`npx -p typescript@${version} tsc -p tsconfig-test.json --lib es2018`),
-                ]),
+                ])
+                    .then(() => ({ err: null, version, success: true }))
+                    .catch((err) => ({ err, version, success: false })),
             { concurrency: 2 }
         );
+
+        const failed = results.filter(({ success }) => success === false);
+        if (failed.length) {
+            failed.map(({ err, version }) => console.error(`typescript@${version} failed`, err));
+            return process.exit(1);
+        }
 
         process.exit(0);
     } catch (error) {
         console.error(error);
-        process.exit(1);
+        process.exit(-1);
     }
 })();
