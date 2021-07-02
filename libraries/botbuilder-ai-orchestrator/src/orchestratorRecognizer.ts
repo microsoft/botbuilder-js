9bdb02d42b6fea501b7a34083f800448637fb728
Tien Suwandy <tiens@microsoft.com>
2021-07-01 17:50:10 -0700

    Update orchestratorRecognizer.ts
    fixed bug in initializemodel

--- libraries/botbuilder-ai-orchestrator/src/orchestratorRecognizer.ts
+++ libraries/botbuilder-ai-orchestrator/src/orchestratorRecognizer.ts
@@ -357,12 +357,21 @@ export class OrchestratorRecognizer extends AdaptiveRecognizer implements Orches
             this.scoreEntities = existsSync(entityModelFolder);
 
             const orchestrator = new oc.Orchestrator();
-            if (this.scoreEntities && !orchestrator.load(fullModelFolder, entityModelFolder)) {
-                throw new Error(
-                    `Model load failed - model folder ${fullModelFolder}, entity model folder ${entityModelFolder}.`
-                );
-            } else if (!orchestrator.load(fullModelFolder)) {
-                throw new Error(`Model load failed - model folder ${fullModelFolder}.`);
+            let ocLoaded = false;
+            if (this.scoreEntities) {
+                ocLoaded = orchestrator.load(fullModelFolder, entityModelFolder);
+                if (!ocLoaded) {
+                    throw new Error(
+                        `Model load failed - model folder ${fullModelFolder}, entity model folder ${entityModelFolder}.`
+                    );
+                }
+            }
+
+            if (!ocLoaded) {
+                ocLoaded = orchestrator.load(fullModelFolder);
+                if (!ocLoaded) {
+                    throw new Error(`Model load failed - model folder ${fullModelFolder}.`);
+                }
             }
             OrchestratorRecognizer.orchestrator = orchestrator;
         }
