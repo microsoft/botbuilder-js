7afa288668abd45b680bc0cd8bc7a58f3e276f34
Hongyang Du (hond) <hond@microsoft.com>
2021-07-09 23:42:57 +0800

    extend merge built-in function (#3858)

--- libraries/adaptive-expressions/src/builtinFunctions/merge.ts
+++ libraries/adaptive-expressions/src/builtinFunctions/merge.ts
@@ -6,50 +6,65 @@
  * Licensed under the MIT License.
  */
 
-import { Expression } from '../expression';
-import { EvaluateExpressionDelegate, ExpressionEvaluator } from '../expressionEvaluator';
+import { EvaluateExpressionDelegate, ExpressionEvaluator, ValueWithError } from '../expressionEvaluator';
 import { ExpressionType } from '../expressionType';
 import { FunctionUtils } from '../functionUtils';
 import { ReturnType } from '../returnType';
 
 /**
- * Merge two JSON objects into one JSON object.
+ * Merge multiple object(json) into one object(json).
+ * If the item is array, the elements of the array are merged as well.
  */
 export class Merge extends ExpressionEvaluator {
     /**
      * Initializes a new instance of the [Merge](xref:adaptive-expressions.Merge) class.
      */
     public constructor() {
-        super(ExpressionType.Merge, Merge.evaluator(), ReturnType.Object, Merge.validator);
+        super(ExpressionType.Merge, Merge.evaluator(), ReturnType.Object, FunctionUtils.validateAtLeastOne);
     }
 
     /**
      * @private
      */
     private static evaluator(): EvaluateExpressionDelegate {
-        return FunctionUtils.applySequenceWithError((args: any[]): any => {
-            let value: any;
-            let error: string;
-            if (
-                typeof args[0] === 'object' &&
-                !Array.isArray(args[0]) &&
-                typeof args[1] === 'object' &&
-                !Array.isArray(args[1])
-            ) {
-                Object.assign(args[0], args[1]);
-                value = args[0];
-            } else {
-                error = `The argumets ${args[0]} and ${args[1]} must be JSON objects.`;
-            }
+        return FunctionUtils.applyWithError(
+            (args: unknown[]): ValueWithError => {
+                const result = {};
+                for (const arg of args) {
+                    const objectResult = this.parseToObjectList(arg);
+                    if (objectResult.error != null) {
+                        return { value: undefined, error: objectResult.error };
+                    }
 
-            return { value, error };
-        });
+                    for (const item of objectResult.result) {
+                        Object.assign(result, item);
+                    }
+                }
+
+                return { value: result, error: undefined };
+            }
+        );
     }
 
-    /**
-     * @private
-     */
-    private static validator(expression: Expression): void {
-        FunctionUtils.validateArityAndAnyType(expression, 2, Number.MAX_SAFE_INTEGER);
+    private static parseToObjectList(arg: unknown): { result: Record<string, unknown>[]; error: string } {
+        const result: Record<string, unknown>[] = [];
+        let error: string;
+        if (arg == null) {
+            error = `The argument ${arg} must be a JSON object or array.`;
+        } else if (Array.isArray(arg)) {
+            for (const item of arg) {
+                if (typeof item === 'object' && !Array.isArray(item)) {
+                    result.push(item);
+                } else {
+                    error = `The argument ${item} in array must be a JSON object.`;
+                }
+            }
+        } else if (typeof arg === 'object') {
+            result.push(arg as Record<string, unknown>);
+        } else {
+            error = `The argument ${arg} must be a JSON object or array.`;
+        }
+
+        return { result: result, error: error };
     }
 }
