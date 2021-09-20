27fbdf0b5a21c6a6b89921100987df46e454a393
sw-joelmut <joel.mut@southworks.com>
2021-09-20 15:42:12 -0300

    fix: skillValidation validTokenIssuers

--- libraries/botframework-connector/src/auth/skillValidation.ts
+++ libraries/botframework-connector/src/auth/skillValidation.ts
@@ -141,7 +141,7 @@ export namespace SkillValidation {
             ...ToBotFromBotOrEmulatorTokenValidationParameters,
             issuer: [
                 ...ToBotFromBotOrEmulatorTokenValidationParameters.issuer,
-                ...(this.authConfiguration.validTokenIssuers || []),
+                ...(authConfig.validTokenIssuers || []),
             ],
         };
 
