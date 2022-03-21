39222d080acd6d54668d63ae3e0c189cae26e5e1
Eric Dahlvang <erdahlva@microsoft.com>
2022-03-21 18:20:58 -0500

    remove space

--- libraries/botframework-schema/src/index.ts
+++ libraries/botframework-schema/src/index.ts
@@ -2414,7 +2414,7 @@ export interface SearchInvokeOptions {
  * Name of 'pplication/search'.
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface SearchInvokeResponse extends AdaptiveCardInvokeResponse { }
+export interface SearchInvokeResponse extends AdaptiveCardInvokeResponse {}
 
 /**
  * Represents a response returned by a bot when it receives an `invoke` activity.
