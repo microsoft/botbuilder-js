6039c43d8e3da2c285461599712b7029514c644c
Ram Fattah <38049078+ramfattah@users.noreply.github.com>
2022-03-21 19:11:05 -0700

    update

--- libraries/botbuilder-dialogs-adaptive/tests/choiceInputDialog.test.js
+++ libraries/botbuilder-dialogs-adaptive/tests/choiceInputDialog.test.js
@@ -10,7 +10,7 @@ const {
     ActivityTemplate,
     ChoiceSet,
 } = require('../lib');
-const { ObjectExpression } = require('adaptive-expressions');
+const { ObjectExpression, ExpressionParser } = require('adaptive-expressions');
 
 describe('ChoiceInput', function () {
     this.timeout(3000);
@@ -26,34 +26,36 @@ describe('ChoiceInput', function () {
     // Create telemetryClient and trackEventStub
     const [telemetryClient, trackEventStub] = createTelemetryClientAndStub(captureTelemetryAction);
 
-    // Setup inputDialog dialog
-    const dialog = new ChoiceInput();
-    dialog.prompt = new ActivityTemplate('testtempl');
-    dialog.alwaysPrompt = true;
-
-    const state = {};
-    const ep = new ObjectExpression(new ChoiceSet([{ value: 'test1' }, { value: 'test2' }, { value: 'test3' }]));
-    const { value } = ep.tryGetValue(state);
-
-    const promptChoices = ep;
-    dialog.choices = value;
-
-    dialog._telemetryClient = telemetryClient;
-
     // Setup dialog manager
     const conversationState = new ConversationState(new MemoryStorage());
     const dm = new DialogManager();
     const dialogState = conversationState.createProperty('dialog');
     const dialogs = new DialogSet(dialogState);
     dm.conversationState = conversationState;
+
+    // Setup inputDialog dialog
+    const dialog = new ChoiceInput();
+    dialog.prompt = new ActivityTemplate('testtempl');
+    dialog.alwaysPrompt = true;
+
+    const state = {
+        choices: [{ value: 'test1' }, { value: 'test2' }, { value: 'test3' }],
+    };
+    const ep = new ObjectExpression('choices');
+    const { value } = ep.tryGetValue(state);
+
+    dialog.choices = ep;
+
+    dialog._telemetryClient = telemetryClient;
     dm.rootDialog = dialog;
 
     it('eval promptUser()', async function () {
         // Send initial activity
         const adapter = new TestAdapter(async (context) => {
             const dc = await dialogs.createContext(context);
-            dc.state = true;
+            dc.state.setValue(value);
             await dialog.promptUser(dc, undefined);
+            // await dialog.beginDialog(dc);
 
             var vv = telemetryProperties;
 
