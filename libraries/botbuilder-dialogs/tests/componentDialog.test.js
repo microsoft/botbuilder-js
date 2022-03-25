ed75c752bb0a63b8c1e4d07d8ae0a6c8e67fe38f
Arsh Kashyap <arshk102001@gmail.com>
2022-03-25 20:42:35 +0530

    Resolved conflicts.

--- libraries/botbuilder-dialogs/tests/componentDialog.test.js
+++ libraries/botbuilder-dialogs/tests/componentDialog.test.js
@@ -68,7 +68,7 @@ describe('ComponentDialog', function () {
         const adapter = new TestAdapter(async (turnContext) => {
             const dc = await dialogs.createContext(turnContext);
             await assert.rejects(async () => await dc.beginDialog('composite'), {
-                message: `Cannot read properties of undefined (reading 'status')`,
+                message: `Cannot read property 'status' of undefined`,
             });
         });
         await adapter.send('Hi').startTest();
