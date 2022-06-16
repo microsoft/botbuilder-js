83ba004aac338d6306974d14ec971fe61e3b5ead
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2022-06-16 12:50:43 -0300

    Fix eslint issues in botbuilder-core (2/2) (#4229)

--- libraries/botbuilder-core/tests/botState.test.js
+++ libraries/botbuilder-core/tests/botState.test.js
@@ -9,98 +9,101 @@ function cachedState(context, stateKey) {
     return cached ? cached.state : undefined;
 }
 
-describe(`BotState`, function () {
+describe('BotState', function () {
     this.timeout(5000);
 
     const storage = new MemoryStorage();
     const adapter = new TestAdapter();
     const context = new TurnContext(adapter, receivedMessage);
     const botState = new BotState(storage, (context) => {
-        assert(context, `context not passed into storage stateKey factory.`);
+        assert(context, 'context not passed into storage stateKey factory.');
         return storageKey;
     });
-    it(`should return undefined from get() if nothing cached.`, async function () {
+    it('should return undefined from get() if nothing cached.', async function () {
         const state = botState.get(context);
-        assert(state === undefined, `state returned.`);
+        assert(state === undefined, 'state returned.');
     });
 
-    it(`should load and save state from storage.`, async function () {
+    it('should load and save state from storage.', async function () {
         await botState.load(context);
-        let state = cachedState(context, botState.stateKey);
-        assert(state, `State not loaded`);
+        const state = cachedState(context, botState.stateKey);
+        assert(state, 'State not loaded');
         state.test = 'foo';
         await botState.saveChanges(context);
 
         const items = await storage.read([storageKey]);
-        assert(items.hasOwnProperty(storageKey), `Saved state not found in storage.`);
-        assert(items[storageKey].test === 'foo', `Missing test value in stored state.`);
+        assert(Object.prototype.hasOwnProperty.call(items, storageKey), 'Saved state not found in storage.');
+        assert(items[storageKey].test === 'foo', 'Missing test value in stored state.');
     });
 
-    it(`should force load() of state from storage.`, async function () {
+    it('should force load() of state from storage.', async function () {
         await botState.load(context);
         const state = cachedState(context, botState.stateKey);
-        assert(state.test === 'foo', `invalid initial state`);
+        assert(state.test === 'foo', 'invalid initial state');
         delete state.test === 'foo';
         await botState.load(context, true);
-        assert(cachedState(context, botState.stateKey).test === 'foo', `state not reloaded`);
+        assert(cachedState(context, botState.stateKey).test === 'foo', 'state not reloaded');
     });
 
-    it(`should clear() state storage.`, async function () {
+    it('should clear() state storage.', async function () {
         await botState.load(context);
-        assert(cachedState(context, botState.stateKey).test === 'foo', `invalid initial state`);
+        assert(cachedState(context, botState.stateKey).test === 'foo', 'invalid initial state');
         await botState.clear(context);
-        assert(!cachedState(context, botState.stateKey).hasOwnProperty('test'), `state not cleared on context.`);
+        assert(
+            !Object.prototype.hasOwnProperty.call(cachedState(context, botState.stateKey), 'test'),
+            'state not cleared on context.'
+        );
         await botState.saveChanges(context);
 
         const items = await storage.read([storageKey]);
-        assert(items[storageKey], `state removed from storage.`);
-        assert(!items[storageKey].hasOwnProperty('test'), `state not cleared from storage.`);
+        assert(items[storageKey], 'state removed from storage.');
+        assert(!Object.prototype.hasOwnProperty.call(items[storageKey], 'test'), 'state not cleared from storage.');
     });
 
-    it(`should delete() state storage.`, async function () {
+    it('should delete() state storage.', async function () {
         await botState.load(context);
-        assert(cachedState(context, botState.stateKey), `invalid initial state`);
+        assert(cachedState(context, botState.stateKey), 'invalid initial state');
         await botState.delete(context);
-        assert(!cachedState(context, botState.stateKey), `state not cleared on context.`);
+        assert(!cachedState(context, botState.stateKey), 'state not cleared on context.');
 
         const items = await storage.read([storageKey]);
-        assert(!items.hasOwnProperty(storageKey), `state not removed from storage.`);
+        assert(!Object.prototype.hasOwnProperty.call(items, storageKey), 'state not removed from storage.');
     });
 
-    it(`should force immediate saveChanges() of state to storage.`, async function () {
+    it('should force immediate saveChanges() of state to storage.', async function () {
         await botState.load(context);
         const state = cachedState(context, botState.stateKey);
-        assert(!state.hasOwnProperty('foo'), `invalid initial state`);
+        assert(!Object.prototype.hasOwnProperty.call(state, 'foo'), 'invalid initial state');
         state.test = 'foo';
         await botState.saveChanges(context, true);
         const items = await storage.read([storageKey]);
-        assert(items[storageKey].test === 'foo', `state not immediately flushed.`);
+        assert(items[storageKey].test === 'foo', 'state not immediately flushed.');
     });
 
-    it(`should load() from storage if cached state missing.`, async function () {
+    it('should load() from storage if cached state missing.', async function () {
         context.turnState.set(botState.stateKey, undefined);
         const state = await botState.load(context);
-        assert(state.test === 'foo', `state not loaded.`);
+        assert(state.test === 'foo', 'state not loaded.');
     });
 
-    it(`should load() from storage if cached.state missing.`, async function () {
+    it('should load() from storage if cached.state missing.', async function () {
         context.turnState.set(botState.stateKey, {});
         const state = await botState.load(context);
-        assert(state.test === 'foo', `state not loaded.`);
+        assert(state.test === 'foo', 'state not loaded.');
     });
 
-    it(`should force saveChanges() to storage of an empty state object.`, async function () {
+    it('should force saveChanges() to storage of an empty state object.', async function () {
         context.turnState.set(botState.stateKey, undefined);
         await botState.saveChanges(context, true);
     });
 
-    it(`should no-op calls to clear() when nothing cached.`, async function () {
+    it('should no-op calls to clear() when nothing cached.', async function () {
         context.turnState.set(botState.stateKey, undefined);
         await botState.clear(context);
     });
 
-    it(`should create new PropertyAccessors`, async function () {
-        let count = botState.createProperty('count', 1);
-        assert(count !== undefined, `did not successfully create PropertyAccessor.`);
+    it('should create new PropertyAccessors', async function () {
+        const count = botState.createProperty('count', 1);
+        assert(count !== undefined, 'did not successfully create PropertyAccessor.');
     });
 });
--- libraries/botbuilder-core/tests/conversationState.test.js
+++ libraries/botbuilder-core/tests/conversationState.test.js
@@ -5,51 +5,51 @@ const receivedMessage = { text: 'received', type: 'message', channelId: 'test', 
 const missingChannelId = { text: 'received', type: 'message', conversation: { id: 'convo' } };
 const missingConversation = { text: 'received', type: 'message', channelId: 'test' };
 
-describe(`ConversationState`, function () {
+describe('ConversationState', function () {
     this.timeout(5000);
 
     const storage = new MemoryStorage();
     const adapter = new TestAdapter();
     const context = new TurnContext(adapter, receivedMessage);
     const conversationState = new ConversationState(storage);
-    it(`should load and save state from storage.`, async function () {
+    it('should load and save state from storage.', async function () {
         // Simulate a "Turn" in a conversation by loading the state,
         // changing it and then saving the changes to state.
         await conversationState.load(context);
         const key = conversationState.getStorageKey(context);
         const state = conversationState.get(context);
-        assert(state, `State not loaded`);
-        assert(key, `Key not found`);
+        assert(state, 'State not loaded');
+        assert(key, 'Key not found');
         state.test = 'foo';
         await conversationState.saveChanges(context);
 
         // Check the storage to see if the changes to state were saved.
         const items = await storage.read([key]);
-        assert(items[key], `Saved state not found in storage.`);
-        assert(items[key].test === 'foo', `Missing test value in stored state.`);
+        assert(items[key], 'Saved state not found in storage.');
+        assert(items[key].test === 'foo', 'Missing test value in stored state.');
     });
 
-    it(`should ignore any activities that aren't "endOfConversation".`, async function () {
+    it('should ignore any activities that aren\'t "endOfConversation".', async function () {
         await conversationState.load(context);
         const key = conversationState.getStorageKey(context);
-        assert(conversationState.get(context).test === 'foo', `invalid initial state`);
+        assert(conversationState.get(context).test === 'foo', 'invalid initial state');
         await context.sendActivity({ type: ActivityTypes.Message, text: 'foo' });
 
         const items = await storage.read([key]);
-        assert(items[key].test != null, `state cleared and shouldn't have been.`);
+        assert(items[key].test != null, "state cleared and shouldn't have been.");
     });
 
-    it(`should reject with error if channelId missing.`, function () {
+    it('should reject with error if channelId missing.', function () {
         const ctx = new TurnContext(adapter, missingChannelId);
         assert.throws(() => conversationState.load(ctx), Error('missing activity.channelId'));
     });
 
-    it(`should reject with error if conversation missing.`, function () {
+    it('should reject with error if conversation missing.', function () {
         const ctx = new TurnContext(adapter, missingConversation);
         assert.throws(() => conversationState.load(ctx), Error('missing activity.conversation.id'));
     });
 
-    it(`should throw NO_KEY error if getStorageKey() returns falsey value.`, async function () {
+    it('should throw NO_KEY error if getStorageKey() returns falsey value.', async function () {
         conversationState.getStorageKey = (_turnContext) => undefined;
         await assert.rejects(
             conversationState.load(context, true),
