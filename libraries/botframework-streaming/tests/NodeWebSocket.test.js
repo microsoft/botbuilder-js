87f03d45d8d381db106f2f02f1d30c909168ca90
William Wong <compulim@users.noreply.github.com>
2023-01-20 17:35:24 +0000

    Remove .only

--- libraries/botframework-streaming/tests/NodeWebSocket.test.js
+++ libraries/botframework-streaming/tests/NodeWebSocket.test.js
@@ -7,7 +7,7 @@ const waitFor = require('./helpers/waitFor');
 
 const TEST_SERVER_PORT = 53978;
 
-describe.only('NodeWebSocket', function () {
+describe('NodeWebSocket', function () {
     it('creates a new NodeWebSocket', function () {
         const socket = new NodeWebSocket(new FauxSock());
         expect(socket).to.be.instanceOf(NodeWebSocket);
