ee04b021e6747ac00d48ff87acac621eb77db4f3
Tracy Boehrer <trboehre@microsoft.com>
2023-02-16 08:27:55 -0600

    Revert "fix: streaming client should connect under Node.js (#4413)"
    This reverts commit 6c1006151b2365783b71c7a632dc4b1051989476.

--- libraries/botframework-streaming/tests/NodeWebSocketFactory.test.js
+++ libraries/botframework-streaming/tests/NodeWebSocketFactory.test.js
@@ -1,12 +1,12 @@
-const { expect } = require('chai');
-const { FauxSocket, TestRequest } = require('./helpers');
 const { NodeWebSocket, NodeWebSocketFactory } = require('..');
+const { FauxSock, TestRequest } = require('./helpers');
+const { expect } = require('chai');
 const { randomBytes } = require('crypto');
 
 describe('NodeWebSocketFactory', function () {
     it('createWebSocket() should create a new NodeWebSocket', async function () {
         const factory = new NodeWebSocketFactory();
-        const sock = new FauxSocket();
+        const sock = new FauxSock();
         const request = new TestRequest();
         request.setIsUpgradeRequest(true);
         request.headers = [];
--- libraries/botframework-streaming/tests/helpers/sleep.js
+++
@@ -1,3 +0,0 @@
-module.exports.sleep = function sleep(durationInMS = 100) {
-    return new Promise((resolve) => setTimeout(resolve, durationInMS));
-};
