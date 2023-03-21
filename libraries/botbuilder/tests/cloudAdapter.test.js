953f44eb99d76109c580b1da651a6b32fede989a
Tracy Boehrer <trboehre@microsoft.com>
2023-03-21 09:08:25 -0500

    Revert "Add expired token exception and tests"
    This reverts commit a2bdf1031d053719e3923f9becdeb1a126d16f32.

--- libraries/botbuilder/tests/cloudAdapter.test.js
+++ libraries/botbuilder/tests/cloudAdapter.test.js
@@ -5,18 +5,9 @@ const assert = require('assert');
 const httpMocks = require('node-mocks-http');
 const net = require('net');
 const sinon = require('sinon');
-const {
-    AuthenticationConfiguration,
-    BotFrameworkAuthenticationFactory,
-    allowedCallersClaimsValidator,
-} = require('botframework-connector');
-const {
-    ConfigurationServiceClientCredentialFactory,
-    createBotFrameworkAuthenticationFromConfiguration,
-} = require('botbuilder');
+const { BotFrameworkAuthenticationFactory } = require('botframework-connector');
 const { CloudAdapter, ActivityTypes, INVOKE_RESPONSE_KEY } = require('..');
 const { NamedPipeServer } = require('botframework-streaming');
-const { StatusCodes } = require('botframework-schema');
 
 const FakeBuffer = () => Buffer.from([]);
 const FakeNodeSocket = () => new net.Socket();
@@ -25,7 +16,7 @@ const noop = () => null;
 describe('CloudAdapter', function () {
     let sandbox;
     beforeEach(function () {
-        sandbox = sinon.createSandbox({ useFakeTimers: false });
+        sandbox = sinon.createSandbox({ useFakeTimers: true });
     });
 
     afterEach(function () {
@@ -95,56 +86,6 @@ describe('CloudAdapter', function () {
             await adapter.process(req, res, logic);
 
             mock.verify();
-        });
-
-        it('throws exception on expired token', async function () {
-            // Expired token with removed AppID
-            const authorization =
-                'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL2FwaS5ib3RmcmFtZXdvcmsuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZDZkNDk0MjAtZjM5Yi00ZGY3LWExZGMtZDU5YTkzNTg3MWRiLyIsImlhdCI6MTY3MDM1MDQxNSwibmJmIjoxNjcwMzUwNDE1LCJleHAiOjE2NzA0MzcxMTUsImFpbyI6IkUyWmdZTkJONEpWZmxlOTJUc2wxYjhtOHBjOWpBQT09IiwiYXBwaWQiOiI5ZGRmM2QwZS02ZDRlLTQ2MWEtYjM4Yi0zMTYzZWQ3Yjg1NmIiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kNmQ0OTQyMC1mMzliLTRkZjctYTFkYy1kNTlhOTM1ODcxZGIvIiwicmgiOiIwLkFXNEFJSlRVMXB2ejkwMmgzTldhazFoeDIwSXpMWTBwejFsSmxYY09EcS05RnJ4dUFBQS4iLCJ0aWQiOiJkNmQ0OTQyMC1mMzliLTRkZjctYTFkYy1kNTlhOTM1ODcxZGIiLCJ1dGkiOiJIWDlncld2bU1rMlhESTRkS3BHSEFBIiwidmVyIjoiMS4wIn0.PBLuja5sCcDfFjweoy-VucvbfHEyEcs1GyqXjekzBqgvK-mSc1UrEfqr5834qY6dLNsXVIMJzMFuH6WyPbnAfIfRcabdiVSOAl8N8e9Tex6vHfPi4h4P2F96VkXU80EtZX4QMjsJMDJ5eXbJlIDEAxXoJbAdHqgy-lHcVBx8XK7toJ_W7vSsFhis3C4CPCHI1cf1WuHVwfFXBiNwsOzj9cnRUKpea6UELV89q4C0L6aeSNdWYXehZmgq-wlo2wIaGgQ7rOXx4MlIrc83LBzMMc6TWvBJecK6O8pJWLe6BTwOltBI8Tmo2hWnY1OnsbOhbSSlfwLaZqKI7QpA50_2GQ';
-
-            const activity = { type: ActivityTypes.Invoke, value: 'invoke' };
-
-            const req = httpMocks.createRequest({
-                method: 'POST',
-                headers: { authorization },
-                body: activity,
-            });
-
-            const res = httpMocks.createResponse();
-
-            const logic = async (context) => {
-                context.turnState.set(INVOKE_RESPONSE_KEY, {
-                    type: ActivityTypes.InvokeResponse,
-                    value: {
-                        status: 200,
-                        body: 'invokeResponse',
-                    },
-                });
-            };
-
-            const validTokenIssuers = [];
-            const claimsValidators = allowedCallersClaimsValidator(['*']);
-            const authConfig = new AuthenticationConfiguration([], claimsValidators, validTokenIssuers);
-            const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-                MicrosoftAppId: '',
-                MicrosoftAppPassword: '',
-                MicrosoftAppType: '',
-                MicrosoftAppTenantId: '',
-            });
-
-            const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(
-                null,
-                credentialsFactory,
-                authConfig,
-                undefined,
-                undefined
-            );
-
-            const adapter = new CloudAdapter(botFrameworkAuthentication);
-
-            await adapter.process(req, res, logic);
-
-            assert.equal(StatusCodes.UNAUTHORIZED, res.statusCode);
         });
     });
 
