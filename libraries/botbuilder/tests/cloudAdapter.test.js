7a8c6a55bc4b7fe655c9efaef7afcf72d8d8401c
JhontSouth <jhonatan.sandoval@southworks.com>
2023-11-08 08:35:15 -0500

    remove skip

--- libraries/botbuilder/tests/cloudAdapter.test.js
+++ libraries/botbuilder/tests/cloudAdapter.test.js
@@ -128,7 +128,7 @@ describe('CloudAdapter', function () {
             mock.verify();
         });
 
-        it.skip('throws exception on expired token', async function () {
+        it('throws exception on expired token', async function () {
             const consoleStub = sandbox.stub(console, 'error');
 
             // Expired token with removed AppID
