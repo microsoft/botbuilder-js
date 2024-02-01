946945ee1cd569853a3bdf04d0eee4a38780b2da
Tracy Boehrer <trboehre@microsoft.com>
2024-02-01 15:23:22 -0600

    Removed Copyright from generated code

--- generators/generator-botbuilder/generators/app/templates/core/tests/bots/dialogAndWelcomeBot.test.js
+++ generators/generator-botbuilder/generators/app/templates/core/tests/bots/dialogAndWelcomeBot.test.js
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 /* eslint-env node, mocha */
 const { TestAdapter, ActivityTypes, TurnContext, ConversationState, MemoryStorage, UserState } = require('botbuilder');
 const { DialogSet, DialogTurnStatus, Dialog } = require('botbuilder-dialogs');
