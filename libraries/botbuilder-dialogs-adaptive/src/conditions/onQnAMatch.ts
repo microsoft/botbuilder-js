df308fad533ba3b9a10f572d246b70640942bfc1
JhontSouth <jhonatan.sandoval@southworks.com>
2024-06-13 10:58:00 -0500

    fix qna double trace error

--- libraries/botbuilder-dialogs-adaptive/src/conditions/onQnAMatch.ts
+++ libraries/botbuilder-dialogs-adaptive/src/conditions/onQnAMatch.ts
@@ -8,13 +8,13 @@
 import { Dialog } from 'botbuilder-dialogs';
 import { OnIntent } from './onIntent';
 
-const qnaMatchIntent = 'QnAMatch';
-
 /**
  * Actions triggered when a MessageUpdateActivity is received.
  */
 export class OnQnAMatch extends OnIntent {
     static $kind = 'Microsoft.OnQnAMatch';
+    // this is a duplicate of QnAMakerRecognizer.QnAMatchIntent, but copying this here removes need to have dependency between QnA and Adaptive assemblies.
+    static qnaMatchIntent = 'QnAMatch';
 
     /**
      * Initializes a new instance of the [OnQnAMatch](xref:botbuilder-dialogs-adaptive.OnQnAMatch) class.
@@ -23,6 +23,6 @@ export class OnQnAMatch extends OnIntent {
      * @param condition Optional. Condition which needs to be met for the actions to be executed.
      */
     constructor(actions: Dialog[] = [], condition?: string) {
-        super(qnaMatchIntent, [], actions, condition);
+        super(OnQnAMatch.qnaMatchIntent, [], actions, condition);
     }
 }
