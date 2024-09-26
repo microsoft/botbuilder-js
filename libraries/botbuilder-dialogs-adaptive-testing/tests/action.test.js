3a56a51e71f80cb99fffc3db9a9b9d4872c6d993
CeciliaAvila <cecilia.avila@southworks.com>
2024-09-26 17:17:28 -0300

    Upgrade nock in adaptive-testing library

--- libraries/botbuilder-dialogs-adaptive-testing/tests/action.test.js
+++ libraries/botbuilder-dialogs-adaptive-testing/tests/action.test.js
@@ -394,10 +394,15 @@ describe('ActionTests', function () {
         nock('http://foo.com').post('/', 'Joe is 52').reply(200, 'string');
         nock('http://foo.com').post('/', { text: 'Joe is 52', age: 52 }).reply(200, 'object');
         nock('http://foo.com')
-            .post('/', [
-                { text: 'Joe is 52', age: 52 },
-                { text: 'text', age: 11 },
-            ])
+            .post(
+                '/',
+                JSON.stringify(
+                    Object.assign({}, [
+                        { text: 'Joe is 52', age: 52 },
+                        { text: 'text', age: 11 },
+                    ])
+                )
+            )
             .reply(200, 'array');
         nock('http://foo.com')
             .post('/', 'Joe is 52')
