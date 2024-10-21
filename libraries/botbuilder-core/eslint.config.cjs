8e288e2c1d567ae6c0634ae5a26ee9d2bda4efad
JhontSouth <jhonatan.sandoval@southworks.com>
2024-10-21 14:24:50 -0500

    Merge branch 'southworks/update/eslint-packages' into southworks/fix/eslint-issues-botbuilder-core

--- libraries/botbuilder-core/eslint.config.cjs
+++ libraries/botbuilder-core/eslint.config.cjs
@@ -1,11 +1,8 @@
-const onlyWarn = require("eslint-plugin-only-warn");
-const sharedConfig = require("../../eslint.config.cjs")
+const onlyWarn = require('eslint-plugin-only-warn');
+const sharedConfig = require('../../eslint.config.cjs');
 
 module.exports = [
     ...sharedConfig,
-    {
-        files: ['**/*.js, **/*.ts'],
-    },
     {
         plugins: {
             'only-warn': onlyWarn,
