8af49a92bd93b0c0bfc1afcdb111b2187682e553
CeciliaAvila <cecilia.avila@southworks.com>
2024-10-23 16:00:56 -0300

    Remove only-warning setting

--- libraries/botbuilder-ai/eslint.config.cjs
+++ libraries/botbuilder-ai/eslint.config.cjs
@@ -1,11 +1,3 @@
-const onlyWarn = require("eslint-plugin-only-warn");
-const sharedConfig = require("../../eslint.config.cjs")
+const sharedConfig = require('../../eslint.config.cjs');
 
-module.exports = [
-    ...sharedConfig,
-    {
-        plugins: {
-            "only-warn": onlyWarn,
-        },
-    }
-]
+module.exports = [...sharedConfig];
