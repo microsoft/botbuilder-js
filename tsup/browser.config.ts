84dc7e4a877eec8e3f5c7268fcd3c313f21c10dd
Joel Mut <62260472+sw-joelmut@users.noreply.github.com>
2024-10-29 14:34:11 -0300

    refactor: [#4684] Replace browserify with tsup (#4774)
    * Replace browserify with tsup in adaptive-expressions
    
    * Remove remaining browserify packages
    
    * Fix streaming tests
    
    * Fix yarn.lock
    
    * fix depcheck

---
+++ tsup/browser.config.ts
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+import { defineConfig } from 'tsup';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
+
+const packageJson = JSON.parse(readFileSync(resolve(process.cwd(), 'package.json'), 'utf-8'));
+
+const external = [/^botbuilder-/, /^botframework-/];
+
+export default defineConfig((options) => {
+    return {
+        name: 'browser',
+        platform: 'browser',
+        entry: options.define?.entry?.split(' ') ?? ['./src/index.ts'],
+        format: ['cjs'],
+        bundle: true,
+        sourcemap: true,
+        minify: true,
+        treeshake: true,
+        splitting: false,
+        external,
+        env: {
+            NODE_ENV: 'production',
+        },
+        noExternal: Object.keys(packageJson.dependencies).filter((packageName) => {
+            return !external.some((e) => packageName.match(e));
+        }),
+        esbuildOptions(options) {
+            options.outdir = '';
+            options.outfile = options.define.outfile ?? 'lib/browser.js';
+            options.inject = [resolve(__dirname, 'esbuild.inject.js')];
+            options.define = {
+                global: 'globalThis',
+            };
+            options.alias = {
+                crypto: 'crypto-browserify',
+                http: 'stream-http',
+                https: 'https-browserify',
+                stream: 'stream-browserify',
+                // Changed from .browser to .es5 file, as imported classes are not exported in .browser file.
+                '@microsoft/recognizers-text-data-types-timex-expression':
+                    '@microsoft/recognizers-text-data-types-timex-expression/dist/recognizers-text-data-types-timex-expression.es5.js',
+            };
+        },
+        esbuildPlugins: [
+            polyfillNode({
+                polyfills: {
+                    buffer: false,
+                    child_process: true,
+                    crypto: false,
+                    fs: true,
+                    http: false,
+                    https: false,
+                    net: true,
+                    process: false,
+                    stream: false,
+                    tls: true,
+                },
+            }),
+        ],
+    };
+});
