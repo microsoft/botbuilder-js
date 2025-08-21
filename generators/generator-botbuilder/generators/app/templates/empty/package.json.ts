ff3859674d8a110288071a0bd88fe3f0b8a51bbc
CeciliaAvila <cecilia.avila@southworks.com>
2025-08-21 14:28:43 -0300

    Update TS and types/node versions

--- generators/generator-botbuilder/generators/app/templates/empty/package.json.ts
+++ generators/generator-botbuilder/generators/app/templates/empty/package.json.ts
@@ -25,10 +25,10 @@
     },
     "devDependencies": {
         "@types/dotenv": "6.1.1",
-        "@types/node": "^18.19.47",
+        "@types/node": "^18.19.123",
         "@types/restify": "8.4.2",
         "nodemon": "^2.0.4",
         "tslint": "^6.1.2",
-        "typescript": "^5.6.3"
+        "typescript": "^5.9.0"
     }
 }
